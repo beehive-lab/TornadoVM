df7d3d75d328847623529b418dc7bf6bbef13e08
James Clarkson <clarksoj@cs.man.ac.uk>
2016-07-01 15:37:37 +0100

    [BUG] Removed scheduling changes for CPU

--- drivers/opencl/src/main/java/tornado/drivers/opencl/OCLScheduler.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/OCLScheduler.java
@@ -2,12 +2,11 @@ package tornado.drivers.opencl;
 
 import tornado.common.Tornado;
 import tornado.drivers.opencl.enums.OCLDeviceType;
-import static tornado.common.Tornado.USE_OPENCL_SCHEDULING;
 public class OCLScheduler {
 	
 	public static final OCLKernelScheduler create(final OCLDeviceContext context){
 		
-		if (USE_OPENCL_SCHEDULING || context.getDevice().getDeviceType() == OCLDeviceType.CL_DEVICE_TYPE_GPU ){
+		if (context.getDevice().getDeviceType() == OCLDeviceType.CL_DEVICE_TYPE_GPU ){
 			return new OCLGpuScheduler(context);
 		} else if(context.getDevice().getDeviceType() == OCLDeviceType.CL_DEVICE_TYPE_CPU || context.getDevice().getDeviceType() == OCLDeviceType.CL_DEVICE_TYPE_ACCELERATOR)
 			return new OCLCpuScheduler(context);
