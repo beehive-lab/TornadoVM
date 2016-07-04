b3d9ee2b81c0fbea8fe73b843ba6cbed2d9b93fc
James Clarkson <clarksoj@cs.man.ac.uk>
2016-07-04 16:28:34 +0100

    [ENHANCE] Added blacklist ability to benchmarks

--- benchmarks/src/main/java/tornado/benchmarks/BenchmarkRunner.java
+++ benchmarks/src/main/java/tornado/benchmarks/BenchmarkRunner.java
@@ -1,6 +1,8 @@
 package tornado.benchmarks;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import tornado.common.DeviceMapping;
 import tornado.drivers.opencl.OCLDriver;
 import tornado.drivers.opencl.runtime.OCLDeviceMapping;
@@ -8,59 +10,90 @@ import tornado.runtime.TornadoRuntime;
 
 public abstract class BenchmarkRunner {
 
-	protected abstract String getName();
-        protected abstract String getIdString();
-        protected abstract String getConfigString();
-        
-        protected abstract BenchmarkDriver getJavaDriver();
-        protected abstract BenchmarkDriver getTornadoDriver(DeviceMapping device);
-        
-        protected int iterations = 0;
+    protected abstract String getName();
 
-	public void run() {
-		final String id = getIdString(); 
+    protected abstract String getIdString();
 
-		System.out.printf("benchmark=%s, iterations=%d, %s\n", id, iterations,
-				getConfigString());
+    protected abstract String getConfigString();
 
-		final BenchmarkDriver referenceTest = getJavaDriver();
-		referenceTest.benchmark();
+    protected abstract BenchmarkDriver getJavaDriver();
 
-		System.out.printf("bm=%-15s, id=%-20s, %s\n", id, "java-reference",
-				referenceTest.getSummary());
+    protected abstract BenchmarkDriver getTornadoDriver(DeviceMapping device);
 
-		final double refElapsed = referenceTest.getElapsed();
+    protected int iterations = 0;
 
-		final OCLDriver oclRuntime = (OCLDriver) TornadoRuntime.runtime.getDriver(0);
-		for (int platformIndex = 0; platformIndex < oclRuntime.getNumPlatforms(); platformIndex++) {
-			for (int deviceIndex = 0; deviceIndex < oclRuntime.getNumDevices(platformIndex); deviceIndex++) {
-				final OCLDeviceMapping device = new OCLDeviceMapping(platformIndex, deviceIndex);
+    public void run() {
+        final String id = getIdString();
 
-				final BenchmarkDriver deviceTest = getTornadoDriver(device);
+        System.out.printf("benchmark=%s, iterations=%d, %s\n", id, iterations,
+                getConfigString());
 
-				deviceTest.benchmark();
+        final BenchmarkDriver referenceTest = getJavaDriver();
+        referenceTest.benchmark();
 
-				System.out.printf("bm=%-15s, id=%-20s, %s, speedup=%.4f\n", id,
-						"opencl-device-" + platformIndex + "-" + deviceIndex,
-						deviceTest.getSummary(), refElapsed / deviceTest.getElapsed());
-			}
-		}
-	}
+        System.out.printf("bm=%-15s, id=%-20s, %s\n", id, "java-reference",
+                referenceTest.getSummary());
 
-        
-        public abstract void parseArgs(String[] args);
-        
-	public static void main(String[] args) {
-            try {
-                final BenchmarkRunner bm = (BenchmarkRunner) Class.forName(args[0]).newInstance();
-                final String[] bmArgs = Arrays.copyOfRange(args, 1, args.length);
-                bm.parseArgs(bmArgs);
-                bm.run();
-            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
-                e.printStackTrace();
-                System.exit(-1);
+        final double refElapsed = referenceTest.getElapsed();
+
+        final Set<Integer> blacklistedPlatforms = new HashSet<Integer>();
+        final Set<Integer> blacklistedDevices = new HashSet<Integer>();
+
+        findBlacklisted(blacklistedPlatforms, "tornado.blacklist.platform");
+        findBlacklisted(blacklistedDevices, "tornado.blacklist.device");
+
+        final OCLDriver oclRuntime = (OCLDriver) TornadoRuntime.runtime.getDriver(0);
+        for (int platformIndex = 0; platformIndex < oclRuntime.getNumPlatforms(); platformIndex++) {
+
+            if (blacklistedPlatforms.contains(platformIndex)) {
+                continue;
             }
-           
-	}
+
+            for (int deviceIndex = 0; deviceIndex < oclRuntime.getNumDevices(platformIndex); deviceIndex++) {
+
+                if (blacklistedDevices.contains(deviceIndex)) {
+                    continue;
+                }
+
+                final OCLDeviceMapping device = new OCLDeviceMapping(platformIndex, deviceIndex);
+
+                final BenchmarkDriver deviceTest = getTornadoDriver(device);
+
+                deviceTest.benchmark();
+
+                System.out.printf("bm=%-15s, id=%-20s, %s, speedup=%.4f\n", id,
+                        "opencl-device-" + platformIndex + "-" + deviceIndex,
+                        deviceTest.getSummary(), refElapsed / deviceTest.getElapsed());
+            }
+        }
+    }
+
+    public abstract void parseArgs(String[] args);
+
+    public static void main(String[] args) {
+        try {
+            final BenchmarkRunner bm = (BenchmarkRunner) Class.forName(args[0]).newInstance();
+            final String[] bmArgs = Arrays.copyOfRange(args, 1, args.length);
+            bm.parseArgs(bmArgs);
+            bm.run();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+    }
+
+    private void findBlacklisted(Set<Integer> blacklist, String property) {
+        final String values = System.getProperty(property, "");
+        if (values.isEmpty()) {
+            return;
+        }
+
+        final String[] ids = values.split(",");
+        for (String id : ids) {
+            final int value = Integer.parseInt(id);
+            blacklist.add(value);
+        }
+    }
 
 }
