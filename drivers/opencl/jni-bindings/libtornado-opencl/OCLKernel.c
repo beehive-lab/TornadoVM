74f52dbeefff95eca5ae0f7f6727b64d51e91e9c
James Clarkson <clarksoj@cs.man.ac.uk>
2016-11-04 10:10:02 +0000

    [DRIVER-OPENCL] Added support for setting kernel args as NULL

--- drivers/opencl/jni-bindings/libtornado-opencl/OCLKernel.c
+++ drivers/opencl/jni-bindings/libtornado-opencl/OCLKernel.c
@@ -14,9 +14,9 @@
  * Signature: (J)V
  */
 JNIEXPORT void JNICALL Java_tornado_drivers_opencl_OCLKernel_clReleaseKernel
-(JNIEnv *env, jclass clazz, jlong kernel_id){
+(JNIEnv *env, jclass clazz, jlong kernel_id) {
     OPENCL_PROLOGUE;
-    OPENCL_SOFT_ERROR("clReleaseKernel",clReleaseKernel((cl_kernel)kernel_id),);
+    OPENCL_SOFT_ERROR("clReleaseKernel", clReleaseKernel((cl_kernel) kernel_id),);
 }
 
 /*
@@ -25,14 +25,15 @@ JNIEXPORT void JNICALL Java_tornado_drivers_opencl_OCLKernel_clReleaseKernel
  * Signature: (JIJ[B)V
  */
 JNIEXPORT void JNICALL Java_tornado_drivers_opencl_OCLKernel_clSetKernelArg
-(JNIEnv *env, jclass clazz, jlong kernel_id, jint index, jlong size, jbyteArray array){
+(JNIEnv *env, jclass clazz, jlong kernel_id, jint index, jlong size, jbyteArray array) {
     OPENCL_PROLOGUE;
-    
-    jbyte *value = (*env)->GetPrimitiveArrayCritical(env, array, NULL);
-   
-    OPENCL_SOFT_ERROR("clSetKernelArg",clSetKernelArg((cl_kernel) kernel_id,(cl_uint) index,(size_t) size, (void*) value),);
-    
-    (*env)->ReleasePrimitiveArrayCritical(env, array, value, 0);
+
+    jbyte *value = (array == NULL) ? NULL : (*env)->GetPrimitiveArrayCritical(env, array, NULL);
+
+    OPENCL_SOFT_ERROR("clSetKernelArg", clSetKernelArg((cl_kernel) kernel_id, (cl_uint) index, (size_t) size, (void*) value),);
+
+    if (value != NULL)
+        (*env)->ReleasePrimitiveArrayCritical(env, array, value, 0);
 }
 
 /*
@@ -41,18 +42,18 @@ JNIEXPORT void JNICALL Java_tornado_drivers_opencl_OCLKernel_clSetKernelArg
  * Signature: (JI[B)V
  */
 JNIEXPORT void JNICALL Java_tornado_drivers_opencl_OCLKernel_clGetKernelInfo
-  (JNIEnv *env, jclass clazz, jlong kernel_id, jint kernel_info, jbyteArray array){
-	 OPENCL_PROLOGUE;
+(JNIEnv *env, jclass clazz, jlong kernel_id, jint kernel_info, jbyteArray array) {
+    OPENCL_PROLOGUE;
 
-	    jbyte *value;
-	    jsize len;
+    jbyte *value;
+    jsize len;
 
-	    value = (*env)->GetPrimitiveArrayCritical(env, array, NULL);
-	    len = (*env)->GetArrayLength(env, array);
+    value = (*env)->GetPrimitiveArrayCritical(env, array, NULL);
+    len = (*env)->GetArrayLength(env, array);
 
-	    size_t return_size = 0;
-	    OPENCL_SOFT_ERROR("clGetKernelInfo",
-	    		clGetKernelInfo((cl_kernel) kernel_id,(cl_kernel_info) kernel_info,len,(void *)value,&return_size),);
+    size_t return_size = 0;
+    OPENCL_SOFT_ERROR("clGetKernelInfo",
+            clGetKernelInfo((cl_kernel) kernel_id, (cl_kernel_info) kernel_info, len, (void *) value, &return_size),);
 
-	    (*env)->ReleasePrimitiveArrayCritical(env, array, value, 0);
+    (*env)->ReleasePrimitiveArrayCritical(env, array, value, 0);
 }
