bd82684fe74e52c55548426d7f7b2103d66a1d71
James Clarkson <clarksoj@cs.man.ac.uk>
2016-11-04 10:47:36 +0000

    [DRIVER-OPENCL] implemented support for vector select element (.) in LIR

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLAddressOps.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLAddressOps.java
@@ -1,6 +1,12 @@
 package tornado.drivers.opencl.graal.lir;
 
+import com.oracle.graal.api.meta.Kind;
+import com.oracle.graal.api.meta.LIRKind;
+import com.oracle.graal.api.meta.PlatformKind;
+import com.oracle.graal.api.meta.Value;
 import static com.oracle.graal.lir.LIRInstruction.OperandFlag.REG;
+import com.oracle.graal.lir.LIRInstructionClass;
+import com.oracle.graal.lir.Opcode;
 import tornado.drivers.opencl.graal.asm.OpenCLAssembler;
 import tornado.drivers.opencl.graal.asm.OpenCLAssemblerConstants;
 import tornado.drivers.opencl.graal.backend.OCLBackend;
@@ -8,163 +14,209 @@ import tornado.drivers.opencl.graal.compiler.OCLCompilationResultBuilder;
 import tornado.drivers.opencl.graal.lir.OCLLIRInstruction.AbstractInstruction;
 import tornado.graal.nodes.vector.VectorKind;
 
-import com.oracle.graal.api.meta.Kind;
-import com.oracle.graal.api.meta.LIRKind;
-import com.oracle.graal.api.meta.PlatformKind;
-import com.oracle.graal.api.meta.Value;
-import com.oracle.graal.lir.LIRInstructionClass;
-import com.oracle.graal.lir.Opcode;
-
 public class OCLAddressOps {
 
-	@Opcode("VMOV")
-	public static class OCLVectorElement extends AbstractInstruction implements Value {
-		public static final LIRInstructionClass<OCLVectorElement>	TYPE	= LIRInstructionClass
-																					.create(OCLVectorElement.class);
+    @Opcode("VSEL")
+    public static class OCLVectorElementSelect extends AbstractInstruction implements Value {
 
-		private final Kind											kind;
-		@Use
-		private final Value											vector;
-		private final int											laneId;
+        public static final LIRInstructionClass<OCLVectorElementSelect> TYPE = LIRInstructionClass
+                .create(OCLVectorElementSelect.class);
 
-		public OCLVectorElement(Kind kind, Value vector, int laneId) {
-			super(TYPE);
-			this.kind = kind;
-			this.vector = vector;
-			this.laneId = laneId;
-		}
+        @Use final Value vector;
+        @Use
+        private final Value selection;
+        private final LIRKind lirKind;
 
-		@Override
-		public Kind getKind() {
-			return kind;
-		}
+        public OCLVectorElementSelect(LIRKind lirKind, Value vector,Value selection) {
+            super(TYPE);
+            this.lirKind = lirKind;
+            this.vector = vector;
+            this.selection = selection;
+        }
 
-		@Override
-		public LIRKind getLIRKind() {
-			return LIRKind.value(kind);
-		}
+        @Override
+        public LIRKind getLIRKind() {
+            return lirKind;
+        }
 
-		@Override
-		public PlatformKind getPlatformKind() {
-			return kind;
-		}
+        @Override
+        public PlatformKind getPlatformKind() {
+            return lirKind.getPlatformKind();
+        }
 
-		@Override
-		public void emit(OCLCompilationResultBuilder crb) {
-			final OpenCLAssembler asm = crb.getAssembler();
-			asm.value(crb, vector);
-			asm.emit(".s");
-			asm.emit(Integer.toHexString(laneId).toLowerCase());
-		}
+        @Override
+        public Kind getKind() {
+            return Kind.Illegal;
+        }
 
-	}
+        @Override
+        public void emit(OCLCompilationResultBuilder crb) {
+            OpenCLAssembler asm = crb.getAssembler();
+            asm.value(crb, vector);
+            asm.emitSymbol(".");
+            asm.value(crb, selection);
+        }
 
-	@Deprecated
-	public static class OCLDereference extends AbstractInstruction {
-		public static final LIRInstructionClass<OCLDereference>	TYPE	= LIRInstructionClass
-																				.create(OCLDereference.class);
+    }
 
-		private Kind											kind;
-		@Use(REG)
-		private OCLAddressValue									address;
+    @Opcode("VMOV")
+    public static class OCLVectorElement extends AbstractInstruction implements Value {
 
-		public OCLDereference(Kind kind, OCLAddressValue address) {
-			super(TYPE);
-			this.kind = kind;
-			this.address = address;
-			address.setAccessKind(kind);
-		}
+        public static final LIRInstructionClass<OCLVectorElement> TYPE = LIRInstructionClass
+                .create(OCLVectorElement.class);
 
-		@Override
-		public void emit(OCLCompilationResultBuilder crb) {
-			final OpenCLAssembler asm = crb.getAssembler();
-			asm.emit("*(");
-			address.toAddress().emit(crb);
-			asm.emit(")");
-		}
+        private final OCLKind kind;
+        @Use
+        private final Value vector;
+        private final int laneId;
 
-		@Override
-		public Kind getKind() {
-			return kind;
-		}
+        public OCLVectorElement(OCLKind kind, Value vector, int laneId) {
+            super(TYPE);
+            this.kind = kind;
+            this.vector = vector;
+            this.laneId = laneId;
+        }
 
-		@Override
-		public LIRKind getLIRKind() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+        @Override
+        public Kind getKind() {
+            return Kind.Illegal;
+        }
 
-		@Override
-		public PlatformKind getPlatformKind() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+        @Override
+        public LIRKind getLIRKind() {
+            return LIRKind.value(kind);
+        }
 
-	}
+        @Override
+        public PlatformKind getPlatformKind() {
+            return kind;
+        }
 
-	public static class OCLAddress extends AbstractInstruction {
-		public static final LIRInstructionClass<OCLAddress>	TYPE	= LIRInstructionClass
-																			.create(OCLAddress.class);
-		private PlatformKind								kind;
-		@Use
-		protected Value										base;
-		@Use
-		protected Value										index;
-		private long										displacement;
-		private int											scale;
+        @Override
+        public void emit(OCLCompilationResultBuilder crb) {
+            final OpenCLAssembler asm = crb.getAssembler();
+            asm.value(crb, vector);
+            asm.emit(".s");
+            asm.emit(Integer.toHexString(laneId).toLowerCase());
+        }
 
-		public OCLAddress(PlatformKind kind, Value base, Value index, long displacement, int scale) {
-			super(TYPE);
-			this.kind = kind;
-			this.base = base;
-			this.index = index;
-			this.scale = scale;
-			this.displacement = displacement;
-		}
+    }
 
-		@Override
-		public void emit(OCLCompilationResultBuilder crb) {
-			final OpenCLAssembler asm = crb.getAssembler();
-			asm.emit(toValueString(asm));
-		}
+    @Deprecated
+    public static class OCLDereference extends AbstractInstruction {
 
-		public String toValueString(OpenCLAssembler asm) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(String.format("(%s %s *) (", OpenCLAssemblerConstants.GLOBAL_MEM_MODIFIER,
-					OCLBackend.platformKindToOpenCLKind(kind)));
-			sb.append(asm.toString(base));
-			if (displacement > 0) sb.append(String.format(" + %d", displacement));
+        public static final LIRInstructionClass<OCLDereference> TYPE = LIRInstructionClass
+                .create(OCLDereference.class);
 
-			if (index != Value.ILLEGAL) {
-				sb.append(" + ( ");
-				if (scale != 1) sb.append("(");
-				sb.append(asm.toString(index));
-				sb.append(")");
-				if (scale != 1) sb.append(String.format(" * %d)", scale));
+        private Kind kind;
+        @Use(REG)
+        private OCLAddressValue address;
 
-			}
+        public OCLDereference(Kind kind, OCLAddressValue address) {
+            super(TYPE);
+            this.kind = kind;
+            this.address = address;
+            address.setAccessKind(kind);
+        }
 
-			sb.append(")");
-			return sb.toString();
-		}
+        @Override
+        public void emit(OCLCompilationResultBuilder crb) {
+            final OpenCLAssembler asm = crb.getAssembler();
+            asm.emit("*(");
+            address.toAddress().emit(crb);
+            asm.emit(")");
+        }
 
-		public String toString() {
-			return String
-					.format("address: (%s + %d) + [%s * %d]", base, displacement, index, scale);
-		}
-	}
+        @Override
+        public Kind getKind() {
+            return kind;
+        }
 
-	public static final class OCLVectorAddress extends OCLAddress {
-		public static final LIRInstructionClass<OCLAddress>	TYPE	= LIRInstructionClass
-																			.create(OCLAddress.class);
+        @Override
+        public LIRKind getLIRKind() {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-		public OCLVectorAddress(
-				VectorKind kind,
-				Value base,
-				Value index,
-				long displacement,
-				int scale) {
-			super(kind.getElementKind(), base, index, displacement, scale);
-		}
-	}
+        @Override
+        public PlatformKind getPlatformKind() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
+
+    public static class OCLAddress extends AbstractInstruction {
+
+        public static final LIRInstructionClass<OCLAddress> TYPE = LIRInstructionClass
+                .create(OCLAddress.class);
+        private PlatformKind kind;
+        @Use
+        protected Value base;
+        @Use
+        protected Value index;
+        private long displacement;
+        private int scale;
+
+        public OCLAddress(PlatformKind kind, Value base, Value index, long displacement, int scale) {
+            super(TYPE);
+            this.kind = kind;
+            this.base = base;
+            this.index = index;
+            this.scale = scale;
+            this.displacement = displacement;
+        }
+
+        @Override
+        public void emit(OCLCompilationResultBuilder crb) {
+            final OpenCLAssembler asm = crb.getAssembler();
+            asm.emit(toValueString(asm));
+        }
+
+        public String toValueString(OpenCLAssembler asm) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("(%s %s *) (", OpenCLAssemblerConstants.GLOBAL_MEM_MODIFIER,
+                    OCLBackend.platformKindToOpenCLKind(kind)));
+            sb.append(asm.toString(base));
+            if (displacement > 0) {
+                sb.append(String.format(" + %d", displacement));
+            }
+
+            if (index != Value.ILLEGAL) {
+                sb.append(" + ( ");
+                if (scale != 1) {
+                    sb.append("(");
+                }
+                sb.append(asm.toString(index));
+                sb.append(")");
+                if (scale != 1) {
+                    sb.append(String.format(" * %d)", scale));
+                }
+
+            }
+
+            sb.append(")");
+            return sb.toString();
+        }
+
+        public String toString() {
+            return String
+                    .format("address: (%s + %d) + [%s * %d]", base, displacement, index, scale);
+        }
+    }
+
+    public static final class OCLVectorAddress extends OCLAddress {
+
+        public static final LIRInstructionClass<OCLAddress> TYPE = LIRInstructionClass
+                .create(OCLAddress.class);
+
+        public OCLVectorAddress(
+                VectorKind kind,
+                Value base,
+                Value index,
+                long displacement,
+                int scale) {
+            super(kind.getElementKind(), base, index, displacement, scale);
+        }
+    }
 }
