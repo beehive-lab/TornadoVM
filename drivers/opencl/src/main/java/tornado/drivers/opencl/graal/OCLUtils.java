47b0c4e9db45899445eed3f59b3350c273a94603
James Clarkson <clarksoj@cs.man.ac.uk>
2016-11-04 10:43:25 +0000

    [DRIVER-OPENCL] Implemented naming support for anonymous methods

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLUtils.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLUtils.java
@@ -1,12 +1,19 @@
 package tornado.drivers.opencl.graal;
 
 import com.oracle.graal.api.meta.ResolvedJavaMethod;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class OCLUtils {
+    
+    private static final AtomicInteger id = new AtomicInteger(0);
 
 	public static String makeMethodName(ResolvedJavaMethod method) {
+            if(method != null){
 		final String declaringClass = method.getDeclaringClass().toJavaName().replace(".", "_");
 		return String.format("%s_%s", declaringClass,method.getName());
+            } else {
+                return String.format("unknown_%d",id.incrementAndGet());
+            }
 	}
 
 }
