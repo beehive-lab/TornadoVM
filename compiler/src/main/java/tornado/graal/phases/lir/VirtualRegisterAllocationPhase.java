6a32ca15b99885d52b67da093d27c938ce969854
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-04 16:12:51 +0000

    [COMPILER] Ported phases to new jvmci/graal api

--- compiler/src/main/java/tornado/graal/phases/lir/VirtualRegisterAllocationPhase.java
+++ compiler/src/main/java/tornado/graal/phases/lir/VirtualRegisterAllocationPhase.java
@@ -1,39 +1,16 @@
 package tornado.graal.phases.lir;
 
-import java.util.List;
-
-import tornado.common.Tornado;
-import com.oracle.graal.api.code.TargetDescription;
-import com.oracle.graal.compiler.common.cfg.AbstractBlockBase;
-import com.oracle.graal.lir.LIRInstruction;
 import com.oracle.graal.lir.gen.LIRGenerationResult;
-import com.oracle.graal.lir.gen.LIRGeneratorTool.SpillMoveFactory;
 import com.oracle.graal.lir.phases.AllocationPhase;
+import jdk.vm.ci.code.TargetDescription;
+
+import static tornado.common.exceptions.TornadoInternalError.unimplemented;
 
 public class VirtualRegisterAllocationPhase extends AllocationPhase {
 
-	@Override
-	protected <B extends AbstractBlockBase<B>> void run(TargetDescription target,
-			LIRGenerationResult lirGenRes, List<B> codeEmittingOrder, List<B> linearScanOrder, SpillMoveFactory spillMoveFactory) {
-		
-		Tornado.warn("virtual reg allocation: to implement!!!!");
-		
-		for(B block : linearScanOrder){
-			Tornado.info("virt: block=%d",block.getId());
-			for(LIRInstruction insn : lirGenRes.getLIR().getLIRforBlock(block)){
-				Tornado.info("virt: %s",insn.toString());
-			
-//				if(insn instanceof OCLStmt){
-//					OCLStmt stmt = (OCLStmt) insn;
-//					if(stmt.getResult() instanceof AllocatableValue){
-//						AllocatableValue value = (AllocatableValue) stmt.getResult();
-//						
-//					}
-//				}
-			
-			}
-		}
-		
-	}
+    @Override
+    protected void run(TargetDescription td, LIRGenerationResult lirgr, AllocationContext c) {
+        unimplemented();
+    }
 
 }
