bbd0e120f805da9b1ccb2a708dac0fecae43042c
James Clarkson <clarksoj@cs.man.ac.uk>
2016-11-04 11:04:20 +0000

    [DRIVER-OPENCL] Formatted source code

--- drivers/opencl/src/main/java/tornado/drivers/opencl/runtime/OCLPrebuiltTask.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/runtime/OCLPrebuiltTask.java
@@ -1,16 +1,12 @@
 package tornado.drivers.opencl.runtime;
 
-import static tornado.common.exceptions.TornadoInternalError.guarantee;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import tornado.common.DeviceMapping;
-import tornado.common.Tornado;
 import tornado.common.enums.Access;
-import tornado.common.exceptions.TornadoInternalError;
+import static tornado.common.exceptions.TornadoInternalError.guarantee;
+import static tornado.common.exceptions.TornadoInternalError.shouldNotReachHere;
 import tornado.drivers.opencl.graal.OpenCLInstalledCode;
 import tornado.drivers.opencl.graal.backend.OCLBackend;
 import tornado.meta.domain.DomainTree;
@@ -18,38 +14,38 @@ import tornado.runtime.api.PrebuiltTask;
 
 public class OCLPrebuiltTask extends PrebuiltTask {
 
-	 private OpenCLInstalledCode code;
-	    private OCLBackend backend;
-	
-	protected OCLPrebuiltTask(String entryPoint,String filename, Object[] args,
-			Access[] access, OCLDeviceMapping device, DomainTree domain) {
-		super(entryPoint,filename, args, access, device,domain);
-		
-		backend = device.getBackend();
-	}
-	
-	
-	public void dumpCode() {
-        for (byte b : code.getCode())
+    private OpenCLInstalledCode code;
+    private final OCLBackend backend;
+
+    protected OCLPrebuiltTask(String entryPoint, String filename, Object[] args,
+            Access[] access, OCLDeviceMapping device, DomainTree domain) {
+        super(entryPoint, filename, args, access, device, domain);
+
+        backend = device.getBackend();
+    }
+
+    public void dumpCode() {
+        for (byte b : code.getCode()) {
             System.out.printf("%c", b);
+        }
 
     }
 
-	 public void compile() {
-	        final Path path = Paths.get(filename);
-	        guarantee(path.toFile().exists(), "file does not exist: %s", filename);
-	        try {
-	            final byte[] source = Files.readAllBytes(path);
-	            code = backend.getCodeCache().addMethod(null,entryPoint,
-	                    source);
-	        } catch (IOException e) {
-	            TornadoInternalError.shouldNotReachHere();
-	        }
+    public void compile() {
+        final Path path = Paths.get(filename);
+        guarantee(path.toFile().exists(), "file does not exist: %s", filename);
+        try {
+            final byte[] source = Files.readAllBytes(path);
+            code = backend.getCodeCache().addMethod(null, entryPoint,
+                    source);
+        } catch (IOException e) {
+            shouldNotReachHere();
+        }
 
-	    }
-	 
-	 public OpenCLInstalledCode getCode(){
-		 return code;
-	 }
-	
+    }
+
+    public OpenCLInstalledCode getCode() {
+        return code;
+    }
+
 }
