a4a8ad932d42e27d0db6159938cdd41b84b973df
James Clarkson <clarksoj@cs.man.ac.uk>
2016-11-04 10:04:19 +0000

    [TIDY] Formatted source code

--- collections/src/main/java/tornado/collections/graphics/ImagingOps.java
+++ collections/src/main/java/tornado/collections/graphics/ImagingOps.java
@@ -1,6 +1,5 @@
 package tornado.collections.graphics;
 
-
 import tornado.api.Parallel;
 import tornado.api.Read;
 import tornado.api.Write;
@@ -13,13 +12,11 @@ import tornado.collections.types.ImageFloat;
 public class ImagingOps {
 
     public static final void resizeImage6(@Write ImageFloat dest,
-             @Read ImageFloat src, @Read  int scaleFactor,@Read  float eDelta,
-            @Read  int radius) {
+            @Read ImageFloat src, @Read int scaleFactor, @Read float eDelta,
+            @Read int radius) {
 
-        for (@Parallel
-        int y = 0; y < dest.Y(); y++)
-            for (@Parallel
-            int x = 0; x < dest.X(); x++) {
+        for (@Parallel int y = 0; y < dest.Y(); y++) {
+            for (@Parallel int x = 0; x < dest.X(); x++) {
 
                 // co-ords of center pixel
                 final int cx = TornadoMath.clamp(scaleFactor * x, 0,
@@ -32,7 +29,7 @@ public class ImagingOps {
                 final float center = src.get(cx, cy);
 
                 // calculate new pixel value from values of surrounding pixels
-                for (int yy = -radius + 1; yy <= radius; yy++)
+                for (int yy = -radius + 1; yy <= radius; yy++) {
                     for (int xx = -radius + 1; xx <= radius; xx++) {
 
                         // co-ords of supporting pixel
@@ -47,18 +44,18 @@ public class ImagingOps {
                             t += current;
                         }
                     }
+                }
 
                 final float value = (sum > 0f) ? t / sum : 0f;
                 dest.set(x, y, value);
             }
+        }
     }
 
     public static final void mm2metersKernel(@Write ImageFloat dest,
             @Read ImageFloat src, @Read int scaleFactor) {
-        for (@Parallel
-        int y = 0; y < dest.Y(); y++)
-            for (@Parallel
-            int x = 0; x < dest.X(); x++) {
+        for (@Parallel int y = 0; y < dest.Y(); y++) {
+            for (@Parallel int x = 0; x < dest.X(); x++) {
 
                 // co-ords of center pixel
                 final int sx = scaleFactor * x;
@@ -68,33 +65,30 @@ public class ImagingOps {
 
                 dest.set(x, y, value);
             }
+        }
     }
 
     public static final void resizeImage(@Write ImageFloat dest,
             @Read ImageFloat src, @Read int scaleFactor) {
-
-        for (@Parallel
-        int y = 0; y < dest.Y(); y++)
-            for (@Parallel
-            int x = 0; x < dest.X(); x++) {
+        
+        for (@Parallel int y = 0; y < dest.Y(); y++) {
+            for (@Parallel int x = 0; x < dest.X(); x++) {
 
                 // co-ords of center pixel
                 int cx = TornadoMath.clamp(scaleFactor * x, 0, src.X() - 1);
                 int cy = TornadoMath.clamp(scaleFactor * y, 0, src.Y() - 1);
 
                 float center = src.get(cx, cy);
-
                 dest.set(x, y, center);
             }
+        }
     }
 
     public static final void resizeImage(@Write ImageByte3 dest,
-             @Read ImageByte3 src, @Read int scaleFactor) {
+            @Read ImageByte3 src, @Read int scaleFactor) {
 
-        for (@Parallel
-        int y = 0; y < dest.Y(); y++)
-            for (@Parallel
-            int x = 0; x < dest.X(); x++) {
+        for (@Parallel int y = 0; y < dest.Y(); y++) {
+            for (@Parallel int x = 0; x < dest.X(); x++) {
 
                 // co-ords of center pixel
                 int cx = TornadoMath.clamp(scaleFactor * x, 0, src.X() - 1);
@@ -104,19 +98,18 @@ public class ImagingOps {
 
                 dest.set(x, y, center);
             }
+        }
     }
 
-    public static final void bilateralFilter( @Write ImageFloat dest,
-             @Read ImageFloat src, @Read float[] gaussian, @Read float eDelta,
+    public static final void bilateralFilter(@Write ImageFloat dest,
+            @Read ImageFloat src, @Read float[] gaussian, @Read float eDelta,
             @Read int radius) {
 
         final float e_d_squared_2 = eDelta * eDelta * 2f;
 
         // for every point
-        for (@Parallel
-        int y = 0; y < src.Y(); y++)
-            for (@Parallel
-            int x = 0; x < src.X(); x++) {
+        for (@Parallel int y = 0; y < src.Y(); y++) {
+            for (@Parallel int x = 0; x < src.X(); x++) {
 
                 final float center = src.get(x, y);
 
@@ -125,7 +118,7 @@ public class ImagingOps {
                     float sum = 0f;
                     float t = 0f;
 
-                    for (int yy = -radius; yy <= radius; yy++)
+                    for (int yy = -radius; yy <= radius; yy++) {
                         for (int xx = -radius; xx <= radius; xx++) {
                             final int px = TornadoMath.clamp(x + xx, 0,
                                     src.X() - 1);
@@ -147,12 +140,14 @@ public class ImagingOps {
                                 sum += factor;
                             }
                         }
+                    }
 
                     dest.set(x, y, t / sum);
                 } else {
                     dest.set(x, y, 0f);
                 }
             }
+        }
 
     }
 
