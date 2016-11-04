603976219704ba197c9fee90a573ae6c99da745f
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-04 17:51:55 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLStamp.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLStamp.java
@@ -1,96 +1,26 @@
 package tornado.drivers.opencl.graal;
 
-import com.oracle.graal.api.meta.Constant;
-import com.oracle.graal.api.meta.Kind;
-import com.oracle.graal.api.meta.LIRKind;
-import com.oracle.graal.api.meta.MemoryAccessProvider;
-import com.oracle.graal.api.meta.MetaAccessProvider;
-import com.oracle.graal.api.meta.ResolvedJavaType;
+import com.oracle.graal.compiler.common.LIRKind;
 import com.oracle.graal.compiler.common.spi.LIRKindTool;
 import com.oracle.graal.compiler.common.type.AbstractPointerStamp;
 import com.oracle.graal.compiler.common.type.Stamp;
+import jdk.vm.ci.meta.*;
+import tornado.drivers.opencl.graal.lir.OCLKind;
+
 import static tornado.common.exceptions.TornadoInternalError.shouldNotReachHere;
 import static tornado.common.exceptions.TornadoInternalError.unimplemented;
-import tornado.drivers.opencl.graal.lir.OCLKind;
 
 public class OCLStamp extends AbstractPointerStamp {
 
     private OCLKind oclKind;
-    
-    public OCLStamp(OCLKind lirKind){
-        super(true,false);
+
+    public OCLStamp(OCLKind lirKind) {
+        super(true, false);
         this.oclKind = lirKind;
     }
-    
-    @Override
-    public ResolvedJavaType javaType(MetaAccessProvider metaAccess) {
-        if(oclKind.getJavaClass() != null){
-            return metaAccess.lookupJavaType(oclKind.getJavaClass());
-        }
-        shouldNotReachHere();
-        
-        return null;
-    }
-    
-    public OCLKind getOCLKind(){
-        return oclKind;
-    }
 
     @Override
-    public Kind getStackKind() {
-        if(oclKind.isPrimitive()){
-            switch(oclKind){
-                case BOOL:
-                    return Kind.Boolean;
-                case CHAR:
-                case UCHAR:
-                    return Kind.Byte;
-                case SHORT:
-                case USHORT:
-                    return Kind.Short;
-                case UINT:
-                case INT:
-                    return Kind.Int;
-                case LONG:
-                case ULONG:
-                    return Kind.Long;
-                case FLOAT:
-                    return Kind.Float;
-                case DOUBLE:
-                    return Kind.Double;
-                default:
-                    return Kind.Illegal;
-            }
-        } else if(oclKind.isVector()){
-           // shouldNotReachHere();
-            return Kind.Illegal;
-        } 
-        return Kind.Illegal;
-    }
-
-    @Override
-    public LIRKind getLIRKind(LIRKindTool lirKindTool) {
-        return LIRKind.value(oclKind);
-    }
-
-    @Override
-    public Stamp meet(Stamp stamp) {
-        shouldNotReachHere();
-        return this;
-    }
-
-    @Override
-    public Stamp join(Stamp stamp) {
-        if(stamp instanceof OCLStamp && ((OCLStamp)stamp).oclKind == oclKind){
-            return this;
-        }
-        
-        unimplemented("join: %s + %s",this,stamp);
-        return this;
-    }
-
-    @Override
-    public Stamp unrestricted() {
+    public Stamp constant(Constant cnstnt, MetaAccessProvider map) {
         shouldNotReachHere();
         return this;
     }
@@ -102,15 +32,43 @@ public class OCLStamp extends AbstractPointerStamp {
     }
 
     @Override
-    public Stamp constant(Constant cnstnt, MetaAccessProvider map) {
-        shouldNotReachHere();
-        return this;
+    public LIRKind getLIRKind(LIRKindTool lirKindTool) {
+        return LIRKind.value(oclKind);
+    }
+
+    public OCLKind getOCLKind() {
+        return oclKind;
     }
 
     @Override
-    public boolean isCompatible(Stamp stamp) {
-        shouldNotReachHere();
-        return false;
+    public JavaKind getStackKind() {
+        if (oclKind.isPrimitive()) {
+            switch (oclKind) {
+                case BOOL:
+                    return JavaKind.Boolean;
+                case CHAR:
+                case UCHAR:
+                    return JavaKind.Byte;
+                case SHORT:
+                case USHORT:
+                    return JavaKind.Short;
+                case UINT:
+                case INT:
+                    return JavaKind.Int;
+                case LONG:
+                case ULONG:
+                    return JavaKind.Long;
+                case FLOAT:
+                    return JavaKind.Float;
+                case DOUBLE:
+                    return JavaKind.Double;
+                default:
+                    return JavaKind.Illegal;
+            }
+        } else if (oclKind.isVector()) {
+            return JavaKind.Illegal;
+        }
+        return JavaKind.Illegal;
     }
 
     @Override
@@ -120,20 +78,65 @@ public class OCLStamp extends AbstractPointerStamp {
     }
 
     @Override
+    public Stamp improveWith(Stamp stamp) {
+        shouldNotReachHere();
+        return this;
+    }
+
+    @Override
+    public boolean isCompatible(Constant constant) {
+
+        shouldNotReachHere();
+        return false;
+    }
+
+    @Override
+    public boolean isCompatible(Stamp stamp) {
+        shouldNotReachHere();
+        return false;
+    }
+
+    @Override
+    public ResolvedJavaType javaType(MetaAccessProvider metaAccess) {
+        if (oclKind.getJavaClass() != null) {
+            return metaAccess.lookupJavaType(oclKind.getJavaClass());
+        }
+        shouldNotReachHere();
+
+        return null;
+    }
+
+    @Override
+    public Stamp join(Stamp stamp) {
+        if (stamp instanceof OCLStamp && ((OCLStamp) stamp).oclKind == oclKind) {
+            return this;
+        }
+
+        unimplemented("join: %s + %s", this, stamp);
+        return this;
+    }
+
+    @Override
+    public Stamp meet(Stamp stamp) {
+        shouldNotReachHere();
+        return this;
+    }
+
+    @Override
     public Constant readConstant(MemoryAccessProvider metaAccess, Constant constant, long displacment) {
         shouldNotReachHere();
         return null;
     }
 
     @Override
-    public Stamp improveWith(Stamp stamp) {
+    public String toString() {
+        return "ocl: " + oclKind.name();
+    }
+
+    @Override
+    public Stamp unrestricted() {
         shouldNotReachHere();
         return this;
-    }
-    
-    @Override
-    public String toString(){
-        return "ocl: " + oclKind.name();
     }
 
     @Override
