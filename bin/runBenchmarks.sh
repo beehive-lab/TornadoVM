4e887f73e9021c6f37a9487f88c3c545c9041f9c
James Clarkson <clarksoj@cs.man.ac.uk>
2016-11-04 11:11:36 +0000

    [TOOLS] updated runBenchmarks to output results into timestamped dir

--- bin/runBenchmarks.sh
+++ bin/runBenchmarks.sh
@@ -5,16 +5,26 @@ BENCHMARKS="sadd saxpy sgemm addvector dotvector rotatevector rotateimage convol
 MAIN_CLASS="Benchmark"
 
 TORNADO_CMD="tornado"
+DATE=$(date '+%Y-%m-%d-%H:%M')
 
-LOGFILE="bm.log"
+RESULTS_ROOT="./results"
+BM_ROOT="${RESULTS_ROOT}/${DATE}"
+
+if [ ! -d ${BM_ROOT} ]; then
+  mkdir -p ${BM_ROOT}
+fi
+
+LOGFILE="${BM_ROOT}/bm"
 
 ITERATIONS=10
 
-rm ${LOGFILE}
+if [ ! -z "${TORNADO_FLAGS}" ];then
+  echo ${TORNADO_FLAGS} > "${BM_ROOT}/tornado.flags"
+fi
 
 for bm in ${BENCHMARKS}; do
 	for (( i=0; i<${ITERATIONS}; i++ )); do
 		echo "running ${i} ${bm} ..."
-		${TORNADO_CMD} ${PACKAGE}.${bm}.${MAIN_CLASS} >> ${LOGFILE}
+		${TORNADO_CMD} ${TORNADO_FLAGS} tornado.benchmarks.BenchmarkRunner ${PACKAGE}.${bm}.${MAIN_CLASS} >> "${LOGFILE}-${bm}-${i}.log"
 	done
 done
