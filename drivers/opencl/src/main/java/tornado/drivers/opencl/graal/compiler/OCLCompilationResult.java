9274ccc552cbefaa9af74e70411bd7aeafd9f10b
James Clarkson <clarksoj@cs.man.ac.uk>
2016-11-04 10:24:45 +0000

    [COMPILER] Formatted source code

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/compiler/OCLCompilationResult.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/compiler/OCLCompilationResult.java
@@ -1,39 +1,38 @@
 package tornado.drivers.opencl.graal.compiler;
 
+import com.oracle.graal.api.code.CompilationResult;
+import com.oracle.graal.api.meta.ResolvedJavaMethod;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Set;
 
-import com.oracle.graal.api.code.CompilationResult;
-import com.oracle.graal.api.meta.ResolvedJavaMethod;
-
 public class OCLCompilationResult extends CompilationResult {
 
-	protected Set<ResolvedJavaMethod> nonInlinedMethods;
-	
-	public OCLCompilationResult(){
-		
-	}
-	
-	public Set<ResolvedJavaMethod> getNonInlinedMethods(){
-		return nonInlinedMethods;
-	}
+    protected Set<ResolvedJavaMethod> nonInlinedMethods;
 
-	public void setNonInlinedMethods(Set<ResolvedJavaMethod> value) {
-		nonInlinedMethods = value;
-	}
-	
-	public void addCompiledMethodCode(byte[] code){
-		final byte[] oldCode = getTargetCode();
-		final int size = oldCode.length + code.length + 1;
-		
-		final byte[] newCode = new byte[size];
-		Arrays.fill(newCode, (byte) 0);
-		
-		final ByteBuffer buffer = ByteBuffer.wrap(newCode);
-		buffer.put(code);
-		buffer.put((byte) '\n');
-		buffer.put(oldCode);
-		setTargetCode(newCode,size);
-	}
+    public OCLCompilationResult(String name) {
+        super(name);
+    }
+
+    public Set<ResolvedJavaMethod> getNonInlinedMethods() {
+        return nonInlinedMethods;
+    }
+
+    public void setNonInlinedMethods(Set<ResolvedJavaMethod> value) {
+        nonInlinedMethods = value;
+    }
+
+    public void addCompiledMethodCode(byte[] code) {
+        final byte[] oldCode = getTargetCode();
+        final int size = oldCode.length + code.length + 1;
+
+        final byte[] newCode = new byte[size];
+        Arrays.fill(newCode, (byte) 0);
+
+        final ByteBuffer buffer = ByteBuffer.wrap(newCode);
+        buffer.put(code);
+        buffer.put((byte) '\n');
+        buffer.put(oldCode);
+        setTargetCode(newCode, size);
+    }
 }
