409120176ed183cfe0794e183218a131a112eb4c
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-06 11:50:43 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLEmitable.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLEmitable.java
@@ -1,11 +1,30 @@
 package tornado.drivers.opencl.graal.lir;
 
+import com.oracle.graal.compiler.common.LIRKind;
+import jdk.vm.ci.meta.PlatformKind;
+import tornado.drivers.opencl.graal.asm.OCLAssembler;
 import tornado.drivers.opencl.graal.compiler.OCLCompilationResultBuilder;
 
-import com.oracle.graal.api.meta.Value;
+public abstract class OCLEmitable {
 
-public interface OCLEmitable  extends Value {
+    protected LIRKind lirKind;
 
-     public void emit(OCLCompilationResultBuilder crb);
-     
+    public OCLEmitable(LIRKind lirKind) {
+        this.lirKind = lirKind;
+    }
+
+    public final void emit(OCLCompilationResultBuilder crb) {
+        emit(crb, crb.getAssembler());
+    }
+
+    public abstract void emit(OCLCompilationResultBuilder crb, OCLAssembler asm);
+
+    public LIRKind getLIRKind() {
+        return lirKind;
+    }
+
+    public PlatformKind getPlatformKind() {
+        return lirKind.getPlatformKind();
+    }
+
 }
