9b7002be2a25d8915217b3a87f62cb3035041c17
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-06 14:13:58 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLFPBuiltinFunctionLIRGenerator.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLFPBuiltinFunctionLIRGenerator.java
@@ -1,156 +1,152 @@
 package tornado.drivers.opencl.graal.lir;
 
-import com.oracle.graal.api.meta.Value;
+import jdk.vm.ci.meta.Value;
 
 public interface OCLFPBuiltinFunctionLIRGenerator {
 
-	/*
-	 * Unary intrinsics
-	 */
-	
-	Value emitFloatAbs(Value input);
-	
-	Value emitFloatACos(Value input);
+    /*
+     * Unary intrinsics
+     */
+    Value emitFloatAbs(Value input);
 
-	Value emitFloatACosh(Value input);
+    Value emitFloatACos(Value input);
 
-	Value emitFloatACospi(Value input);
+    Value emitFloatACosh(Value input);
 
-	Value emitFloatASin(Value input);
+    Value emitFloatACospi(Value input);
 
-	Value emitFloatASinh(Value input);
+    Value emitFloatASin(Value input);
 
-	Value emitFloatASinpi(Value input);
+    Value emitFloatASinh(Value input);
 
-	Value emitFloatATan(Value input);
+    Value emitFloatASinpi(Value input);
 
-	Value emitFloatATanh(Value input);
+    Value emitFloatATan(Value input);
 
-	Value emitFloatATanpi(Value input);
+    Value emitFloatATanh(Value input);
 
-	Value emitFloatCbrt(Value input);
+    Value emitFloatATanpi(Value input);
 
-	Value emitFloatCeil(Value input);
+    Value emitFloatCbrt(Value input);
 
-	Value emitFloatCos(Value input);
+    Value emitFloatCeil(Value input);
 
-	Value emitFloatCosh(Value input);
+    Value emitFloatCos(Value input);
 
-	Value emitFloatCospi(Value input);
+    Value emitFloatCosh(Value input);
 
-	Value emitFloatErfc(Value input);
+    Value emitFloatCospi(Value input);
 
-	Value emitFloatErf(Value input);
+    Value emitFloatErfc(Value input);
 
-	Value emitFloatExp(Value input);
+    Value emitFloatErf(Value input);
 
-	Value emitFloatExp2(Value input);
+    Value emitFloatExp(Value input);
 
-	Value emitFloatExp10(Value input);
+    Value emitFloatExp2(Value input);
 
-	Value emitFloatExpm1(Value input);
+    Value emitFloatExp10(Value input);
 
-	Value emitFloatFloor(Value input);
+    Value emitFloatExpm1(Value input);
 
-	Value emitFloatILogb(Value input);
+    Value emitFloatFloor(Value input);
 
-	Value emitFloatLGamma(Value input);
+    Value emitFloatILogb(Value input);
 
-	Value emitFloatLog(Value input);
+    Value emitFloatLGamma(Value input);
 
-	Value emitFloatLog2(Value input);
+    Value emitFloatLog(Value input);
 
-	Value emitFloatLog10(Value input);
+    Value emitFloatLog2(Value input);
 
-	Value emitFloatLog1p(Value input);
+    Value emitFloatLog10(Value input);
 
-	Value emitFloatLogb(Value input);
+    Value emitFloatLog1p(Value input);
 
-	Value emitFloatNan(Value input);
+    Value emitFloatLogb(Value input);
 
-	Value emitFloatRint(Value input);
+    Value emitFloatNan(Value input);
 
-	Value emitFloatRound(Value input);
+    Value emitFloatRint(Value input);
 
-	Value emitFloatRSqrt(Value input);
+    Value emitFloatRound(Value input);
 
-	Value emitFloatSin(Value input);
+    Value emitFloatRSqrt(Value input);
 
-	Value emitFloatSinh(Value input);
+    Value emitFloatSin(Value input);
 
-	Value emitFloatSinpi(Value input);
+    Value emitFloatSinh(Value input);
 
-	Value emitFloatSqrt(Value input);
+    Value emitFloatSinpi(Value input);
 
-	Value emitFloatTan(Value input);
+    Value emitFloatSqrt(Value input);
 
-	Value emitFloatTanh(Value input);
+    Value emitFloatTan(Value input);
 
-	Value emitFloatTanpi(Value input);
+    Value emitFloatTanh(Value input);
 
-	Value emitFloatTGamma(Value input);
+    Value emitFloatTanpi(Value input);
 
-	Value emitFloatTrunc(Value input);
+    Value emitFloatTGamma(Value input);
 
-	/*
-	 * Binary intrinsics
-	 */
+    Value emitFloatTrunc(Value input);
 
-	Value emitFloatATan2(Value input1, Value input2);
+    /*
+     * Binary intrinsics
+     */
+    Value emitFloatATan2(Value input1, Value input2);
 
-	Value emitFloatATan2pi(Value input1, Value input2);
+    Value emitFloatATan2pi(Value input1, Value input2);
 
-	Value emitFloatCopySign(Value input1, Value input2);
+    Value emitFloatCopySign(Value input1, Value input2);
 
-	Value emitFloatDim(Value input1, Value input2);
+    Value emitFloatDim(Value input1, Value input2);
 
-	Value emitFloatFma(Value input1, Value input2);
+    Value emitFloatFma(Value input1, Value input2);
 
-	Value emitFloatMax(Value input1, Value input2);
+    Value emitFloatMax(Value input1, Value input2);
 
-	Value emitFloatMin(Value input1, Value input2);
+    Value emitFloatMin(Value input1, Value input2);
 
-	Value emitFloatMod(Value input1, Value input2);
+    Value emitFloatMod(Value input1, Value input2);
 
-	Value emitFloatFract(Value input1, Value input2);
+    Value emitFloatFract(Value input1, Value input2);
 
-	Value emitFloatFrexp(Value input1, Value input2);
+    Value emitFloatFrexp(Value input1, Value input2);
 
-	Value emitFloatHypot(Value input1, Value input2);
+    Value emitFloatHypot(Value input1, Value input2);
 
-	Value emitFloatLdexp(Value input1, Value input2);
+    Value emitFloatLdexp(Value input1, Value input2);
 
-	Value emitFloatMad(Value input1, Value input2);
+    Value emitFloatMad(Value input1, Value input2);
 
-	Value emitFloatMaxmag(Value input1, Value input2);
+    Value emitFloatMaxmag(Value input1, Value input2);
 
-	Value emitFloatMinmag(Value input1, Value input2);
+    Value emitFloatMinmag(Value input1, Value input2);
 
-	Value emitFloatModf(Value input1, Value input2);
+    Value emitFloatModf(Value input1, Value input2);
 
-	Value emitFloatNextAfter(Value input1, Value input2);
+    Value emitFloatNextAfter(Value input1, Value input2);
 
-	Value emitFloatPow(Value input1, Value input2);
+    Value emitFloatPow(Value input1, Value input2);
 
-	Value emitFloatPown(Value input1, Value input2);
+    Value emitFloatPown(Value input1, Value input2);
 
-	Value emitFloatPowr(Value input1, Value input2);
+    Value emitFloatPowr(Value input1, Value input2);
 
-	Value emitFloatRemainder(Value input1, Value input2);
+    Value emitFloatRemainder(Value input1, Value input2);
 
-	Value emitFloatRootn(Value input1, Value input2);
+    Value emitFloatRootn(Value input1, Value input2);
 
-	Value emitFloatSincos(Value input1, Value input2);
-	
-	/*
-	 * Ternary intrinsics
-	 */
+    Value emitFloatSincos(Value input1, Value input2);
 
-	Value emitFloatFMA(Value x, Value y, Value z);
+    /*
+     * Ternary intrinsics
+     */
+    Value emitFloatFMA(Value x, Value y, Value z);
 
-	Value emitFloatMAD(Value x, Value y, Value z);
+    Value emitFloatMAD(Value x, Value y, Value z);
 
-	Value emitFloatRemquo(Value x, Value y, Value z);
+    Value emitFloatRemquo(Value x, Value y, Value z);
 
-	
 }
