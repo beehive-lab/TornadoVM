862b7d4c0dfd5f3d0600d907b666fef4609f6eb7
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-06 14:10:40 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLReturnSlot.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLReturnSlot.java
@@ -1,19 +1,29 @@
 package tornado.drivers.opencl.graal.lir;
 
-import com.oracle.graal.api.meta.AllocatableValue;
-import com.oracle.graal.api.meta.LIRKind;
+import com.oracle.graal.compiler.common.LIRKind;
 import com.oracle.graal.lir.Opcode;
+import tornado.drivers.opencl.graal.asm.OCLAssembler;
+import tornado.drivers.opencl.graal.asm.OCLAssembler.OCLNullaryOp;
+import tornado.drivers.opencl.graal.compiler.OCLCompilationResultBuilder;
 
-@Opcode("RETURN")
-public class OCLReturnSlot extends AllocatableValue {
+@Opcode("RETURN VALUE")
+public class OCLReturnSlot extends OCLEmitable {
+
+    private OCLNullaryOp op;
 
     public OCLReturnSlot(LIRKind lirKind) {
         super(lirKind);
+        op = OCLNullaryOp.SLOTS_BASE_ADDRESS;
+    }
+
+    @Override
+    public void emit(OCLCompilationResultBuilder crb, OCLAssembler asm) {
+        op.emit(crb);
     }
 
     @Override
     public String toString() {
-        return "RETURN_SLOT";
+        return op.toString();
     }
 
 }
