5acb69ccf9b32f3c81860f2d5a03f139203a8386
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-06 17:54:08 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/compiler/OCLHighTier.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/compiler/OCLHighTier.java
@@ -1,33 +1,26 @@
 package tornado.drivers.opencl.graal.compiler;
 
-import static com.oracle.graal.compiler.common.GraalOptions.*;
-import static com.oracle.graal.compiler.phases.HighTier.Options.*;
-import static com.oracle.graal.phases.common.DeadCodeEliminationPhase.Optionality.*;
-import tornado.drivers.opencl.graal.phases.TornadoInvokeCleanup;
-import tornado.drivers.opencl.graal.phases.TornadoParallelScheduler;
-import tornado.drivers.opencl.graal.phases.TornadoParameterCleanup;
-import tornado.drivers.opencl.graal.phases.TornadoTaskSpecialisation;
-import tornado.drivers.opencl.graal.phases.TornadoVectorResolver;
-import tornado.graal.compiler.TornadoHighTier;
-import tornado.graal.phases.ExceptionSuppression;
-import tornado.graal.phases.TornadoApiReplacement;
-import tornado.graal.phases.TornadoInliningPolicy;
-import tornado.graal.phases.TornadoShapeAnalysis;
-import tornado.graal.phases.TornadoValueTypeCleanup;
-
-import com.oracle.graal.nodes.spi.*;
-import com.oracle.graal.phases.common.*;
+import com.oracle.graal.nodes.spi.LoweringTool;
 import com.oracle.graal.phases.common.CanonicalizerPhase.CustomCanonicalizer;
-import com.oracle.graal.phases.common.inlining.*;
-import com.oracle.graal.virtual.phases.ea.*;
+import com.oracle.graal.phases.common.*;
+import com.oracle.graal.phases.common.inlining.InliningPhase;
+import com.oracle.graal.virtual.phases.ea.PartialEscapePhase;
+import tornado.drivers.opencl.graal.phases.TornadoTaskSpecialisation;
+import tornado.drivers.opencl.graal.phases.*;
+import tornado.graal.compiler.TornadoHighTier;
+import tornado.graal.phases.*;
+
+import static com.oracle.graal.compiler.common.GraalOptions.*;
+import static com.oracle.graal.compiler.phases.HighTier.Options.Inline;
+import static com.oracle.graal.phases.common.DeadCodeEliminationPhase.Optionality.Optional;
 
 public class OCLHighTier extends TornadoHighTier {
-	
+
     public OCLHighTier(CustomCanonicalizer customCanonicalizer) {
-    	super(customCanonicalizer);
-    	
-    	 final CanonicalizerPhase canonicalizer = new CanonicalizerPhase(customCanonicalizer);
-    	
+        super(customCanonicalizer);
+
+        final CanonicalizerPhase canonicalizer = new CanonicalizerPhase(customCanonicalizer);
+
         if (ImmutableCode.getValue()) {
             canonicalizer.disableReadCanonicalization();
         }
@@ -35,11 +28,11 @@ public class OCLHighTier extends TornadoHighTier {
         if (OptCanonicalizer.getValue()) {
             appendPhase(canonicalizer);
         }
-        
+
         if (Inline.getValue()) {
             appendPhase(new InliningPhase(new TornadoInliningPolicy(), canonicalizer));
             appendPhase(new TornadoInvokeCleanup());
-            
+
             appendPhase(new DeadCodeEliminationPhase(Optional));
 
             if (ConditionalElimination.getValue() && OptCanonicalizer.getValue()) {
@@ -47,59 +40,51 @@ public class OCLHighTier extends TornadoHighTier {
                 appendPhase(new IterativeConditionalEliminationPhase(canonicalizer, false));
             }
         }
-        
+
         appendPhase(new TornadoParameterCleanup());
         appendPhase(new TornadoApiReplacement());
-       
+
         appendPhase(new TornadoTaskSpecialisation(canonicalizer));
         appendPhase(new TornadoVectorResolver());
         appendPhase(canonicalizer);
         appendPhase(new DeadCodeEliminationPhase(Optional));
 
-        appendPhase(new CleanTypeProfileProxyPhase(canonicalizer));
-
+//        appendPhase(new CleanTypeProfileProxyPhase(canonicalizer));
 //        if (FullUnroll.getValue()) {
 //            appendPhase(new LoopFullUnrollPhase(canonicalizer));
 //        }
-        
 //        appendPhase(new TornadoVectorization());
 //        appendPhase(new TornadoVectorizationSimplification());
-        
         appendPhase(canonicalizer);
-        
+
         if (PartialEscapeAnalysis.getValue()) {
             appendPhase(new PartialEscapePhase(true, canonicalizer));
         }
         appendPhase(new TornadoValueTypeCleanup());
-     
 
         if (OptConvertDeoptsToGuards.getValue()) {
             appendPhase(new ConvertDeoptimizeToGuardPhase());
         }
 
-        
         if (OptLoopTransform.getValue()) {
 //            if (LoopPeeling.getValue()) {
 //                appendPhase(new LoopPeelingPhase());
 //            }
-        	
+
 //            if (LoopUnswitch.getValue()) {
 //                appendPhase(new LoopUnswitchingPhase());
 //            }
         }
         appendPhase(new RemoveValueProxyPhase());
 
-        
         appendPhase(new TornadoShapeAnalysis());
         appendPhase(new TornadoParallelScheduler());
-       
-        appendPhase(canonicalizer);
 
+        appendPhase(canonicalizer);
 
         appendPhase(new LoweringPhase(canonicalizer, LoweringTool.StandardLoweringStage.HIGH_TIER));
         appendPhase(new ExceptionSuppression());
-       
-       // appendPhase(new TornadoOopsRemoval());
-       
+
+        // appendPhase(new TornadoOopsRemoval());
     }
 }
