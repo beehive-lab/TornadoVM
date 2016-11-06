1daa4fadbc3c64f8cbafb5a907c6039cb43ef810
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-06 17:57:56 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- compiler/src/main/java/tornado/graal/compiler/TornadoCompilerConfiguration.java
+++ compiler/src/main/java/tornado/graal/compiler/TornadoCompilerConfiguration.java
@@ -3,14 +3,13 @@ package tornado.graal.compiler;
 import com.oracle.graal.lir.phases.LIRPhaseSuite;
 import com.oracle.graal.lir.phases.PostAllocationOptimizationPhase.PostAllocationOptimizationContext;
 import com.oracle.graal.lir.phases.PreAllocationOptimizationPhase.PreAllocationOptimizationContext;
-import com.oracle.graal.phases.common.CanonicalizerPhase.CustomCanonicalizer;
 import tornado.graal.phases.lir.TornadoAllocationStage;
 
 public interface TornadoCompilerConfiguration {
 
     public TornadoAllocationStage createAllocationStage();
 
-    public TornadoHighTier createHighTier(CustomCanonicalizer canonicalizer);
+    public TornadoHighTier createHighTier();
 
     public TornadoLowTier createLowTier();
 
