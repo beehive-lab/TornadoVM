082534e1e31d017f6b67e4b8f944d5a847b5a874
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-07 16:19:25 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLProviders.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLProviders.java
@@ -1,40 +1,35 @@
 package tornado.drivers.opencl.graal;
 
-import com.oracle.graal.graphbuilderconf.GraphBuilderConfiguration.Plugins;
-import com.oracle.graal.hotspot.HotSpotGraalRuntimeProvider;
+import com.oracle.graal.hotspot.meta.HotSpotStampProvider;
+import com.oracle.graal.nodes.graphbuilderconf.GraphBuilderConfiguration.Plugins;
 import com.oracle.graal.nodes.spi.LoweringProvider;
-import com.oracle.graal.phases.tiers.SuitesProvider;
 import com.oracle.graal.phases.util.Providers;
+import jdk.vm.ci.runtime.JVMCIBackend;
+import tornado.graal.compiler.TornadoCompilerConfiguration;
+import tornado.graal.compiler.TornadoConstantFieldProvider;
+import tornado.graal.compiler.TornadoForeignCallsProvider;
+import tornado.graal.compiler.TornadoNodeCostProvider;
 
 public class OCLProviders extends Providers {
-	
-	private final HotSpotGraalRuntimeProvider runtime;
-	private final OCLSuitesProvider suites;
-	private final Plugins graphBuilderPlugins;
 
-	public OCLProviders(HotSpotGraalRuntimeProvider vmRuntime,
-			Providers vmProviders, SuitesProvider suitesProvider, Plugins graphBuilderPlugins, LoweringProvider lowerer, OpenCLCodeCache codeCache) {
-		super(vmProviders.getMetaAccess(),codeCache, vmProviders.getConstantReflection(), vmProviders.getForeignCalls(), lowerer,
-				vmProviders.getReplacements(), vmProviders.getStampProvider());
-		this.runtime = vmRuntime;
-		this.suites = new OCLSuitesProvider(graphBuilderPlugins,vmProviders.getMetaAccess());
-		
-		this.graphBuilderPlugins = graphBuilderPlugins;
-	}
-	
-	public OCLSuitesProvider getSuitesProvider() {
+    private final OCLSuitesProvider suites;
+    private final Plugins graphBuilderPlugins;
+
+    public OCLProviders(TornadoCompilerConfiguration compilerConfig, JVMCIBackend jvmciBackend,
+            Plugins graphBuilderPlugins, LoweringProvider lowerer, OpenCLCodeCache codeCache) {
+        super(jvmciBackend.getMetaAccess(), codeCache, jvmciBackend.getConstantReflection(), new TornadoConstantFieldProvider(), new TornadoForeignCallsProvider(), lowerer,
+                null, new HotSpotStampProvider(), new TornadoNodeCostProvider());
+
+        this.suites = new OCLSuitesProvider(graphBuilderPlugins, jvmciBackend.getMetaAccess());
+
+        this.graphBuilderPlugins = graphBuilderPlugins;
+    }
+
+    public OCLSuitesProvider getSuitesProvider() {
         return suites;
     }
 
     public Plugins getGraphBuilderPlugins() {
         return graphBuilderPlugins;
     }
-    
-    public HotSpotGraalRuntimeProvider getRuntime(){
-    	return runtime;
-    }
-    
-    
-
-
 }
