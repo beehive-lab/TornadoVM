c11387530abcb578b91fe4c673f0b6749cc2a31c
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-07 17:59:57 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/phases/TornadoInvokeCleanup.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/phases/TornadoInvokeCleanup.java
@@ -1,60 +1,30 @@
 package tornado.drivers.opencl.graal.phases;
 
-import com.oracle.graal.api.meta.ResolvedJavaType;
-import com.oracle.graal.nodes.AbstractBeginNode;
-import com.oracle.graal.nodes.BeginNode;
-import com.oracle.graal.nodes.GuardingPiNode;
-import com.oracle.graal.nodes.InvokeWithExceptionNode;
-import com.oracle.graal.nodes.KillingBeginNode;
-import com.oracle.graal.nodes.StructuredGraph;
+import com.oracle.graal.nodes.*;
 import com.oracle.graal.phases.BasePhase;
-import java.util.List;
-import tornado.api.Vector;
-import static tornado.common.exceptions.TornadoInternalError.unimplemented;
 import tornado.graal.phases.TornadoHighTierContext;
 
+@Deprecated
 public class TornadoInvokeCleanup extends BasePhase<TornadoHighTierContext> {
 
-	@Override
-	protected void run(StructuredGraph graph, TornadoHighTierContext context) {
-		graph.getNodes().filter(InvokeWithExceptionNode.class).forEach(invoke -> {
-			//System.out.printf("cleaning: %s\n",invoke);
-			final List<GuardingPiNode> guardingPis = invoke.usages().filter(GuardingPiNode.class).snapshot();
-			
-			if(invoke.exceptionEdge() != null)
-				invoke.killExceptionEdge();
-			
-			AbstractBeginNode begin = invoke.next();
-            if (begin instanceof KillingBeginNode) {
-                AbstractBeginNode newBegin = new BeginNode();
-                graph.addAfterFixed(begin, graph.add(newBegin));
-                begin.replaceAtUsages(newBegin);
-                graph.removeFixed(begin);
-            }
-		
-            if(invoke.getKind().isObject()){
-            	final ResolvedJavaType type = invoke.stamp().javaType(context.getMetaAccess());
-            	
-           
-            	
-            	if(type.getAnnotation(Vector.class)!= null ){
-                    unimplemented();
-//            		final VectorKind vectorKind = VectorKind.fromResolvedJavaType(type);
-//            		final VectorValueNode vector = graph.addOrUnique(new VectorValueNode(type,vectorKind,invoke));
-//            		
-//            		invoke.usages().filter(IsNullNode.class).forEach(isNullNode -> {
-//            			isNullNode.replaceAndDelete(LogicConstantNode.contradiction(graph));
-//            		});
-//            		
-//            		for(GuardingPiNode guardingPi : guardingPis){
-//            			graph.replaceFixedWithFloating((FixedWithNextNode)guardingPi, vector);
-//            		}         		
-            	}
-            	
-            	
-            
-            }           
-		});
-	}
+    @Override
+    protected void run(StructuredGraph graph, TornadoHighTierContext context) {
+        graph.getNodes().filter(InvokeWithExceptionNode.class).forEach(invoke -> {
+            //System.out.printf("cleaning: %s\n",invoke);
+//            final List<GuardingPiNode> guardingPis = invoke.usages().filter(GuardingPiNode.class).snapshot();
+//
+//            if (invoke.exceptionEdge() != null) {
+//                invoke.killExceptionEdge();
+//            }
+//
+//            AbstractBeginNode begin = invoke.next();
+//            if (begin instanceof KillingBeginNode) {
+//                AbstractBeginNode newBegin = new BeginNode();
+//                graph.addAfterFixed(begin, graph.add(newBegin));
+//                begin.replaceAtUsages(newBegin);
+//                graph.removeFixed(begin);
+//            }
+        });
+    }
 
 }
