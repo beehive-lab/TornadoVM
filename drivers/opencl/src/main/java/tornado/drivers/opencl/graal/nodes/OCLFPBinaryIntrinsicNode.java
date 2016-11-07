0c1e5e22091d8872159c3fe53d674466378b171d
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-07 14:36:50 +0000

    [DRIVER-OPENCL] Ported to new graal/jvmci api

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/nodes/OCLFPBinaryIntrinsicNode.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/nodes/OCLFPBinaryIntrinsicNode.java
@@ -1,115 +1,136 @@
 package tornado.drivers.opencl.graal.nodes;
 
-import tornado.common.exceptions.TornadoInternalError;
-
-import com.oracle.graal.api.meta.Kind;
-import com.oracle.graal.api.meta.Value;
-import com.oracle.graal.compiler.common.GraalInternalError;
+import com.oracle.graal.compiler.common.type.Stamp;
 import com.oracle.graal.compiler.common.type.StampFactory;
 import com.oracle.graal.graph.Node;
 import com.oracle.graal.graph.NodeClass;
 import com.oracle.graal.graph.spi.CanonicalizerTool;
-import com.oracle.graal.lir.gen.ArithmeticLIRGenerator;
+import com.oracle.graal.lir.Variable;
+import com.oracle.graal.lir.gen.ArithmeticLIRGeneratorTool;
 import com.oracle.graal.nodeinfo.NodeInfo;
 import com.oracle.graal.nodes.ConstantNode;
 import com.oracle.graal.nodes.ValueNode;
 import com.oracle.graal.nodes.calc.BinaryNode;
 import com.oracle.graal.nodes.spi.ArithmeticLIRLowerable;
-import com.oracle.graal.nodes.spi.NodeMappableLIRBuilder;
+import com.oracle.graal.nodes.spi.NodeLIRBuilderTool;
+import jdk.vm.ci.meta.JavaKind;
+import jdk.vm.ci.meta.Value;
+import tornado.common.exceptions.TornadoInternalError;
+import tornado.drivers.opencl.graal.lir.OCLArithmeticTool;
 import tornado.drivers.opencl.graal.lir.OCLBuiltinTool;
+import tornado.drivers.opencl.graal.lir.OCLLIRInstruction.AssignStmt;
+
+import static tornado.common.exceptions.TornadoInternalError.shouldNotReachHere;
 
 @NodeInfo(nameTemplate = "{p#operation}")
 public class OCLFPBinaryIntrinsicNode extends BinaryNode implements ArithmeticLIRLowerable {
 
-	protected OCLFPBinaryIntrinsicNode(ValueNode x, ValueNode y, Operation op, Kind kind) {
-		super(TYPE, StampFactory.forKind(kind), x, y);
-		this.operation = op;
-	}
+    protected OCLFPBinaryIntrinsicNode(ValueNode x, ValueNode y, Operation op, JavaKind kind) {
+        super(TYPE, StampFactory.forKind(kind), x, y);
+        this.operation = op;
+    }
 
-	public static final NodeClass<OCLFPBinaryIntrinsicNode>	TYPE	= NodeClass
-																			.create(OCLFPBinaryIntrinsicNode.class);
-	protected final Operation								operation;
+    public static final NodeClass<OCLFPBinaryIntrinsicNode> TYPE = NodeClass
+            .create(OCLFPBinaryIntrinsicNode.class);
+    protected final Operation operation;
 
-	public enum Operation {
-		ATAN2, ATAN2PI, COPYSIGN, FDIM, FMA, FMAX, FMIN, FMOD, FRACT, FREXP, HYPOT, LDEXP, MAD, MAXMAG, MINMAG, MODF, NEXTAFTER, POW, POWN, POWR, REMAINDER, REMQUO, ROOTN, SINCOS
-	}
+    public enum Operation {
+        ATAN2, ATAN2PI, COPYSIGN, FDIM, FMA, FMAX, FMIN, FMOD, FRACT, FREXP, HYPOT, LDEXP, MAD, MAXMAG, MINMAG, MODF, NEXTAFTER, POW, POWN, POWR, REMAINDER, REMQUO, ROOTN, SINCOS
+    }
 
-	public Operation operation() {
-		return operation;
-	}
+    @Override
+    public Node canonical(CanonicalizerTool tool) {
+        return canonical(tool, getX(), getY());
+    }
 
-	public static ValueNode create(ValueNode x, ValueNode y, Operation op, Kind kind) {
-		ValueNode c = tryConstantFold(x,y, op, kind);
-		if (c != null) {
-			return c;
-		}
-		return new OCLFPBinaryIntrinsicNode(x, y, op, kind);
-	}
+    @Override
+    public Stamp foldStamp(Stamp stampX, Stamp stampY) {
+        return stampX.join(stampY);
+    }
 
-	protected static ValueNode tryConstantFold(ValueNode x, ValueNode y, Operation op, Kind kind) {
-		ConstantNode result = null;
+    @Override
+    public void generate(NodeLIRBuilderTool builder) {
+        generate(builder, builder.getLIRGeneratorTool().getArithmetic());
+    }
 
-		if (x.isConstant() && y.isConstant()) {
-			if (kind == Kind.Double) {
-				double ret = doCompute(x.asJavaConstant().asDouble(),y.asJavaConstant().asDouble(), op);
-				result = ConstantNode.forDouble(ret);
-			} else if (kind == Kind.Float) {
-				float ret = doCompute(x.asJavaConstant().asFloat(),y.asJavaConstant().asFloat(), op);
-				result = ConstantNode.forFloat((float) ret);
-			}
-		}
-		return result;
-	}
+    public Operation operation() {
+        return operation;
+    }
 
-	@Override
-	public void generate(NodeMappableLIRBuilder builder, ArithmeticLIRGenerator lirGen) {
-		OCLBuiltinTool gen = (OCLBuiltinTool) lirGen;
-		Value x = builder.operand(getX());
-		Value y = builder.operand(getY());
-		Value result;
-		switch (operation()) {
-			case FMIN:
-				result = gen.emitFloatMin(x, y);
-				break;
-			case FMAX:
-				result = gen.emitFloatMax(x, y);
-				break;
-			default:
-				throw TornadoInternalError.shouldNotReachHere();
-		}
-		builder.setResult(this, result);
+    public static ValueNode create(ValueNode x, ValueNode y, Operation op, JavaKind kind) {
+        ValueNode c = tryConstantFold(x, y, op, kind);
+        if (c != null) {
+            return c;
+        }
+        return new OCLFPBinaryIntrinsicNode(x, y, op, kind);
+    }
 
-	}
+    protected static ValueNode tryConstantFold(ValueNode x, ValueNode y, Operation op, JavaKind kind) {
+        ConstantNode result = null;
 
-	private static double doCompute(double x, double y, Operation op) {
-		switch (op) {
-			case FMIN:
-				return Math.min(x,y);
-			case FMAX:
-				return Math.max(x,y);
-			default:
-				throw new TornadoInternalError("unknown op %s", op);
-		}
-	}
+        if (x.isConstant() && y.isConstant()) {
+            if (kind == JavaKind.Double) {
+                double ret = doCompute(x.asJavaConstant().asDouble(), y.asJavaConstant().asDouble(), op);
+                result = ConstantNode.forDouble(ret);
+            } else if (kind == JavaKind.Float) {
+                float ret = doCompute(x.asJavaConstant().asFloat(), y.asJavaConstant().asFloat(), op);
+                result = ConstantNode.forFloat(ret);
+            }
+        }
+        return result;
+    }
 
-	private static float doCompute(float x, float y, Operation op) {
-		switch (op) {
-			case FMIN:
-				return Math.min(x,y);
-			case FMAX:
-				return Math.max(x,y);
-			default:
-				throw new GraalInternalError("unknown op %s", op);
-		}
-	}
+    @Override
+    public void generate(NodeLIRBuilderTool builder, ArithmeticLIRGeneratorTool lirGen) {
+        OCLBuiltinTool gen = ((OCLArithmeticTool) lirGen).getGen().getOCLBuiltinTool();
+        Value x = builder.operand(getX());
+        Value y = builder.operand(getY());
+        Value result;
+        switch (operation()) {
+            case FMIN:
+                result = gen.genFloatMin(x, y);
+                break;
+            case FMAX:
+                result = gen.genFloatMax(x, y);
+                break;
+            default:
+                throw shouldNotReachHere();
+        }
+        Variable var = builder.getLIRGeneratorTool().newVariable(result.getValueKind());
+        builder.getLIRGeneratorTool().append(new AssignStmt(var, result));
+        builder.setResult(this, var);
 
-	@Override
-	public Node canonical(CanonicalizerTool tool, ValueNode x, ValueNode y) {
-		ValueNode c = tryConstantFold(x,y, operation(), getKind());
-		if (c != null) {
-			return c;
-		}
-		return this;
-	}
+    }
+
+    private static double doCompute(double x, double y, Operation op) {
+        switch (op) {
+            case FMIN:
+                return Math.min(x, y);
+            case FMAX:
+                return Math.max(x, y);
+            default:
+                throw new TornadoInternalError("unknown op %s", op);
+        }
+    }
+
+    private static float doCompute(float x, float y, Operation op) {
+        switch (op) {
+            case FMIN:
+                return Math.min(x, y);
+            case FMAX:
+                return Math.max(x, y);
+            default:
+                throw new TornadoInternalError("unknown op %s", op);
+        }
+    }
+
+    @Override
+    public Node canonical(CanonicalizerTool tool, ValueNode x, ValueNode y) {
+        ValueNode c = tryConstantFold(x, y, operation(), getStackKind());
+        if (c != null) {
+            return c;
+        }
+        return this;
+    }
 
 }
--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/nodes/OCLFPUnaryIntrinsicNode.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/nodes/OCLFPUnaryIntrinsicNode.java
@@ -1,56 +1,60 @@
 package tornado.drivers.opencl.graal.nodes;
 
-
-import com.oracle.graal.api.meta.Kind;
-import com.oracle.graal.api.meta.Value;
-import com.oracle.graal.compiler.common.GraalInternalError;
 import com.oracle.graal.compiler.common.type.FloatStamp;
 import com.oracle.graal.compiler.common.type.PrimitiveStamp;
 import com.oracle.graal.compiler.common.type.StampFactory;
 import com.oracle.graal.graph.Node;
 import com.oracle.graal.graph.NodeClass;
 import com.oracle.graal.graph.spi.CanonicalizerTool;
-import com.oracle.graal.lir.gen.ArithmeticLIRGenerator;
+import com.oracle.graal.lir.Variable;
+import com.oracle.graal.lir.gen.ArithmeticLIRGeneratorTool;
 import com.oracle.graal.nodeinfo.NodeInfo;
 import com.oracle.graal.nodes.ConstantNode;
 import com.oracle.graal.nodes.ValueNode;
 import com.oracle.graal.nodes.calc.UnaryNode;
 import com.oracle.graal.nodes.spi.ArithmeticLIRLowerable;
-import com.oracle.graal.nodes.spi.NodeMappableLIRBuilder;
+import com.oracle.graal.nodes.spi.NodeLIRBuilderTool;
+import jdk.vm.ci.meta.JavaKind;
+import jdk.vm.ci.meta.Value;
+import tornado.common.exceptions.TornadoInternalError;
+import tornado.drivers.opencl.graal.lir.OCLArithmeticTool;
 import tornado.drivers.opencl.graal.lir.OCLBuiltinTool;
+import tornado.drivers.opencl.graal.lir.OCLLIRInstruction.AssignStmt;
+
+import static tornado.common.exceptions.TornadoInternalError.shouldNotReachHere;
 
 @NodeInfo(nameTemplate = "{p#operation}")
 public class OCLFPUnaryIntrinsicNode extends UnaryNode implements ArithmeticLIRLowerable {
 
-	protected OCLFPUnaryIntrinsicNode(ValueNode value, Operation op, Kind kind) {
+    protected OCLFPUnaryIntrinsicNode(ValueNode value, Operation op, JavaKind kind) {
         super(TYPE, StampFactory.forKind(kind), value);
         assert value.stamp() instanceof FloatStamp && PrimitiveStamp.getBits(value.stamp()) == kind.getBitCount();
         this.operation = op;
     }
 
-	public static final NodeClass<OCLFPUnaryIntrinsicNode> TYPE = NodeClass.create(OCLFPUnaryIntrinsicNode.class);
+    public static final NodeClass<OCLFPUnaryIntrinsicNode> TYPE = NodeClass.create(OCLFPUnaryIntrinsicNode.class);
     protected final Operation operation;
 
     public enum Operation {
-    	ACOS,
-    	ACOSH,
-    	ACOSPI,
-    	ASIN,
-    	ASINH,
-    	ASINPI,
-    	ATAN,
-    	//ATAN2,
-    	ATANH,
-    	ATANPI,
-    	//ATAN2PI,
-    	CBRT,
-    	CEIL,
-    	//COPYSIGN,
-    	COS,
-    	COSH,
-    	COSPI,
-    	ERFC,
-    	ERF,
+        ACOS,
+        ACOSH,
+        ACOSPI,
+        ASIN,
+        ASINH,
+        ASINPI,
+        ATAN,
+        //ATAN2,
+        ATANH,
+        ATANPI,
+        //ATAN2PI,
+        CBRT,
+        CEIL,
+        //COPYSIGN,
+        COS,
+        COSH,
+        COSPI,
+        ERFC,
+        ERF,
         EXP,
         EXP2,
         EXP10,
@@ -104,98 +108,100 @@ public class OCLFPUnaryIntrinsicNode extends UnaryNode implements ArithmeticLIRL
         return operation;
     }
 
-    public static ValueNode create(ValueNode value, Operation op, Kind kind) {
+    public static ValueNode create(ValueNode value, Operation op, JavaKind kind) {
         ValueNode c = tryConstantFold(value, op, kind);
         if (c != null) {
             return c;
         }
         return new OCLFPUnaryIntrinsicNode(value, op, kind);
     }
-    
-    protected static ValueNode tryConstantFold(ValueNode value, Operation op, Kind kind) {
-       ConstantNode result = null;
-    	
-    	if (value.isConstant()) {
-    		if(kind == Kind.Double){
-            double ret = doCompute(value.asJavaConstant().asDouble(), op);
-            result = ConstantNode.forDouble(ret);
-    		} else if (kind == Kind.Float){
-    			float ret = doCompute(value.asJavaConstant().asFloat(), op);
-                result = ConstantNode.forFloat((float) ret);
-    		}
+
+    protected static ValueNode tryConstantFold(ValueNode value, Operation op, JavaKind kind) {
+        ConstantNode result = null;
+
+        if (value.isConstant()) {
+            if (kind == JavaKind.Double) {
+                double ret = doCompute(value.asJavaConstant().asDouble(), op);
+                result = ConstantNode.forDouble(ret);
+            } else if (kind == JavaKind.Float) {
+                float ret = doCompute(value.asJavaConstant().asFloat(), op);
+                result = ConstantNode.forFloat(ret);
+            }
         }
         return result;
     }
-	
-	@Override
-	public Node canonical(CanonicalizerTool tool, ValueNode forValue) {
-		 ValueNode c = tryConstantFold(forValue, operation(), forValue.getKind());
-	        if (c != null) {
-	            return c;
-	        }
-	        return this;
-	}
 
-	@Override
-	public void generate(NodeMappableLIRBuilder builder, ArithmeticLIRGenerator lirGen) {
-		OCLBuiltinTool gen = (OCLBuiltinTool) lirGen;
+    @Override
+    public Node canonical(CanonicalizerTool tool, ValueNode forValue) {
+        ValueNode c = tryConstantFold(forValue, operation(), forValue.getStackKind());
+        if (c != null) {
+            return c;
+        }
+        return this;
+    }
+
+    @Override
+    public void generate(NodeLIRBuilderTool builder, ArithmeticLIRGeneratorTool lirGen) {
+        OCLBuiltinTool gen = ((OCLArithmeticTool) lirGen).getGen().getOCLBuiltinTool();
         Value input = builder.operand(getValue());
         Value result;
         switch (operation()) {
-        	case FABS:
-        		result = gen.emitFloatAbs(input);
-        		break;
-        	case EXP:
-        		result = gen.emitFloatExp(input);
-        		break;
-        	case SQRT:
-        		result = gen.emitFloatSqrt(input);
-        		break;
-        	case FLOOR:
-        		result = gen.emitFloatFloor(input);
-        		break;
-        	case LOG:
-        		result = gen.emitFloatLog(input);
-        		break;
+            case FABS:
+                result = gen.genFloatAbs(input);
+                break;
+            case EXP:
+                result = gen.genFloatExp(input);
+                break;
+            case SQRT:
+                result = gen.genFloatSqrt(input);
+                break;
+            case FLOOR:
+                result = gen.genFloatFloor(input);
+                break;
+            case LOG:
+                result = gen.genFloatLog(input);
+                break;
             default:
-                throw GraalInternalError.shouldNotReachHere();
+                throw shouldNotReachHere();
         }
-        builder.setResult(this, result);
+        Variable x = builder.getLIRGeneratorTool().newVariable(result.getValueKind());
+        builder.getLIRGeneratorTool().append(new AssignStmt(x, result));
+        builder.setResult(this, x);
 
-	}
-	
-	 private static double doCompute(double value, Operation op) {
-	        switch (op) {
-	        	case FABS:
-	        		return Math.abs(value);
-	        	case EXP:
-	        		return Math.exp(value);
-	        	case SQRT:
-	        		return Math.sqrt(value);
-	        	case FLOOR:
-	        		return Math.floor(value);
-	        	case LOG:
-	        		return Math.log(value);
-	            default:
-	                throw new GraalInternalError("unable to compute op %s", op);
-	        }
-	 }
-	 
-	 private static float doCompute(float value, Operation op) {
-	        switch (op) {
-	        	case FABS:
-	        		return Math.abs(value);
-	        	case EXP:
-	        		return (float) Math.exp(value);
-	        	case SQRT:
-	        		return (float) Math.sqrt(value);
-	        	case FLOOR:
-	        		return (float) Math.floor(value);
-	        	case LOG:
-	        		return (float) Math.log(value);
-	            default:
-	                throw new GraalInternalError("unable to compute op %s", op);
-	        }
-	 }
+    }
+
+    private static double doCompute(double value, Operation op) {
+        switch (op) {
+            case FABS:
+                return Math.abs(value);
+            case EXP:
+                return Math.exp(value);
+            case SQRT:
+                return Math.sqrt(value);
+            case FLOOR:
+                return Math.floor(value);
+            case LOG:
+                return Math.log(value);
+            default:
+                throw new TornadoInternalError("unable to compute op %s", op);
+        }
+    }
+
+    private static float doCompute(float value, Operation op) {
+        switch (op) {
+            case FABS:
+                return Math.abs(value);
+            case EXP:
+                return (float) Math.exp(value);
+            case SQRT:
+                return (float) Math.sqrt(value);
+            case FLOOR:
+                return (float) Math.floor(value);
+            case LOG:
+                return (float) Math.log(value);
+            default:
+                throw new TornadoInternalError("unable to compute op %s", op);
+        }
+    }
 
 }
