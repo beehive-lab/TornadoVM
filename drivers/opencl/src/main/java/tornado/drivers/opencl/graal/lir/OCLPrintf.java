b7ba09edb47fc5baf866dcdab88c6f33c165258c
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-11 17:27:06 +0000

    [DRIVERS-OPENCL] OCLPrintf fixed formatting bug

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLPrintf.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/lir/OCLPrintf.java
@@ -1,6 +1,7 @@
 package tornado.drivers.opencl.graal.lir;
 
 import com.oracle.graal.compiler.common.LIRKind;
+import com.oracle.graal.lir.ConstantValue;
 import com.oracle.graal.lir.Opcode;
 import jdk.vm.ci.meta.Value;
 import tornado.drivers.opencl.graal.asm.OCLAssembler;
@@ -18,29 +19,41 @@ public class OCLPrintf extends OCLLIROp {
 
     @Override
     public void emit(OCLCompilationResultBuilder crb, OCLAssembler asm) {
-        asm.emit("printf( \"tornado[%3d,%3d,%3d]> ");
-        asm.emitValue(crb, inputs[0]);
+        asm.emit("printf( \"tornado[%%3d,%%3d,%%3d]> %s", asm.formatConstant((ConstantValue) inputs[0]));
+
         asm.emit("\", ");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
             asm.emit("get_global_id(%d), ", i);
+        }
+        asm.emit("get_global_id(%d) ", 2);
+        if (inputs.length > 1) {
+            asm.emit(", ");
         }
         for (int i = 1; i < inputs.length - 1; i++) {
             asm.emitValue(crb, inputs[i]);
             asm.emit(", ");
         }
-        asm.emitValue(crb, inputs[inputs.length - 1]);
+
+        if (inputs.length > 1) {
+            asm.emitValue(crb, inputs[inputs.length - 1]);
+        }
         asm.emit(")");
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format("<%s,%s,%s> tprintf( %s", inputs[0], inputs[1], inputs[2], inputs[3]));
-        for (int i = 4; i < inputs.length - 1; i++) {
+        sb.append(String.format("printf( %s", inputs[0]));
+        if (inputs.length > 1) {
+            sb.append(", ");
+        }
+        for (int i = 1; i < inputs.length - 1; i++) {
             sb.append(inputs[i]);
             sb.append(", ");
         }
-        sb.append(inputs[inputs.length - 1]);
+        if (inputs.length > 1) {
+            sb.append(inputs[inputs.length - 1]);
+        }
         sb.append(" )");
         return sb.toString();
     }
