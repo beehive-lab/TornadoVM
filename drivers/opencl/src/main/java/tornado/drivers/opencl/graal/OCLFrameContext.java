2faa5c5b9c16f00a1310e0e09214b9ba5e0fd342
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-13 19:31:11 +0000

    [DRIVERS-OPENCL] OCLFrameContext code tidy

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLFrameContext.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/OCLFrameContext.java
@@ -1,27 +1,26 @@
 package tornado.drivers.opencl.graal;
 
-import tornado.common.TornadoLogger;
-
 import com.oracle.graal.lir.asm.CompilationResultBuilder;
 import com.oracle.graal.lir.asm.FrameContext;
+import tornado.common.TornadoLogger;
 
 public class OCLFrameContext extends TornadoLogger implements FrameContext {
 
-	@Override
-	public void enter(CompilationResultBuilder crb) {
-		trace("FrameContext.enter()");
-		
-	}
+    @Override
+    public void enter(CompilationResultBuilder crb) {
+        trace("FrameContext.enter()");
 
-	@Override
-	public boolean hasFrame() {
-		return false;
-	}
+    }
 
-	@Override
-	public void leave(CompilationResultBuilder crb) {
-		trace("FrameContext.leave()");
-		
-	}
+    @Override
+    public boolean hasFrame() {
+        return false;
+    }
+
+    @Override
+    public void leave(CompilationResultBuilder crb) {
+        trace("FrameContext.leave()");
+
+    }
 
 }
