b3caa256b676dd7bb4d894ac569aa409315261dc
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-15 15:36:16 +0000

    [DRIVERS-OPENCL] VectorLoadElementNode updated so that lane id can be a valuenode

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/nodes/vector/VectorLoadElementNode.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/nodes/vector/VectorLoadElementNode.java
@@ -3,19 +3,18 @@ package tornado.drivers.opencl.graal.nodes.vector;
 import com.oracle.graal.graph.NodeClass;
 import com.oracle.graal.nodeinfo.NodeInfo;
 import com.oracle.graal.nodes.ValueNode;
-import tornado.drivers.opencl.graal.OCLStamp;
+import tornado.drivers.opencl.graal.lir.OCLKind;
 
 /**
  * The {@code LoadIndexedNode} represents a read from an element of an array.
  */
 @NodeInfo(nameTemplate = "Load .s{p#lane}")
-public class VectorLoadElementNode extends VectorElementOpNode{
+public class VectorLoadElementNode extends VectorElementOpNode {
 
     public static final NodeClass<VectorLoadElementNode> TYPE = NodeClass.create(VectorLoadElementNode.class);
 
-    public VectorLoadElementNode(ValueNode vector, ValueNode lane) {
-        super(TYPE, ((OCLStamp)vector.stamp()).getOCLKind().getElementKind(), vector, lane);
+    public VectorLoadElementNode(OCLKind kind, ValueNode vector, ValueNode lane) {
+        super(TYPE, kind, vector, lane);
     }
 
 }
-
