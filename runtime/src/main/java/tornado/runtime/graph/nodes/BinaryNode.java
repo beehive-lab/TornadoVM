a8a8d31c4fe98582342f6a012de5a8a05832e366
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-25 20:54:58 +0000

    [TIDY] Code formatting

--- runtime/src/main/java/tornado/runtime/graph/nodes/BinaryNode.java
+++ runtime/src/main/java/tornado/runtime/graph/nodes/BinaryNode.java
@@ -3,25 +3,26 @@ package tornado.runtime.graph.nodes;
 import static tornado.common.exceptions.TornadoInternalError.shouldNotReachHere;
 
 public abstract class BinaryNode extends AbstractNode {
-	protected AbstractNode x;
-	protected AbstractNode y;
-	
-	public AbstractNode getX(){
-		return x;
-	}
-	
-	public AbstractNode getY(){
-		return y;
-	}
-	
-	public void replaceAtInputs(AbstractNode toReplace, AbstractNode replacement){
-		if(x == toReplace){
-			x = replacement;
-		} else if(y == toReplace){
-			y = replacement;
-		} else {
-			shouldNotReachHere();
-		}
-	}
-	
+
+    protected AbstractNode x;
+    protected AbstractNode y;
+
+    public AbstractNode getX() {
+        return x;
+    }
+
+    public AbstractNode getY() {
+        return y;
+    }
+
+    public void replaceAtInputs(AbstractNode toReplace, AbstractNode replacement) {
+        if (x == toReplace) {
+            x = replacement;
+        } else if (y == toReplace) {
+            y = replacement;
+        } else {
+            shouldNotReachHere();
+        }
+    }
+
 }
