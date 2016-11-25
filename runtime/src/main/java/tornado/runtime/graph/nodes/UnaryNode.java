ef882f7b702c5dda9781fbaf6faf20a4efa57b74
James Clarkson <james.clarkson@manchester.ac.uk>
2016-11-25 20:55:05 +0000

    [TIDY] Code formatting

--- runtime/src/main/java/tornado/runtime/graph/nodes/UnaryNode.java
+++ runtime/src/main/java/tornado/runtime/graph/nodes/UnaryNode.java
@@ -3,19 +3,20 @@ package tornado.runtime.graph.nodes;
 import static tornado.common.exceptions.TornadoInternalError.shouldNotReachHere;
 
 public abstract class UnaryNode extends AbstractNode {
-	protected AbstractNode input;
-	
-	public AbstractNode getInput(){
-		return input;
-	}
-	
-	public void replaceAtInputs(AbstractNode toReplace, AbstractNode replacement){
-		if(input == toReplace){
-			input = replacement;
-		} else {
-			shouldNotReachHere();
-		}
-			
-	}
-	
+
+    protected AbstractNode input;
+
+    public AbstractNode getInput() {
+        return input;
+    }
+
+    public void replaceAtInputs(AbstractNode toReplace, AbstractNode replacement) {
+        if (input == toReplace) {
+            input = replacement;
+        } else {
+            shouldNotReachHere();
+        }
+
+    }
+
 }
