820d7b7ff2d4d801cdee319e6fec1be84364a1b6
James Clarkson <james.clarkson@manchester.ac.uk>
2017-04-24 11:58:30 +0100

    [TIDY] Removed @Read annotation

--- examples/src/main/java/tornado/examples/HelloWorld.java
+++ examples/src/main/java/tornado/examples/HelloWorld.java
@@ -1,13 +1,12 @@
 package tornado.examples;
 
 import tornado.api.Parallel;
-import tornado.api.Read;
 import tornado.lang.Debug;
 import tornado.runtime.api.TaskSchedule;
 
 public class HelloWorld {
 
-    public static void printHello(@Read int n) {
+    public static void printHello(int n) {
         for (@Parallel int i = 0; i < n; i++) {
             Debug.printf("hello\n");
         }
