b1d1fddf051e0ccf308d5b5294734dd14ec22c82
James Clarkson <james.clarkson@manchester.ac.uk>
2017-04-24 10:50:41 +0100

    [ENHANCE] Re-introduced ability to use old-style parallelisation scheme

--- drivers/opencl/src/main/java/tornado/drivers/opencl/graal/phases/TornadoParallelScheduler.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/graal/phases/TornadoParallelScheduler.java
@@ -2,9 +2,17 @@ package tornado.drivers.opencl.graal.phases;
 
 import com.oracle.graal.debug.Debug;
 import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.calc.AddNode;
+import com.oracle.graal.nodes.calc.DivNode;
+import com.oracle.graal.nodes.calc.MulNode;
+import com.oracle.graal.nodes.calc.SubNode;
 import com.oracle.graal.nodes.util.GraphUtil;
 import com.oracle.graal.phases.BasePhase;
+import jdk.vm.ci.meta.JavaKind;
+import tornado.api.enums.TornadoSchedulingStrategy;
 import tornado.drivers.opencl.graal.nodes.GlobalThreadIdNode;
+import tornado.drivers.opencl.graal.nodes.GlobalThreadSizeNode;
+import tornado.drivers.opencl.graal.nodes.OCLIntBinaryIntrinsicNode;
 import tornado.drivers.opencl.runtime.OCLDeviceMapping;
 import tornado.graal.nodes.AbstractParallelNode;
 import tornado.graal.nodes.ParallelOffsetNode;
@@ -12,9 +20,117 @@ import tornado.graal.nodes.ParallelRangeNode;
 import tornado.graal.nodes.ParallelStrideNode;
 import tornado.graal.phases.TornadoHighTierContext;
 
+import static tornado.api.enums.TornadoSchedulingStrategy.PER_BLOCK;
+import static tornado.api.enums.TornadoSchedulingStrategy.PER_ITERATION;
 import static tornado.common.Tornado.ENABLE_PARALLELIZATION;
+import static tornado.common.Tornado.USE_THREAD_COARSENING;
 
 public class TornadoParallelScheduler extends BasePhase<TornadoHighTierContext> {
+
+    private ValueNode blockSize;
+
+    private void replaceOffsetNode(TornadoSchedulingStrategy schedule, StructuredGraph graph,
+            ParallelOffsetNode offset) {
+        if (schedule == PER_BLOCK) {
+            replacePerBlock(graph, offset);
+        } else if (schedule == PER_ITERATION) {
+            replacePerIteration(graph, offset);
+        }
+    }
+
+    private void replacePerIteration(StructuredGraph graph, ParallelOffsetNode offset) {
+
+        final ConstantNode index = graph.addOrUnique(ConstantNode.forInt(offset.index()));
+
+        final GlobalThreadIdNode threadId = graph.addOrUnique(new GlobalThreadIdNode(index));
+
+        final AddNode addNode = graph.addOrUnique(new AddNode(threadId, offset.value()));
+
+        offset.replaceAtUsages(addNode);
+        offset.safeDelete();
+    }
+
+    private void replacePerBlock(StructuredGraph graph, ParallelOffsetNode offset) {
+        final GlobalThreadIdNode threadId = graph.addOrUnique(new GlobalThreadIdNode(ConstantNode.forInt(
+                offset.index(), graph)));
+        final MulNode newOffset = graph.addOrUnique(new MulNode(threadId, blockSize));
+        offset.replaceAtUsages(newOffset);
+        offset.safeDelete();
+    }
+
+    private void replaceStrideNode(TornadoSchedulingStrategy schedule, StructuredGraph graph,
+            ParallelStrideNode stride) {
+        if (schedule == PER_BLOCK) {
+            replacePerBlock(graph, stride);
+        } else if (schedule == PER_ITERATION) {
+            replacePerIteration(graph, stride);
+        }
+    }
+
+    private void replacePerIteration(StructuredGraph graph, ParallelStrideNode stride) {
+
+        final ConstantNode index = graph.addOrUnique(ConstantNode.forInt(stride.index()));
+
+        final GlobalThreadSizeNode threadCount = graph.addOrUnique(new GlobalThreadSizeNode(index));
+
+        stride.replaceAtUsages(threadCount);
+        stride.safeDelete();
+    }
+
+    private void replacePerBlock(StructuredGraph graph, ParallelStrideNode stride) {
+
+        // final MulNode newStride = graph.addOrUnique(new MulNode(stride.value(),)
+        stride.replaceAtUsages(stride.value());
+        stride.safeDelete();
+
+    }
+
+    private void replaceRangeNode(TornadoSchedulingStrategy schedule, StructuredGraph graph,
+            ParallelRangeNode range) {
+        if (schedule == PER_BLOCK) {
+            replacePerBlock(graph, range);
+        } else if (schedule == PER_ITERATION) {
+            replacePerIteration(graph, range);
+        }
+    }
+
+    private void replacePerIteration(StructuredGraph graph, ParallelRangeNode range) {
+
+        range.replaceAtUsages(range.value());
+
+    }
+
+    private void buildBlockSize(StructuredGraph graph, ParallelRangeNode range) {
+
+        final DivNode rangeByStride = graph.addOrUnique(new DivNode(range.value(), range.stride()
+                .value()));
+        final SubNode trueRange = graph.addOrUnique(new SubNode(rangeByStride, range.offset()
+                .value()));
+        final ConstantNode index = ConstantNode.forInt(range.index(), graph);
+        final GlobalThreadSizeNode threadCount = graph.addOrUnique(new GlobalThreadSizeNode(index));
+        final SubNode threadCountM1 = graph.addOrUnique(new SubNode(threadCount, ConstantNode
+                .forInt(1, graph)));
+        final AddNode adjustedTrueRange = graph.addOrUnique(new AddNode(trueRange, threadCountM1));
+        blockSize = graph.addOrUnique(new DivNode(adjustedTrueRange, threadCount));
+
+    }
+
+    private void replacePerBlock(StructuredGraph graph, ParallelRangeNode range) {
+        buildBlockSize(graph, range);
+
+        final GlobalThreadIdNode threadId = graph.addOrUnique(new GlobalThreadIdNode(ConstantNode.forInt(
+                range.index(), graph)));
+        final MulNode newOffset = graph.addOrUnique(new MulNode(threadId, blockSize));
+
+        final AddNode newRange = graph.addOrUnique(new AddNode(newOffset, blockSize));
+
+        final ValueNode adjustedRange = graph.addOrUnique(OCLIntBinaryIntrinsicNode.create(
+                newRange, range.value(), OCLIntBinaryIntrinsicNode.Operation.MIN, JavaKind.Int));
+
+        range.replaceAtUsages(adjustedRange);
+        range.safeDelete();
+
+    }
 
     @Override
     protected void run(StructuredGraph graph, TornadoHighTierContext context) {
@@ -23,11 +139,22 @@ public class TornadoParallelScheduler extends BasePhase<TornadoHighTierContext> 
         }
 
         OCLDeviceMapping device = (OCLDeviceMapping) context.getDeviceMapping();
+        final TornadoSchedulingStrategy strategy = device.getPreferedSchedule();
         long[] maxWorkItemSizes = device.getDevice().getMaxWorkItemSizes();
 
         graph.getNodes().filter(ParallelRangeNode.class).forEach(node -> {
             if (ENABLE_PARALLELIZATION && maxWorkItemSizes[node.index()] > 1) {
-                paralleliseLoop(graph, node);
+                if (USE_THREAD_COARSENING) {
+                    paralleliseLoop(graph, node);
+                } else {
+
+                    ParallelOffsetNode offset = node.offset();
+                    ParallelStrideNode stride = node.stride();
+                    replaceRangeNode(strategy, graph, node);
+                    replaceOffsetNode(strategy, graph, offset);
+                    replaceStrideNode(strategy, graph, stride);
+
+                }
             } else {
                 serialiseLoop(node);
             }
