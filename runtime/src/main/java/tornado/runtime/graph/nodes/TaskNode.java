68ab9f83f49655b98d1c6226cf66a4377c5738f8
James Clarkson <james.clarkson@manchester.ac.uk>
2017-04-25 21:39:11 +0100

    [TIDY] Re-formatted source code

--- runtime/src/main/java/tornado/runtime/graph/nodes/TaskNode.java
+++ runtime/src/main/java/tornado/runtime/graph/nodes/TaskNode.java
@@ -3,48 +3,49 @@ package tornado.runtime.graph.nodes;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TaskNode extends AsyncNode{
+public class TaskNode extends AsyncNode {
 
-	private AbstractNode[] arguments;
-	private int taskIndex;
-	
-	public TaskNode(ContextNode context, int index, AbstractNode[] arguments){
-		super(context);
-		this.taskIndex = index;
-		this.arguments = arguments;
-	}
-	
-	public AbstractNode getArg(int index){
-		return arguments[index];
-	}
-	
-	public int getTaskIndex(){
-		return taskIndex;
-	}
-	
-	public List<AbstractNode> getInputs(){
-		final List<AbstractNode> inputs = new ArrayList<AbstractNode>();
-		for(AbstractNode input : arguments){
-			inputs.add(input);
-		}
-		return inputs;
-	}
-	
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("[" + id + "]: ");
-		sb.append("task=" + taskIndex);
-		sb.append(", args=[ ");
-		for(AbstractNode arg : arguments){
-			sb.append("" + arg.getId() + " ");
-		}
-		sb.append("]");
-		return sb.toString();
-	}
+    private AbstractNode[] arguments;
+    private int taskIndex;
 
-	public int getNumArgs() {
-		return arguments.length;
-	}
-	
-	
+    public TaskNode(ContextNode context, int index, AbstractNode[] arguments) {
+        super(context);
+        this.taskIndex = index;
+        this.arguments = arguments;
+    }
+
+    public AbstractNode getArg(int index) {
+        return arguments[index];
+    }
+
+    public int getTaskIndex() {
+        return taskIndex;
+    }
+
+    @Override
+    public List<AbstractNode> getInputs() {
+        final List<AbstractNode> inputs = new ArrayList<>();
+        for (AbstractNode input : arguments) {
+            inputs.add(input);
+        }
+        return inputs;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[" + id + "]: ");
+        sb.append("task=" + taskIndex);
+        sb.append(", args=[ ");
+        for (AbstractNode arg : arguments) {
+            sb.append("" + arg.getId() + " ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public int getNumArgs() {
+        return arguments.length;
+    }
+
 }
