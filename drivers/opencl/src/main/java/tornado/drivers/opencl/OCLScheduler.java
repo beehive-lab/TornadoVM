39094189504241f3d3801e7426a91cf84183a1ad
James Clarkson <james.clarkson@manchester.ac.uk>
2017-04-25 21:40:56 +0100

    [TIDY] Re-formatted source code

--- drivers/opencl/src/main/java/tornado/drivers/opencl/OCLScheduler.java
+++ drivers/opencl/src/main/java/tornado/drivers/opencl/OCLScheduler.java
@@ -1,19 +1,24 @@
 package tornado.drivers.opencl;
 
-import tornado.common.Tornado;
-import tornado.drivers.opencl.enums.OCLDeviceType;
+import static tornado.common.Tornado.fatal;
+
 public class OCLScheduler {
-	
-	public static final OCLKernelScheduler create(final OCLDeviceContext context){
-		
-		if (context.getDevice().getDeviceType() == OCLDeviceType.CL_DEVICE_TYPE_GPU ){
-			return new OCLGpuScheduler(context);
-		} else if(context.getDevice().getDeviceType() == OCLDeviceType.CL_DEVICE_TYPE_CPU || context.getDevice().getDeviceType() == OCLDeviceType.CL_DEVICE_TYPE_ACCELERATOR)
-			return new OCLCpuScheduler(context);
-		else{
-			Tornado.fatal("No scheduler available for device: %s",context);
-		}
-		
-		return null;		
-	}
+
+    public static final OCLKernelScheduler create(final OCLDeviceContext context) {
+
+        if (null != context.getDevice().getDeviceType()) {
+            switch (context.getDevice().getDeviceType()) {
+                case CL_DEVICE_TYPE_GPU:
+                    return new OCLGpuScheduler(context);
+                case CL_DEVICE_TYPE_CPU:
+                case CL_DEVICE_TYPE_ACCELERATOR:
+                    return new OCLCpuScheduler(context);
+                default:
+                    fatal("No scheduler available for device: %s", context);
+                    break;
+            }
+        }
+
+        return null;
+    }
 }
