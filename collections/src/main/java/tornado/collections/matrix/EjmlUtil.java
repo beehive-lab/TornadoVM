6b1ba37647a3f6cce9abd717591b1476b94ec213
James Clarkson <james.clarkson@manchester.ac.uk>
2017-05-05 10:32:17 +0100

    [TIDY] Code tidy

--- collections/src/main/java/tornado/collections/matrix/EjmlUtil.java
+++ collections/src/main/java/tornado/collections/matrix/EjmlUtil.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2012 James Clarkson.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,57 +16,68 @@
 package tornado.collections.matrix;
 
 import org.ejml.simple.SimpleMatrix;
-
 import tornado.collections.types.Matrix4x4Float;
+import tornado.collections.types.MatrixDouble;
 
 public class EjmlUtil {
-	
-	
+
 //	public static MatrixFloat toMatrixFloat(SimpleMatrix m){
 //        //System.out.printf("Matrix: row=%d, col=%d\n",m.numRows(),m.numCols());
-//        
-//        
+//
+//
 //	    MatrixFloat result = new MatrixFloat(m.numCols(),m.numRows());
-//        
+//
 //        for(int i=0;i<m.numRows();i++)
 //            for(int j=0;j<m.numCols();j++)
 //                result.set(i,j,(float) m.get(i,j));
-//        
-//        
+//
+//
 //        return result;
 //    }
-	
-	public static Matrix4x4Float toMatrix4x4Float(SimpleMatrix m){
+    public static Matrix4x4Float toMatrix4x4Float(SimpleMatrix m) {
         //System.out.printf("Matrix: row=%d, col=%d\n",m.numRows(),m.numCols());
-        
-        
-		Matrix4x4Float result = new Matrix4x4Float();
-        
-        for(int i=0;i<m.numRows();i++)
-            for(int j=0;j<m.numCols();j++)
-                result.set(i,j,(float) m.get(i,j));
-        
-        
+
+        Matrix4x4Float result = new Matrix4x4Float();
+
+        for (int i = 0; i < m.numRows(); i++) {
+            for (int j = 0; j < m.numCols(); j++) {
+                result.set(i, j, (float) m.get(i, j));
+            }
+        }
+
         return result;
     }
-	
+
 //	public static SimpleMatrix toMatrix(MatrixFloat m){
 //        SimpleMatrix result = new SimpleMatrix(m.M(),m.N());
-//        
+//
 //        for(int i=0;i<m.M();i++)
 //            for(int j=0;j<m.N();j++)
 //                result.set(i,j,(double)m.get(i, j));
-//        
+//
 //        return result;
 //    }
-	
-	public static SimpleMatrix toMatrix(Matrix4x4Float m){
-        SimpleMatrix result = new SimpleMatrix(m.M(),m.N());
-        
-        for(int i=0;i<m.M();i++)
-            for(int j=0;j<m.N();j++)
-                result.set(i,j,(double)m.get(i, j));
-        
+    public static SimpleMatrix toMatrix(Matrix4x4Float m) {
+        SimpleMatrix result = new SimpleMatrix(m.M(), m.N());
+
+        for (int i = 0; i < m.M(); i++) {
+            for (int j = 0; j < m.N(); j++) {
+                result.set(i, j, (double) m.get(i, j));
+            }
+        }
+
+        return result;
+    }
+
+    public static SimpleMatrix toMatrix(MatrixDouble m) {
+        SimpleMatrix result = new SimpleMatrix(m.M(), m.N());
+
+        for (int i = 0; i < m.M(); i++) {
+            for (int j = 0; j < m.N(); j++) {
+                result.set(i, j, m.get(i, j));
+            }
+        }
+
         return result;
     }
 }
