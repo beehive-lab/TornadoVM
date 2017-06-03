6e4c45159863102b247f3ac8962ed4596d02cf3e
James Clarkson <james.clarkson@manchester.ac.uk>
2017-06-03 20:04:47 +0100

    [TIDY] ObjectBuffer formatted code

--- runtime/src/main/java/tornado/common/ObjectBuffer.java
+++ runtime/src/main/java/tornado/common/ObjectBuffer.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2012 James Clarkson.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,25 +18,33 @@ package tornado.common;
 import tornado.common.exceptions.TornadoOutOfMemoryException;
 
 public interface ObjectBuffer {
-	
-	public long toBuffer();
-	public long getBufferOffset();
-	public long toAbsoluteAddress();
-	public long toRelativeAddress();
 
-	public void read(Object ref);
-	public void write(Object ref);
-	
-	public int enqueueRead(Object ref,int[] events);
-	
-	public int enqueueWrite(Object ref,int[] events);
-	
-	public void allocate(Object ref) throws TornadoOutOfMemoryException;
-	public int getAlignment();
-	
-	public boolean isValid();
-	public void invalidate();
-	public void printHeapTrace();
-	public long size();
-	
+    public long toBuffer();
+
+    public long getBufferOffset();
+
+    public long toAbsoluteAddress();
+
+    public long toRelativeAddress();
+
+    public void read(Object ref);
+
+    public void write(Object ref);
+
+    public int enqueueRead(Object ref, int[] events, boolean useDeps);
+
+    public int enqueueWrite(Object ref, int[] events, boolean useDeps);
+
+    public void allocate(Object ref) throws TornadoOutOfMemoryException;
+
+    public int getAlignment();
+
+    public boolean isValid();
+
+    public void invalidate();
+
+    public void printHeapTrace();
+
+    public long size();
+
 }
