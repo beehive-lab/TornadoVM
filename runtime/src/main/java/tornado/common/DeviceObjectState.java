6955bc78081fbad192c145d5d592cecd46901e2d
James Clarkson <james.clarkson@manchester.ac.uk>
2017-09-01 20:05:57 +0100

    [ENHANCE] DeviceObjectState format source

--- runtime/src/main/java/tornado/common/DeviceObjectState.java
+++ runtime/src/main/java/tornado/common/DeviceObjectState.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2012 James Clarkson.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,82 +15,84 @@
  */
 package tornado.common;
 
+import static tornado.common.RuntimeUtilities.humanReadableByteCount;
 
 public class DeviceObjectState {
-	private boolean valid;
-	private boolean modified;
-	private boolean contents;
-	
-	private ObjectBuffer buffer;
-	
-	public DeviceObjectState(){
-		valid = false;
-		modified = false;
-		contents = false;
-		buffer = null;
-	}
-	
-	public void setBuffer(ObjectBuffer value){
-		buffer = value;
-	}
-	
-	public boolean hasBuffer(){
-		return buffer != null;
-	}
-	
-	public ObjectBuffer getBuffer(){
-		return buffer;
-	}
-	
-	public boolean isValid(){
-		return valid;
-	}
-	
-	public boolean isModified(){
-		return modified;
-	}
-	
-	public void invalidate(){
-		valid = false;
-	}
-	
-	public boolean hasContents(){
-		return contents;
-	}
-	
-	public void setContents(boolean value){
-		contents = value;
-	}
-	
-        @Override
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append((isValid()) ? "V" : "-");
-		sb.append((isModified()) ? "M" : "-");
-		sb.append((hasContents()) ? "C" : "-");
-		if(hasBuffer()){
-		sb.append(String.format(" address=0x%x, size=%s ",buffer.toAbsoluteAddress(),RuntimeUtilities.humanReadableByteCount(buffer.size(), true)));
-		} else {
-			sb.append(" <unbuffered>");	
-		}
-		
-		return sb.toString();
-	}
 
-	public void setModified(boolean value) {
-		modified = value;
-	}
+    private boolean valid;
+    private boolean modified;
+    private boolean contents;
 
-	public void setValid(boolean value) {
-		valid = value;
-	}
+    private ObjectBuffer buffer;
 
-	public long getAddress() {
-		return buffer.toAbsoluteAddress();
-	}
-        
-        public long getOffset() {
-		return buffer.toRelativeAddress();
-	}
+    public DeviceObjectState() {
+        valid = false;
+        modified = false;
+        contents = false;
+        buffer = null;
+    }
+
+    public void setBuffer(ObjectBuffer value) {
+        buffer = value;
+    }
+
+    public boolean hasBuffer() {
+        return buffer != null;
+    }
+
+    public ObjectBuffer getBuffer() {
+        return buffer;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public boolean isModified() {
+        return modified;
+    }
+
+    public void invalidate() {
+        valid = false;
+    }
+
+    public boolean hasContents() {
+        return contents;
+    }
+
+    public void setContents(boolean value) {
+        contents = value;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append((isValid()) ? "V" : "-");
+        sb.append((isModified()) ? "M" : "-");
+        sb.append((hasContents()) ? "C" : "-");
+        if (hasBuffer()) {
+            sb.append(String.format(" address=0x%x, size=%s ", buffer.toAbsoluteAddress(), humanReadableByteCount(buffer.size(), true)));
+        } else {
+            sb.append(" <unbuffered>");
+        }
+
+        return sb.toString();
+    }
+
+    public void setModified(boolean value) {
+        modified = value;
+    }
+
+    public void setValid(boolean value) {
+        valid = value;
+    }
+
+    public long getAddress() {
+        return buffer.toAbsoluteAddress();
+    }
+
+    public long getOffset() {
+        return buffer.toRelativeAddress();
+    }
 }
