6ca6159435f2eb719682fd297a395738971f55df
Juan Fumero <juan.fumero@ed.ac.uk>
2017-12-11 12:08:46 +0100

    raycast added as a unittest

--- unittests/src/main/java/tornado/unittests/slam/graphics/GraphicsTests.java
+++ unittests/src/main/java/tornado/unittests/slam/graphics/GraphicsTests.java
@@ -27,6 +27,8 @@
 package tornado.unittests.slam.graphics;
 
 import static org.junit.Assert.assertEquals;
+import static tornado.collections.types.Float3.length;
+import static tornado.collections.types.Float3.normalise;
 
 import java.util.Random;
 
@@ -35,10 +37,13 @@ import org.junit.Test;
 import tornado.api.Parallel;
 import tornado.collections.graphics.GraphicsMath;
 import tornado.collections.types.Float3;
+import tornado.collections.types.Float4;
 import tornado.collections.types.ImageFloat;
 import tornado.collections.types.ImageFloat3;
 import tornado.collections.types.Matrix4x4Float;
+import tornado.collections.types.Short2;
 import tornado.collections.types.VectorFloat3;
+import tornado.collections.types.VolumeShort2;
 import tornado.runtime.api.TaskSchedule;
 import tornado.unittests.common.TornadoTestBase;
 
@@ -263,6 +268,90 @@ public class GraphicsTests extends TornadoTestBase {
             assertEquals(s.getS1(), o.getS1(), 0.001);
             assertEquals(s.getS1(), o.getS1(), 0.001);
         }
+    }
+
+    // Ray Cast testing
+
+    private static final float INVALID = -2;
+
+    public static final void raycast(ImageFloat3 verticies, ImageFloat3 normals, VolumeShort2 volume, Float3 volumeDims, Matrix4x4Float view, float nearPlane, float farPlane, float largeStep,
+            float smallStep) {
+
+        for (@Parallel int y = 0; y < verticies.Y(); y++) {
+            for (@Parallel int x = 0; x < verticies.X(); x++) {
+                final Float4 hit = GraphicsMath.raycastPoint(volume, volumeDims, x, y, view, nearPlane, farPlane, smallStep, largeStep);
+
+                final Float3 normal;
+                final Float3 position;
+                if (hit.getW() > 0f) {
+                    position = hit.asFloat3();
+
+                    // final Float3 surfNorm = VolumeOps.grad(volume,
+                    // volumeDims, position);
+
+                    final Float3 surfNorm = new Float3(0f, 0f, 0f);
+
+                    if (length(surfNorm) != 0) {
+                        normal = normalise(surfNorm);
+                    } else {
+                        normal = new Float3(INVALID, 0f, 0f);
+                    }
+                } else {
+                    normal = new Float3(INVALID, 0f, 0f);
+                    position = new Float3();
+                }
+
+                verticies.set(x, y, position);
+                normals.set(x, y, normal);
+            }
+        }
+    }
+
+    @Test
+    public void raycastTest() {
+
+        final int size = 4;
+        Random r = new Random();
+
+        Matrix4x4Float view = new Matrix4x4Float();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                view.set(i, j, j + r.nextFloat());
+            }
+        }
+
+        ImageFloat3 verticies = new ImageFloat3(size, size);
+        ImageFloat3 normals = new ImageFloat3(size, size);
+        VolumeShort2 volume = new VolumeShort2(size, size, size);
+        Float3 volumeDims = new Float3(r.nextFloat(), r.nextFloat(), r.nextFloat());
+
+        float nearPlane = r.nextFloat();
+        float farPlane = r.nextFloat();
+        float largeStep = r.nextFloat();
+        float smallStep = r.nextFloat();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                verticies.set(i, j, new Float3(1f, 2f, 3f));
+                normals.set(i, j, new Float3(1f, 2f, 3f));
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    volume.set(i, j, k, new Short2((short) 1, (short) 2));
+                }
+            }
+        }
+
+        // @formatter:off
+        new TaskSchedule("t0")
+            .task("s0", GraphicsTests::raycast, verticies, normals, volume, volumeDims, view, nearPlane, farPlane, largeStep, smallStep)
+            .streamOut(verticies, normals)
+            .execute();        
+        // @formatter:on
+
     }
 
 }
