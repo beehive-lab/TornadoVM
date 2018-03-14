93333f004c71c968975661a38c631573ebed9e85
Juan Fumero <juan.fumero@manchester.ac.uk>
2018-03-14 17:08:51 +0100

    Example of parallel reductions added

---
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/reductions/Example.java
@@ -0,0 +1,67 @@
+/*
+ * This file is part of Tornado: A heterogeneous programming framework: 
+ * https://github.com/beehive-lab/tornado
+ *
+ * Copyright (c) 2013-2018, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Authors: Juan Fumero
+ *
+ */
+package uk.ac.manchester.tornado.examples.reductions;
+
+import java.util.stream.IntStream;
+
+import uk.ac.manchester.tornado.api.Parallel;
+import uk.ac.manchester.tornado.api.Reduce;
+import uk.ac.manchester.tornado.runtime.api.TaskSchedule;
+
+public class Example {
+
+    public static void reductionAnnotation(int[] input, @Reduce int[] result) {
+        for (@Parallel int i = 0; i < input.length; i++) {
+            result[0] += input[i];
+        }
+    }
+
+    public static void testReductionAnnotation() {
+        int[] input = new int[128];
+        int[] result = new int[1];
+
+        IntStream.range(0, 128).parallel().forEach(i -> {
+            input[i] = 1;
+        });
+
+        //@formatter:off
+        new TaskSchedule("s0")
+            .streamIn(input)
+            .task("t0", Example::reductionAnnotation, input, result)
+            .streamOut(result)
+            .execute();
+        //@formatter:on
+
+        int[] sequential = new int[1];
+        reductionAnnotation(input, sequential);
+
+    }
+
+    public static void main(String[] args) {
+        testReductionAnnotation();
+    }
+
+}
