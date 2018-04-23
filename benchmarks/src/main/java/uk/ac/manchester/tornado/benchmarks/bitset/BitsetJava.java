93fb1a9e3c2e9e86ff6cf1ec9852e669387f63ba
Michalis Papadimitriou <mikepapadim@hotmail.com>
2018-04-23 17:37:35 +0100

    Adding java and tornado bitset benchmark

--- benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/bitset/BitsetJava.java
+++ benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/bitset/BitsetJava.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of Tornado: A heterogeneous programming framework: 
+ * This file is part of Tornado: A heterogeneous programming framework:
  * https://github.com/beehive-lab/tornado
  *
  * Copyright (c) 2013-2018, APT Group, School of Computer Science,
@@ -20,7 +20,7 @@
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Authors: James Clarkson
+ * Authors: Michalis Papadimitriou
  *
  */
 package uk.ac.manchester.tornado.benchmarks.bitset;
@@ -29,17 +29,14 @@ import java.util.*;
 
 import org.apache.lucene.util.*;
 
-import uk.ac.manchester.tornado.api.*;
-import uk.ac.manchester.tornado.runtime.api.*;
+public class BitsetJava {
 
-public class BitsetTest {
-
-    public static final int intersectionCount(int numWords, LongBitSet a, LongBitSet b) {
+    public static final int intersectionCountSequential(int numWords, LongBitSet a, LongBitSet b) {
         final long[] aBits = a.getBits();
         final long[] bBits = b.getBits();
         int sum = 0;
-        for (@Parallel int i = 0; i < numWords; i++) {
-            Long.bitCount(aBits[i] & bBits[i]);
+        for (int i = 0; i < numWords; i++) {
+            sum += Long.bitCount(aBits[i] & bBits[i]);
         }
         return sum;
     }
@@ -48,7 +45,6 @@ public class BitsetTest {
 
         final int numWords = Integer.parseInt(args[0]);
         final int iterations = Integer.parseInt(args[1]);
-
         StringBuffer resultsIterations = new StringBuffer();
 
         final Random rand = new Random(7);
@@ -62,26 +58,14 @@ public class BitsetTest {
         final LongBitSet a = new LongBitSet(aBits, numWords * 8);
         final LongBitSet b = new LongBitSet(bBits, numWords * 8);
 
-        TaskSchedule s0 = new TaskSchedule("s0").task("t0", BitsetTest::intersectionCount, numWords, a, b);
-
-        s0.warmup();
-
         for (int i = 0; i < iterations; i++) {
             long start = System.nanoTime();
-            s0.execute();
+            intersectionCountSequential(numWords, a, b);
             long end = System.nanoTime();
             resultsIterations.append("Execution time of iteration " + i + " is: " + (end - start) + " ns");
             resultsIterations.append("\n");
         }
 
         System.out.println(resultsIterations.toString());
-
-        final long value = s0.getReturnValue("t0");
-        System.out.printf("value = 0x%x, %d\n", value, value);
-
-        final long ref = intersectionCount(numWords, a, b);
-        System.out.printf("ref   = 0x%x, %d\n", ref, ref);
-
     }
-
 }
