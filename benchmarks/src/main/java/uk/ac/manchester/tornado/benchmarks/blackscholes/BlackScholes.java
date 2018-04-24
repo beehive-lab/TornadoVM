97277ddb504cbbb36b13216997f894787b48edbd
Michalis Papadimitriou <mikepapadim@hotmail.com>
2018-04-24 12:45:36 +0100

    Standalone BlackScholes benchmark

--- benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/blackscholes/BlackScholes.java
+++ benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/blackscholes/BlackScholes.java
@@ -25,11 +25,11 @@
  */
 package uk.ac.manchester.tornado.benchmarks.blackscholes;
 
-import static uk.ac.manchester.tornado.common.Tornado.getProperty;
+import static uk.ac.manchester.tornado.common.Tornado.*;
 
-import uk.ac.manchester.tornado.api.Parallel;
-import uk.ac.manchester.tornado.collections.math.TornadoMath;
-import uk.ac.manchester.tornado.runtime.api.TaskSchedule;
+import uk.ac.manchester.tornado.api.*;
+import uk.ac.manchester.tornado.collections.math.*;
+import uk.ac.manchester.tornado.runtime.api.*;
 
 public class BlackScholes {
 
@@ -39,9 +39,9 @@ public class BlackScholes {
     /*
      * For a description of the algorithm and the terms used, please see the
      * documentation for this sample. On invocation of kernel blackScholes, each
-     * work thread calculates call price and put price values for given stock
-     * price, option strike price, time to expiration date, risk free interest
-     * and volatility factor.
+     * work thread calculates call price and put price values for given stock price,
+     * option strike price, time to expiration date, risk free interest and
+     * volatility factor.
      */
     public static void main(final String[] args) {
 
@@ -54,8 +54,7 @@ public class BlackScholes {
 
         final BlackScholes bs = new BlackScholes(size);
 
-        final TaskSchedule tasks = new TaskSchedule("benchmark")
-                .task("blackscholes", BlackScholes::blackscholes, bs.randArray, bs.put, bs.call);
+        final TaskSchedule tasks = new TaskSchedule("benchmark").task("blackscholes", BlackScholes::blackscholes, bs.randArray, bs.put, bs.call);
 
         tasks.warmup();
 
@@ -96,9 +95,9 @@ public class BlackScholes {
 
     /**
      * @brief Abromowitz Stegun approxmimation for PHI (Cumulative Normal
-     * Distribution Function)
-     * @param X   input value
-     * @param phi pointer to store calculated CND of X
+     *        Distribution Function)
+     * @param X
+     *            input value
      */
     final static float phi(final float X) {
         final float c1 = 0.319381530f;
@@ -117,8 +116,7 @@ public class BlackScholes {
         final float absX = Math.abs(X);
         final float t = one / (one + (temp4 * absX));
 
-        final float y = (one - (oneBySqrt2pi * TornadoMath.exp((-X * X) / two)
-                * t * (c1 + (t * (c2 + (t * (c3 + (t * (c4 + (t * c5))))))))));
+        final float y = (one - (oneBySqrt2pi * TornadoMath.exp((-X * X) / two) * t * (c1 + (t * (c2 + (t * (c3 + (t * (c4 + (t * c5))))))))));
 
         final float result = (X < zero) ? (one - y) : y;
 
@@ -163,33 +161,29 @@ public class BlackScholes {
 
     /*
      * @brief Calculates the call and put prices by using Black Scholes model
-     * @param s Array of random values of current option price @param sigma
-     * Array of random values sigma @param k Array of random values strike price
-     * @param t Array of random values of expiration time @param r Array of
-     * random values of risk free interest rate @param width Width of call price
-     * or put price array @param call Array of calculated call price values
+     * 
+     * @param s Array of random values of current option price @param sigma Array of
+     * random values sigma @param k Array of random values strike price
+     * 
+     * @param t Array of random values of expiration time @param r Array of random
+     * values of risk free interest rate @param width Width of call price or put
+     * price array @param call Array of calculated call price values
+     * 
      * @param put Array of calculated put price values
      */
-    public static void blackscholes(final float[] randArray,
-            final float[] put, final float[] call) {
+    public static void blackscholes(final float[] randArray, final float[] put, final float[] call) {
         for (@Parallel int gid = 0; gid < call.length; gid++) {
             final float two = 2.0f;
             final float inRand = randArray[gid];
-            final float S = (S_LOWER_LIMIT * inRand)
-                    + (S_UPPER_LIMIT * (1.0f - inRand));
-            final float K = (K_LOWER_LIMIT * inRand)
-                    + (K_UPPER_LIMIT * (1.0f - inRand));
-            final float T = (T_LOWER_LIMIT * inRand)
-                    + (T_UPPER_LIMIT * (1.0f - inRand));
-            final float R = (R_LOWER_LIMIT * inRand)
-                    + (R_UPPER_LIMIT * (1.0f - inRand));
-            final float sigmaVal = (SIGMA_LOWER_LIMIT * inRand)
-                    + (SIGMA_UPPER_LIMIT * (1.0f - inRand));
+            final float S = (S_LOWER_LIMIT * inRand) + (S_UPPER_LIMIT * (1.0f - inRand));
+            final float K = (K_LOWER_LIMIT * inRand) + (K_UPPER_LIMIT * (1.0f - inRand));
+            final float T = (T_LOWER_LIMIT * inRand) + (T_UPPER_LIMIT * (1.0f - inRand));
+            final float R = (R_LOWER_LIMIT * inRand) + (R_UPPER_LIMIT * (1.0f - inRand));
+            final float sigmaVal = (SIGMA_LOWER_LIMIT * inRand) + (SIGMA_UPPER_LIMIT * (1.0f - inRand));
 
             final float sigmaSqrtT = sigmaVal * TornadoMath.sqrt(T);
 
-            final float d1 = (TornadoMath.log(S / K) + ((R + ((sigmaVal * sigmaVal) / two)) * T))
-                    / sigmaSqrtT;
+            final float d1 = (TornadoMath.log(S / K) + ((R + ((sigmaVal * sigmaVal) / two)) * T)) / sigmaSqrtT;
             final float d2 = d1 - sigmaSqrtT;
 
             final float KexpMinusRT = K * TornadoMath.exp(-R * T);
