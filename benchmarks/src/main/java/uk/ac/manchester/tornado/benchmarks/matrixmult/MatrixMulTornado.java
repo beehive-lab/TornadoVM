6c28e0b008348e44a64618948dfcb60aefd5bbd8
Michalis Papadimitriou <mikepapadim@hotmail.com>
2018-04-29 17:13:37 +0100

    Intergrating matrix multiplication benchmark to benchmark runner

---
+++ benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/matrixmult/MatrixMulTornado.java
@@ -0,0 +1,80 @@
+/*
+ * This file is part of Tornado: A heterogeneous programming framework:
+ * https://github.com/beehive-lab/tornado
+ *
+ * Copyright (c) 2013-2018, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Authors: Michalis Papadimitriou
+ *
+ */
+
+package uk.ac.manchester.tornado.benchmarks.matrixmult;
+
+import java.util.*;
+
+import uk.ac.manchester.tornado.benchmarks.*;
+import uk.ac.manchester.tornado.runtime.api.*;
+
+public class MatrixMulTornado extends BenchmarkDriver {
+    private int numElements;
+    private float[] a,b,c;
+    private TaskSchedule graph;
+
+    public MatrixMulTornado(int iterations, int numElements) {
+        super(iterations);
+        this.numElements = numElements;
+    }
+
+    @Override
+    public void setUp() {
+        a = new float[numElements];
+        b = new float[numElements];
+        c = new float[numElements];
+
+        Arrays.fill(a, 3);
+        Arrays.fill(b, 2);
+        Arrays.fill(c, 0);
+
+        graph = new TaskSchedule("benchmark");
+        graph.task("t0", ComputeKernels::vectorMultiply, a, b, c);
+        graph.warmup();
+    }
+
+    @Override
+    public boolean validate() {
+        return true;
+    }
+
+    @Override
+    public void tearDown() {
+        graph.dumpProfiles();
+
+        a = null;
+        b = null;
+        c = null;
+
+        graph.getDevice().reset();
+        super.tearDown();
+    }
+
+    @Override
+    public void code() {
+        graph.execute();
+    }
+}
