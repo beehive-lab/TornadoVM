776015fb8c13a76117fd8b704dfa66a5b439623a
Michalis Papadimitriou <mikepapadim@hotmail.com>
2018-07-02 10:49:38 +0100

    Add dynamic loop size for DFT

--- examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/DFT.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/DFT.java
@@ -26,25 +26,27 @@
 
 package uk.ac.manchester.tornado.examples.fpga;
 
-import uk.ac.manchester.tornado.api.*;
-import uk.ac.manchester.tornado.collections.math.*;
 import static uk.ac.manchester.tornado.collections.math.TornadoMath.abs;
-import uk.ac.manchester.tornado.runtime.api.*;
+
+import uk.ac.manchester.tornado.api.Parallel;
+import uk.ac.manchester.tornado.collections.math.TornadoMath;
+import uk.ac.manchester.tornado.runtime.api.TaskSchedule;
 
 public class DFT {
 
-    private static    int size;
+    private static int size;
     private static TaskSchedule graph;
-    private static   float[] inReal,inImag,outReal,outImag;
+    private static float[] inReal,inImag,outReal,outImag;
+    private static int[] inputSize;
 
-    public static void computeDft(float[] inreal, float[] inimag, float[] outreal, float[] outimag) {
+    public static void computeDft(float[] inreal, float[] inimag, float[] outreal, float[] outimag, int[] inputSize) {
         int n = inreal.length;
         for (@Parallel int k = 0; k < n; k++) { // For each output element
             float sumreal = 0;
             float sumimag = 0;
-            for (int t = 0; t < n; t++) { // For each input element
-                float angle =  ((2 *  TornadoMath.floatPI() * t * k) / (float) n);
-                sumreal +=  (inreal[t] * (TornadoMath.floatCos(angle)) + inimag[t] * (TornadoMath.floatSin(angle)));
+            for (int t = 0; t < inputSize[0]; t++) { // For each input element
+                float angle = ((2 * TornadoMath.floatPI() * t * k) / (float) n);
+                sumreal += (inreal[t] * (TornadoMath.floatCos(angle)) + inimag[t] * (TornadoMath.floatSin(angle)));
                 sumimag += -(inreal[t] * (TornadoMath.floatSin(angle)) + inimag[t] * (TornadoMath.floatCos(angle)));
             }
             outreal[k] = sumreal;
@@ -62,19 +64,18 @@ public class DFT {
         graph.execute();
         graph.streamOut(outReal, outImag);
 
-        DFT.computeDft(inReal, inImag, outRealTor, outImagTor);
+        DFT.computeDft(inReal, inImag, outRealTor, outImagTor, inputSize);
 
-        for(int i = 0;i < 5; i++){
+        for (int i = 0; i < 1; i++) {
 
             long t1 = System.nanoTime();
-            DFT.computeDft(inReal, inImag, outRealTor, outImagTor);
+            DFT.computeDft(inReal, inImag, outRealTor, outImagTor, inputSize);
             long t2 = System.nanoTime();
 
             long seqTime = t2 - t1;
-           System.out.println("Sequential time: " + seqTime + "\n");
+            System.out.println("Sequential time: " + seqTime + "\n");
 
         }
-
 
         for (int i = 0; i < size; i++) {
             if (abs(outImagTor[i] - outImag[i]) > 0.1) {
@@ -91,8 +92,8 @@ public class DFT {
         System.out.printf("Number validation: " + val + "\n");
         return val;
     }
-    
-    public static void  main(String[] args){
+
+    public static void main(String[] args) {
 
         size = Integer.parseInt(args[0]);
 
@@ -100,6 +101,9 @@ public class DFT {
         inImag = new float[size];
         outReal = new float[size];
         outImag = new float[size];
+        inputSize = new int[1];
+
+        inputSize[0] = size;
 
         for (int i = 0; i < size; i++) {
             inReal[i] = 1 / (float) (i + 2);
@@ -107,18 +111,18 @@ public class DFT {
         }
 
         graph = new TaskSchedule("s0");
-        graph.task("t0", DFT::computeDft, inReal, inImag, outReal, outImag);
+        graph.task("t0", DFT::computeDft, inReal, inImag, outReal, outImag, inputSize);
         graph.streamOut(outReal, outImag);
         graph.warmup();
 
-        for(int i = 0;i < 10; i++){
+        for (int i = 0; i < 2; i++) {
             graph.execute();
         }
 
-        if(validate()){
-            System.out.println("Validation: " + "SUCCESS "+"\n");
+        if (validate()) {
+            System.out.println("Validation: " + "SUCCESS " + "\n");
         } else {
-            System.out.println("Validation: " + " FAIL "+"\n");
+            System.out.println("Validation: " + " FAIL " + "\n");
 
         }
     }
