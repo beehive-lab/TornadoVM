60b1b5f6b4ddc1e34cea147a5d06716141810813
Michalis <mikepapadim@hotmail.com>
2018-08-01 13:01:59 +0100

    Add FPGA version for SGEMM

--- examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/SGEMMFPGA.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/SGEMMFPGA.java
@@ -3,19 +3,21 @@ package uk.ac.manchester.tornado.examples.fpga;
 import java.util.Random;
 
 import uk.ac.manchester.tornado.api.Parallel;
+import uk.ac.manchester.tornado.common.enums.*;
+import uk.ac.manchester.tornado.drivers.opencl.*;
 import uk.ac.manchester.tornado.runtime.api.TaskSchedule;
 
 public class SGEMMFPGA {
 
-    public static void sgemm(final int M, final int N, final int K, final int A[], final int B[], final int C[], final int[] dims) {
+    public static void sgemm(final int A[], final int B[], final int C[], final int[] dims) {
 
         for (@Parallel int i = 0; i < dims[0]; i++) {
             for (@Parallel int j = 0; j < dims[0]; j++) {
                 int sum = 0;
                 for (int k = 0; k < dims[0]; k++) {
-                    sum += A[(i * N) + k] * B[(k * N) + j];
+                    sum += A[(i * dims[0]) + k] * B[(k * dims[0]) + j];
                 }
-                C[(i * N) + j] = sum;
+                C[(i * dims[0]) + j] = sum;
             }
         }
 
@@ -40,42 +42,65 @@ public class SGEMMFPGA {
         final Random random = new Random();
 
         for (int i = 0; i < m; i++) {
-            a[i * (m + 1)] = 1;
+            a[i * (m + 1)] = 45;
         }
 
         for (int i = 0; i < m * n; i++) {
-            b[i] = random.nextInt();
+            b[i] = 1;
         }
 
-        TaskSchedule graph = new TaskSchedule("s0");
-        graph.task("t0", SGEMMFPGA::sgemm, m, n, n, a, b, c, dims).streamOut(c);
+//
+        TaskSchedule t0 = new TaskSchedule("s0")
+                .task("t0", SGEMMFPGA::sgemm,  a, b, c, dims).streamOut(c);
+////
+//        new TaskSchedule("s0")
+//                .prebuiltTask("t0",
+//                        "sgemm",
+//                        "sgemm_test2.cl",
+//                        new Object[] { a, b, c, dims},
+//                        new Access[] {Access.READ, Access.READ, Access.READ_WRITE, Access.READ },
+//                        OpenCL.defaultDevice(),
+//                        new int[] {dims[0],dims[0]})
+//                .streamOut(c)
+//                .execute();
 
-        graph.warmup();
 
-        graph.execute();
+        t0.warmup();
+
+        for (int y  = 0; y < 10; y++) {
+
+        t0.execute();
+
+        t0.syncObject(c);
 
         System.out.println("Checking result");
         boolean wrongResult = false;
+            long start = System.nanoTime();
 
-        for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 int sum = 0;
                 for (int k = 0; k < size; k++) {
                     sum += a[(i * size) + k] * b[(k * size) + j];
                 }
                 result[(i * size) + j] = sum;
+                //:System.out.println("C: result  " + c[(i * size) + j] + "\n");
 
                 if (result[(i * size) + j] != c[(i * size) + j]) {
+                   // System.out.println("Wrong result  " + c[(i * size) + j] + "\n");
                     wrongResult = true;
                     break;
                 }
             }
         }
+            long end = System.nanoTime();
+            System.out.println("Sequential execution time of iteration  is: " + (end - start) + " ns");
 
-        if (!wrongResult) {
+            if (!wrongResult) {
             System.out.println("Test success");
         } else {
             System.out.println("Result is wrong");
         }
+    }
     }
 }
