543664a9dab5665c1dab1f534aab6e2aee28d8c1
Juan Fumero <juan.fumero@manchester.ac.uk>
2018-08-29 13:36:23 +0200

    Fixed wrong javadoc from AMD blackscholes

--- benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/ComputeKernels.java
+++ benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/ComputeKernels.java
@@ -171,15 +171,11 @@ public class ComputeKernels {
     /*
      * @brief Calculates the call and put prices by using Black Scholes model
      * 
-     * @param randArray array of random values of current option price @param
-     * sigma Array of random values sigma @param k Array of random values strike
-     * price
+     * @param randArray array of random values of current option price
      * 
-     * @param put array of random values of expiration time @param r Array of
-     * random values of risk free interest rate @param width Width of call price
-     * or put price array @param call Array of calculated call price values
+     * @param out array of calculated put price values
      * 
-     * @param call array of calculated put price values
+     * @param call array of calculated call price values
      */
     public static void blackscholes(final float[] randArray, final float[] put, final float[] call) {
         for (@Parallel int gid = 0; gid < call.length; gid++) {
