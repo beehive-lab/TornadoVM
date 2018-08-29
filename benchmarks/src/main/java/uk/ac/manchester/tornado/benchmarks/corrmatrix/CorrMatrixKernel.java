edb3146beeb59076691bcbdb72e7a63534a57d10
Juan Fumero <juan.fumero@manchester.ac.uk>
2018-08-29 15:00:30 +0200

    Copyright CorrMatrixKernel from Aparapi fixed

--- benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/corrmatrix/CorrMatrixKernel.java
+++ benchmarks/src/main/java/uk/ac/manchester/tornado/benchmarks/corrmatrix/CorrMatrixKernel.java
@@ -1,24 +1,30 @@
-/*
- * Copyright (c) 2013-2018, APT Group, School of Computer Science,
- * The University of Manchester.
- * 
+/**
+ * Copyright (c) 2016 - 2018 Syncleus, Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
  */
 package uk.ac.manchester.tornado.benchmarks.corrmatrix;
 
 import uk.ac.manchester.tornado.api.annotations.Parallel;
 
+/**
+ * This material was prepared as an account of work sponsored by an agency of the United States Government.  
+ * Neither the United States Government nor the United States Department of Energy, nor Battelle, nor any of 
+ * their employees, nor any jurisdiction or organization that has cooperated in the development of these materials, 
+ * makes any warranty, express or implied, or assumes any legal liability or responsibility for the accuracy, 
+ * completeness, or usefulness or any information, apparatus, product, software, or process disclosed, or represents
+ * that its use would not infringe privately owned rights.
+ */
 /**
  * This kernel attempts to re-implement the Lucene OpenBitSet functionality on a
  * GPU
