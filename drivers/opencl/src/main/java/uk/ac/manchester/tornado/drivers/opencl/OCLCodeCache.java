8ebf6c72b6152e2baf4c3e223a5a3e298e7a1076
Michalis <mikepapadim@hotmail.com>
2018-09-18 13:29:22 +0100

    [toolchain] First working version

--- drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/OCLCodeCache.java
+++ drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/OCLCodeCache.java
@@ -168,7 +168,6 @@ public class OCLCodeCache {
 
             // For each entry, we should add also an entry for lookup-buffer
             String device = taskAndDeviceInfo.split("\\.")[2];
-            System.out.println("Device in process : -->" + device + "\n");
             String kernelName = "oclbackend.lookupBufferAddress." + device;
             precompiledBinariesPerDevice.put(kernelName, binaryFile);
         }
@@ -226,7 +225,6 @@ public class OCLCodeCache {
         if (precompiledBinariesPerDevice != null) {
             return precompiledBinariesPerDevice.get(taskName);
         } else {
-
             return null;
         }
     }
@@ -319,8 +317,8 @@ public class OCLCodeCache {
             if (RuntimeUtilities.ifFileExists(f)) {
                 return installEntryPointForBinaryForFPGAs(path, LOOKUP_BUFFER_KERNEL_NAME);
             } else {
-                RuntimeUtilities.sysCall(cmd);
-                RuntimeUtilities.sysCall(cmdRename);
+                RuntimeUtilities.sysCall(cmd, true);
+                RuntimeUtilities.sysCall(cmdRename, true);
             }
             return installEntryPointForBinaryForFPGAs(resolveFPGADir(), LOOKUP_BUFFER_KERNEL_NAME);
         } else {
@@ -511,6 +509,7 @@ public class OCLCodeCache {
             error("Empty input binary: %s (%s)", file);
         }
         try {
+            System.out.println("Path: " + lookupPath + "  " + "Entrypoint " + entrypoint + "\n");
             final byte[] binary = Files.readAllBytes(lookupPath);
             lookupCode = installBinary(entrypoint, binary);
         } catch (OCLException | IOException e) {
