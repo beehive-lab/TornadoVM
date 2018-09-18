b55f0b990c0a5a1ba2eced2e48831a9bbd59f5d2
Michalis <mikepapadim@hotmail.com>
2018-09-18 17:45:56 +0100

    [toolchain] First complete example

--- drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/OCLCodeCache.java
+++ drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/OCLCodeCache.java
@@ -226,7 +226,6 @@ public class OCLCodeCache {
         if (precompiledBinariesPerDevice != null) {
             return precompiledBinariesPerDevice.get(taskName);
         } else {
-
             return null;
         }
     }
