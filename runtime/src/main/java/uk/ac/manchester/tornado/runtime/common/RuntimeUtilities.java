0a95fcf1109ff51c76bd99f33bb763c0ecad4f9c
Michalis Papadimitriou <mikepapadim@hotmail.com>
2018-10-23 16:57:20 +0100

    [toolchain] Move write files to runtime utils

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/RuntimeUtilities.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/RuntimeUtilities.java
@@ -26,9 +26,11 @@
 package uk.ac.manchester.tornado.runtime.common;
 
 import static uk.ac.manchester.tornado.runtime.common.Tornado.error;
+import static uk.ac.manchester.tornado.runtime.common.Tornado.info;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
@@ -382,6 +384,15 @@ public class RuntimeUtilities {
             t.printStackTrace();
         }
 
+    }
+
+    public static void writeToFile(String file, byte[] binary) {
+        info("dumping binary %s", file);
+        try (FileOutputStream fis = new FileOutputStream(file);) {
+            fis.write(binary);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private RuntimeUtilities() {
