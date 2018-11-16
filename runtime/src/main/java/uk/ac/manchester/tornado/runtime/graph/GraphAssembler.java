6a51dce4efce584a7ac784e8a96645beb4f7f585
Juan Fumero <juan.fumero@manchester.ac.uk>
2018-11-16 09:44:34 +0100

    minor clean-up

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/graph/GraphAssembler.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/graph/GraphAssembler.java
@@ -34,18 +34,17 @@ public class GraphAssembler {
 
     // @formatter:off 
     public enum TornadoVMBytecodes {
-        
-        ALLOCATE  ((byte)10),   // ALLOCATE(obj,dest)
-        COPY_IN   ((byte)11),   // COPY(obj, src, dest)
-        STREAM_IN ((byte)12),   // STREAM_IN(obj, src, dest)
-        STREAM_OUT((byte)13),   // STREAM_OUT(obj, src, dest)
-        STREAM_OUT_BLOCKING((byte)14), // STREAM_OUT(obj, src, dest)
-        LAUNCH    ((byte)15),   // LAUNCH(dep list index)
+        ALLOCATE  ((byte)10),           // ALLOCATE(obj,dest)
+        COPY_IN   ((byte)11),           // COPY(obj, src, dest)
+        STREAM_IN ((byte)12),           // STREAM_IN(obj, src, dest)
+        STREAM_OUT((byte)13),           // STREAM_OUT(obj, src, dest)
+        STREAM_OUT_BLOCKING((byte)14),  // STREAM_OUT(obj, src, dest)
+        LAUNCH    ((byte)15),           // LAUNCH(dep list index)
         BARRIER   ((byte)16),
         SETUP     ((byte)17),  
-        BEGIN     ((byte)18),   // BEGIN(num contexts, num stacks, num dep lists)
-        ADD_DEP   ((byte)19),   // ADD_DEP(list index)
-        CONTEXT   ((byte)20),   // CONTEXT(ctx)
+        BEGIN     ((byte)18),           // BEGIN(num contexts, num stacks, num dep lists)
+        ADD_DEP   ((byte)19),           // ADD_DEP(list index)
+        CONTEXT   ((byte)20),           // CONTEXT(ctx)
         END       ((byte)21),
         CONSTANT_ARG ((byte)22),
         REFERENCE_ARG((byte)23);
@@ -59,33 +58,8 @@ public class GraphAssembler {
         public byte index() {
             return index;
         }
-        
     }
     // @formatter:on
-
-    // public final static byte ALLOCATE = 20; // ALLOCATE(obj,dest)
-    // public final static byte COPY_IN = 21; // COPY(obj, src, dest)
-    // public final static byte STREAM_IN = 22; // STREAM_IN(obj, src, dest)
-    // public final static byte STREAM_OUT = 23; // STREAM_OUT(obj, src, dest)
-    // public final static byte STREAM_OUT_BLOCKING = 24; // STREAM_OUT(obj,
-    // src,
-    // // dest)
-    //
-    // public final static byte LAUNCH = 28; // LAUNCH(dep list index)
-    //
-    // public final static byte BARRIER = 50;
-    //
-    // public final static byte SETUP = 10; // BEGIN(num contexts, num stacks,
-    // num
-    // // dep lists)
-    //
-    // public final static byte BEGIN = 11;
-    // public final static byte ADD_DEP = 12; // ADD_DEP(list index)
-    // public final static byte CONTEXT = 13; // CONTEXT(ctx)
-    // public final static byte END = 0;
-    //
-    // public final static byte CONSTANT_ARG = 32;
-    // public final static byte REFERENCE_ARG = 33;
 
     private final ByteBuffer buffer;
 
