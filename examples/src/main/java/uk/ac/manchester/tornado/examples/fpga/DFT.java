48420f023a76221c36cca12b4efc5b942e10eff1
Michalis Papadimitriou <mikepapadim@hotmail.com>
2018-11-30 13:48:59 +0000

    Add dft version with multiple policies

--- examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/DFT.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/DFT.java
@@ -20,7 +20,8 @@ package uk.ac.manchester.tornado.examples.fpga;
 
 import static uk.ac.manchester.tornado.api.collections.math.TornadoMath.abs;
 
-import uk.ac.manchester.tornado.api.*;
+import uk.ac.manchester.tornado.api.Policy;
+import uk.ac.manchester.tornado.api.TaskSchedule;
 import uk.ac.manchester.tornado.api.annotations.Parallel;
 import uk.ac.manchester.tornado.api.collections.math.TornadoMath;
 
@@ -67,7 +68,7 @@ public class DFT {
             long t2 = System.nanoTime();
 
             long seqTime = t2 - t1;
-//            System.out.println("Sequential time: " + seqTime + "\n");
+            // System.out.println("Sequential time: " + seqTime + "\n");
 
         }
 
@@ -90,6 +91,10 @@ public class DFT {
     public static void main(String[] args) {
 
         size = Integer.parseInt(args[0]);
+        String executionType = args[1];
+        int iterations = Integer.parseInt(args[2]);
+
+        long end,start;
 
         inReal = new float[size];
         inImag = new float[size];
@@ -107,12 +112,26 @@ public class DFT {
         graph = new TaskSchedule("s0");
         graph.task("t0", DFT::computeDft, inReal, inImag, outReal, outImag, inputSize);
         graph.streamOut(outReal, outImag);
-        //aph.warmup();
 
-        graph.executeWithProfilerSequential(Policy.PERFORMANCE);
-	//for (int i = 0; i < 10; i++) {
-        //    graph.execute();
-        //}
+        for (int i = 0; i < iterations; i++) {
+            switch (executionType) {
+                case "performance":
+                    start = System.nanoTime();
+                    graph.executeWithProfilerSequential(Policy.PERFORMANCE);
+                    end = System.nanoTime();
+                    break;
+                case "end":
+                    start = System.nanoTime();
+                    graph.executeWithProfilerSequential(Policy.END_2_END);
+                    end = System.nanoTime();
+                    break;
+                default:
+                    start = System.nanoTime();
+                    graph.execute();
+                    end = System.nanoTime();
+            }
+            System.out.println("End to end time:  " + (end - start) + " ns" + " \n");
+        }
 
         if (CHECK_RESULT) {
             if (validate()) {
