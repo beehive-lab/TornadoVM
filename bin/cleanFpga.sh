823cd96c7139ecf5da65345d11b473559b02a7e9
Michalis <mikepapadim@hotmail.com>
2019-01-31 17:24:26 +0000

    Add copy of the input source to hls

--- bin/cleanFpga.sh
+++ bin/cleanFpga.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mv fpga-source-comp/lookupBufferAddress.cl fpga-source-comp/backup_source.cl
 rm -rf /home/admin/Tornado/tornado/fpga-source-comp/lookupBufferAddress
 mv fpga-source-comp/lookupBufferAddress.aocx fpga-source-comp/lookupBufferAddress
 
