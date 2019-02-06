bac0c60fef605bce1b1773fec252c44da74b96b0
Michalis <mikepapadim@hotmail.com>
2019-02-06 14:25:59 +0000

    Remove hardcoded path from fpga script

--- bin/cleanFpga.sh
+++ bin/cleanFpga.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mv fpga-source-comp/lookupBufferAddress.cl fpga-source-comp/backup_source.cl
-rm -rf /home/admin/Tornado/tornado/fpga-source-comp/lookupBufferAddress
+rm -rf ${TORNADO_ROOT}/fpga-source-comp/lookupBufferAddress
 mv fpga-source-comp/lookupBufferAddress.aocx fpga-source-comp/lookupBufferAddress
 
 
