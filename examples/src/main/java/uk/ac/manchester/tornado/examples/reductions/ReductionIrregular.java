08f49c70bd3710f33429b1b26007ea262b5d691b
Juan Fumero <juan.fumero@manchester.ac.uk>
2019-05-20 12:17:03 +0200

    Example reduction with non-power 2 added

---
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/reductions/ReductionIrregular.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2013-2019, APT Group, School of Computer Science,
+ * The University of Manchester.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package uk.ac.manchester.tornado.examples.reductions;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import uk.ac.manchester.tornado.api.TaskSchedule;
+import uk.ac.manchester.tornado.api.annotations.Parallel;
+import uk.ac.manchester.tornado.api.annotations.Reduce;
+
+public class ReductionIrregular {
+
+    private static void reduceFloats(float[] input, @Reduce float[] output) {
+        for (@Parallel int i = 0; i < input.length; i++) {
+            output[0] += input[i];
+        }
+    }
+
+    private void run(final int inputSize) {
+        float[] input = new float[inputSize];
+        float[] result = new float[] { 0.0f };
+        Random r = new Random(101);
+        IntStream.range(0, inputSize).parallel().forEach(i -> {
+            input[i] = r.nextFloat();
+        });
+
+        //@formatter:off
+        TaskSchedule task = new TaskSchedule("s0")
+            .streamIn(input)
+            .task("t0", ReductionIrregular::reduceFloats, input, result)
+            .streamOut(result);
+        //@formatter:on
+
+        float[] sequential = new float[1];
+        ReductionIrregular.reduceFloats(input, sequential);
+
+        ArrayList<Long> timers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            task.execute();
+            long end = System.nanoTime();
+            timers.add((end - start));
+
+            if (Math.abs(sequential[0] - result[0]) > 0.01f) {
+                System.out.println("Result is not correct - iteration: " + i + " expected: " + sequential[0] + " but found: " + result[0]);
+            } else {
+                System.out.println("Iteration: " + i + " is correct");
+            }
+
+        }
+
+        System.out.println("Median TotalTime: " + Stats.computeMedian(timers));
+
+    }
+
+    public static void main(String[] args) {
+        int inputSize = 256;
+        if (args.length > 0) {
+            inputSize = Integer.parseInt(args[0]);
+        }
+        System.out.println("Size = " + inputSize);
+        new ReductionIrregular().run(inputSize);
+    }
+}
