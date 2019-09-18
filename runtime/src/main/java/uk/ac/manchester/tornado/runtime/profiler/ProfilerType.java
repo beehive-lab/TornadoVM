63257dd693f4037b3658504d5b84f91b1dba29f9
Juan Fumero <juan.fumero@manchester.ac.uk>
2019-09-18 10:47:15 +0200

    License header to new profile classes

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/profiler/ProfilerType.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/profiler/ProfilerType.java
@@ -1,13 +1,39 @@
+/*
+ * This file is part of Tornado: A heterogeneous programming framework:
+ * https://github.com/beehive-lab/tornado
+ *
+ * Copyright (c) 2019, APT Group, School of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 package uk.ac.manchester.tornado.runtime.profiler;
 
 public enum ProfilerType {
 
+    /**
+     * Profile-type that helps to classify all timers
+     */
     // @formatter:off
-    TOTAL_TIME("Total Time"),
     COMPILE_TIME("Compilation Time"),
-    KERNEL_TIME("Kernel Time"),
     COPY_IN_TIME("CopyIn Time"),
-    COPY_OUT_TIME("CopyOut Time");
+    COPY_OUT_TIME("CopyOut Time"),
+    KERNEL_TIME("Kernel Time"),
+    TOTAL_TIME("Total Time");
     // @formatter:on
 
     String description;
