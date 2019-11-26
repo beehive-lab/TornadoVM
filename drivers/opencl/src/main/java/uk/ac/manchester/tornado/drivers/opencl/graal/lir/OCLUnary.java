391183adcd6933a8204a0dd3852f5ecc7e4bd979
Juan Fumero <juan.fumero@manchester.ac.uk>
2019-11-26 17:42:55 +0100

    [refactor] relative address code emision

--- drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/lir/OCLUnary.java
+++ drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/lir/OCLUnary.java
@@ -163,10 +163,13 @@ public class OCLUnary {
             this.needsBase = needsBase;
         }
 
+        private boolean shouldEmitRelativeAddress() {
+            return needsBase || (!(base.memorySpace == OCLMemorySpace.LOCAL) && OPENCL_USE_RELATIVE_ADDRESSES);
+        }
+
         @Override
         public void emit(OCLCompilationResultBuilder crb, OCLAssembler asm) {
-
-            if (needsBase || (!(base.memorySpace == OCLMemorySpace.LOCAL) && OPENCL_USE_RELATIVE_ADDRESSES)) {
+            if (shouldEmitRelativeAddress()) {
                 asm.emitSymbol(ADDRESS_OF);
                 asm.emit(base.name);
                 asm.emitSymbol(SQUARE_BRACKETS_OPEN);
