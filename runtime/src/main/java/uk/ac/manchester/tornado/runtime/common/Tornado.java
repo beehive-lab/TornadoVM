07b143567b2bbd799e5d6b99642f8ac8880370c0
Juan Fumero <juan.fumero@manchester.ac.uk>
2019-11-27 13:20:32 +0100

    [WIP] old options removed

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/Tornado.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/Tornado.java
@@ -74,7 +74,6 @@ public final class Tornado implements TornadoCI {
     public static final boolean VM_USE_DEPS = Boolean.parseBoolean(Tornado.getProperty("tornado.vm.deps", "False"));
 
     public static boolean FORCE_BLOCKING_API_CALLS = false;
-    public static final boolean ENABLE_PARALLELIZATION = Boolean.parseBoolean(Tornado.getProperty("tornado.kernels.parallelize", "True"));
     public static final boolean ENABLE_VECTORS = Boolean.parseBoolean(settings.getProperty("tornado.vectors.enable", "True"));
     public static final boolean TORNADO_ENABLE_BIFS = Boolean.parseBoolean(settings.getProperty("tornado.bifs.enable", "False"));
     public static final boolean DEBUG = Boolean.parseBoolean(settings.getProperty("tornado.debug", "True"));
@@ -82,9 +81,6 @@ public final class Tornado implements TornadoCI {
 
     public static final boolean SHOULD_LOAD_RMI = Boolean.parseBoolean(settings.getProperty("tornado.rmi.enable", "false"));
     public final static boolean TIME_IN_NANOSECONDS = Boolean.parseBoolean(System.getProperty("tornado.ns.time", "true"));
-
-    public static boolean REMOVE_OUTER_LOOPS = Boolean.parseBoolean(getProperty("tornado.assembler.removeloops", "False"));
-    public final static boolean ACCELERATOR_IS_GPU = Boolean.parseBoolean(getProperty("tornado.opencl.accelerator.asgpu", "True"));
 
     public final static boolean FPGA_EMULATION = isFPGAEmulation();
 
