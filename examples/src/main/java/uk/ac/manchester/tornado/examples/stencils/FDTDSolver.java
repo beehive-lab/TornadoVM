2df4b121d570bb440ac46a414a09fd53c0d84972
Michalis Papadimitriou <mikepapadim@hotmail.com>
2020-02-08 19:01:58 +0000

    [feat] Add tornado version of fdtd solver

--- examples/src/main/java/uk/ac/manchester/tornado/examples/stencils/FDTDSolver.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/stencils/FDTDSolver.java
@@ -18,6 +18,8 @@
 
 package uk.ac.manchester.tornado.examples.stencils;
 
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+import uk.ac.manchester.tornado.api.TaskSchedule;
 import uk.ac.manchester.tornado.api.annotations.Parallel;
 
 import java.util.ArrayList;
@@ -30,35 +32,35 @@ public class FDTDSolver {
     final static int PB_N = 1024;
     final static int ITERATIONS = 31;
 
-    // private static void kernelOne() {
-    // for (@Parallel int i = 0; i < sizeX; i++) {
-    // for (@Parallel int j = 0; j < sizeY; j++) {
-    // if (i == 0) {
-    //
-    // } else {
-    //
-    // }
-    // }
-    // }
-    // }
-    //
-    // private static void kernelTwo() {
-    // for (@Parallel int i = 0; i < sizeX; i++) {
-    // for (@Parallel int j = 0; j < sizeY; j++) {
-    //
-    // }
-    // }
-    // }
-    //
-    // private static void kernelThree() {
-    // for (@Parallel int i = 0; i < sizeX; i++) {
-    // for (@Parallel int j = 0; j < sizeY; j++) {
-    //
-    // }
-    // }
-    // }
+    public static void kernelOne(int nx, int ny, float[] fict, float[] ey, float[] hz, int step) {
+        for (@Parallel int i = 0; i < nx; i++) {
+            for (@Parallel int j = 0; j < ny; j++) {
+                if (i == 0) {
+                    ey[0 * nx + j] = fict[step];
+                } else {
+                    ey[i * nx + j] = (float) (ey[i * nx + j] - 0.5 * (hz[i * nx + j] - hz[(i - 1) * nx + j]));
+                }
+            }
+        }
+    }
 
-    private static float[]  fdtd(int tmax, int nx, int ny, float[] fict, float[] ex, float[] ey, float[] hz) {
+    public static void kernelTwo(int nx, int ny, float[] ex, float[] hz) {
+        for (@Parallel int i = 0; i < nx; i++) {
+            for (@Parallel int j = 1; j < ny; j++) {
+                ex[i * nx + j] = (float) (ex[i * nx + j] - 0.5 * (hz[i * nx + j] - hz[i * nx + (j - 1)]));
+            }
+        }
+    }
+
+    public static void kernelThree(int nx, int ny, float[] ex, float[] hz, float[] ey) {
+        for (@Parallel int i = 0; i < nx - 1; i++) {
+            for (@Parallel int j = 0; j < ny - 1; j++) {
+                hz[i * nx + j] = (float) (hz[i * nx + j] - 0.7 * (ex[i * nx + (j + 1)] - ex[i * nx + j] + ey[(i + 1) * nx + j] - ey[i * nx + j]));
+            }
+        }
+    }
+
+    private static float[] fdtd(int tmax, int nx, int ny, float[] fict, float[] ex, float[] ey, float[] hz) {
         for (int t = 0; t < tmax; t++) {
             for (int j = 0; j < ny; j++) {
                 ey[0 * nx + j] = fict[t];
@@ -112,11 +114,21 @@ public class FDTDSolver {
     }
 
     public static void main(String[] args) {
-        int size,steps,iterations,input;
+        int size = 0,steps,iterations,input;
 
-        size = (args.length == 1) ? Integer.parseInt(args[0]) : PB_N;
-        steps = (args.length == 2) ? Integer.parseInt(args[1]) : PB_STEPS;
-        iterations = (args.length == 3) ? Integer.parseInt(args[2]) : ITERATIONS;
+        size = PB_N;
+        steps = PB_STEPS;
+        iterations = ITERATIONS;
+        try {
+            size = (args[0] != null) ? Integer.parseInt(args[0]) : PB_N;
+            steps = (args[1] != null) ? Integer.parseInt(args[1]) : PB_STEPS;
+            iterations = (args[2] != null) ? Integer.parseInt(args[2]) : ITERATIONS;
+        } catch (NullPointerException e) {
+            System.out.println("Null args");
+        }
+
+        System.out.println("Size : " + size + " Step : " + steps + " Iterarions : " + iterations);
+        System.out.println("args   = = " + args.length);
 
         LinkedList<float[]> initializedArrays = initArrays(steps, size, size);
 
@@ -128,11 +140,40 @@ public class FDTDSolver {
         long start = 0L;
         long end = 0L;
 
-        start = System.nanoTime();
-        hz = fdtd(steps,size,size,fict, ex,ey,hz);
-        end = System.nanoTime();
+        StringBuilder se = new StringBuilder();
+        StringBuilder par = new StringBuilder();
+        for (int i = 0; i < iterations; i++) {
+            System.gc();
+            start = System.nanoTime();
+            hz = fdtd(steps, size, size, fict, ex, ey, hz);
+            end = System.nanoTime();
+            se.append("\tSequential execution time of iteration is: " + (end - start) + " ns \n");
+        }
 
-        System.out.println(Arrays.toString(hz));
+        TaskSchedule graph = new TaskSchedule("s0");
+        for (int i = 0; i < iterations; i++) {
+            System.gc();
+            start = System.nanoTime();
+            for (int step = 0; step < steps; step++) {
+                // graph.streamIn(size, size, fict, ey, hz, step);
+                //@formatter:off
+                graph
+                        .task("t0", FDTDSolver::kernelOne, size, size, fict, ey, hz, step)
+                        .task("t1", FDTDSolver::kernelTwo, size, size, ex, hz)
+                        .task("t2", FDTDSolver::kernelThree, size, size, ex, hz, ey);
+                //@formatter:on
+                graph.execute();
+                graph.streamOut(hz);
+            }
+            end = System.nanoTime();
+            graph.syncObject(hz);
+            par.append("\tTornado execution time of iteration is: " + (end - start) + " ns \n");
+        }
+
+        System.out.println(se);
+        System.out.println(par);
+        // System.out.println("\tVerify : " + verify(a, aSeq));
+        // System.out.println(Arrays.toString(hz));
     }
 
 }
