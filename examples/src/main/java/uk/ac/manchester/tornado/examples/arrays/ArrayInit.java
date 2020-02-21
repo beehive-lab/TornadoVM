381b21d2f25e897fb1890a3d636120ed0f5309d8
Gyorgy Rethy <rethygy0410@gmail.com>
2020-02-21 16:15:30 +0000

    Add a very simple example to test the PTX Compiler with.
    This can be deleted later on.

---
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/arrays/ArrayInit.java
@@ -0,0 +1,33 @@
+package uk.ac.manchester.tornado.examples.arrays;
+
+import uk.ac.manchester.tornado.api.TaskSchedule;
+import uk.ac.manchester.tornado.api.annotations.Parallel;
+
+import java.util.Arrays;
+
+//TODO: These examples seem pretty simple and dumb I just used these for testing
+// and therefore could be deleted afterwards
+public class ArrayInit {
+
+    public static void init(int[] a) {
+        for (@Parallel int i = 0; i < a.length; i++) {
+            a[i] = 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        final int numElements = 8;
+        int[] test = new int[numElements];
+
+        Arrays.fill(test, 0);
+
+        System.out.println("Before: " + Arrays.toString(test));
+
+        new TaskSchedule("s0")
+                .task("t0", ArrayInit::init, test)
+                .streamOut(test)
+                .execute();
+
+        System.out.println("After: " + Arrays.toString(test));
+    }
+}
