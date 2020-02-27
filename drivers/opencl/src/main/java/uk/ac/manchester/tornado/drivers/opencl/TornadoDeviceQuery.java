966e4b715210c1827ecc89a66f958681c8732de3
Juan Fumero <juan.fumero@manchester.ac.uk>
2020-02-27 09:40:11 +0100

    Refactor TornadoDeviceQuery

--- drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/TornadoDeviceQuery.java
+++ drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/TornadoDeviceQuery.java
@@ -32,59 +32,55 @@ import uk.ac.manchester.tornado.runtime.TornadoAcceleratorDriver;
 import uk.ac.manchester.tornado.runtime.TornadoCoreRuntime;
 
 /**
- * Run:
+ * Program to query all devices reachable from TornadoVM.
  * 
+ * Run as follows:
  * <p>
  * <code>
- * $ tornado uk.ac.manchester.tornado.drivers.opencl.TornadoDeviceOutput
+ * $ tornado uk.ac.manchester.tornado.drivers.opencl.TornadoDeviceQuery
  * </code>
  * </p>
  *
  */
 public class TornadoDeviceQuery {
 
-    public static void main(String[] args) {
-
-        String verbosity = "";
-
-        if (args.length != 0) {
-            verbosity = args[0];
-        }
-
-        StringBuffer bufferDriversAndPlatforms = new StringBuffer().append("\n");
-        StringBuffer bufferDevices = new StringBuffer();
-        final int numDrivers = TornadoCoreRuntime.getTornadoRuntime().getNumDrivers();
-        bufferDriversAndPlatforms.append("Number of Tornado drivers: " + numDrivers + "\n");
-
-        for (int driverIndex = 0; driverIndex < numDrivers; driverIndex++) {
-            final TornadoAcceleratorDriver driver = TornadoCoreRuntime.getTornadoRuntime().getDriver(driverIndex);
-            final int numDevices = driver.getDeviceCount();
-            bufferDriversAndPlatforms.append("Total number of devices  : " + numDevices + "\n");
-            for (int deviceIndex = 0; deviceIndex < numDevices; deviceIndex++) {
-                bufferDevices.append("Tornado device=" + driverIndex + ":" + deviceIndex + "\n");
-                bufferDevices.append("\t" + driver.getDevice(deviceIndex)).append("\n");
-                if (verbosity.equals("verbose")) {
-                    bufferDevices.append("\t\t" + "Global Memory Size: " + formatSize(driver.getDevice(deviceIndex).getMaxGlobalMemory()) + "\n");
-                    bufferDevices.append("\t\t" + "Local Memory Size: " + formatSize(driver.getDevice(deviceIndex).getDeviceLocalMemorySize()) + "\n");
-                    bufferDevices.append("\t\t" + "Workgroup Dimensions: " + driver.getDevice(deviceIndex).getDeviceMaxWorkgroupDimensions().length + "\n");
-                    bufferDevices.append("\t\t" + "Max WorkGroup Configuration: " + Arrays.toString(driver.getDevice(deviceIndex).getDeviceMaxWorkgroupDimensions()) + "\n");
-                    bufferDevices.append("\t\t" + "Device OpenCL C version: " + driver.getDevice(deviceIndex).getDeviceOpenCLCVersion() + "\n");
-                } else {
-                    bufferDevices.append("\n");
-                }
-            }
-        }
-
-        System.out.println(bufferDriversAndPlatforms.toString());
-        bufferDevices.setLength(bufferDevices.length() - 1);
-        System.out.println(bufferDevices.toString());
-    }
-
-    public static String formatSize(long v) {
+    private static String formatSize(long v) {
         if (v < 1024) {
             return v + " B";
         }
         int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
         return String.format("%.1f %sB", (double) v / (1L << (z * 10)), " KMGTPE".charAt(z));
+    }
+
+    public static void main(String[] args) {
+
+        String verboseFlag = "";
+
+        if (args.length != 0) {
+            verboseFlag = args[0];
+        }
+
+        StringBuffer deviceInfoBuffer = new StringBuffer().append("\n");
+        final int numDrivers = TornadoCoreRuntime.getTornadoRuntime().getNumDrivers();
+        deviceInfoBuffer.append("Number of Tornado drivers: " + numDrivers + "\n");
+
+        for (int driverIndex = 0; driverIndex < numDrivers; driverIndex++) {
+            final TornadoAcceleratorDriver driver = TornadoCoreRuntime.getTornadoRuntime().getDriver(driverIndex);
+            final int numDevices = driver.getDeviceCount();
+            deviceInfoBuffer.append("Total number of devices  : " + numDevices + "\n");
+            for (int deviceIndex = 0; deviceIndex < numDevices; deviceIndex++) {
+                deviceInfoBuffer.append("Tornado device=" + driverIndex + ":" + deviceIndex + "\n");
+                deviceInfoBuffer.append("\t" + driver.getDevice(deviceIndex)).append("\n");
+                if (verboseFlag.equals("verbose")) {
+                    deviceInfoBuffer.append("\t\t" + "Global Memory Size: " + formatSize(driver.getDevice(deviceIndex).getMaxGlobalMemory()) + "\n");
+                    deviceInfoBuffer.append("\t\t" + "Local Memory Size: " + formatSize(driver.getDevice(deviceIndex).getDeviceLocalMemorySize()) + "\n");
+                    deviceInfoBuffer.append("\t\t" + "Workgroup Dimensions: " + driver.getDevice(deviceIndex).getDeviceMaxWorkgroupDimensions().length + "\n");
+                    deviceInfoBuffer.append("\t\t" + "Max WorkGroup Configuration: " + Arrays.toString(driver.getDevice(deviceIndex).getDeviceMaxWorkgroupDimensions()) + "\n");
+                    deviceInfoBuffer.append("\t\t" + "Device OpenCL C version: " + driver.getDevice(deviceIndex).getDeviceOpenCLCVersion() + "\n");
+                }
+                deviceInfoBuffer.append("\n");
+            }
+        }
+        System.out.println(deviceInfoBuffer.toString());
     }
 }
