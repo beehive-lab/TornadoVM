639d43422d246fb520cf542145a3261947388ac4
foutrisn <nikos.foutris@manchester.ac.uk>
2020-03-17 15:18:52 +0200

    Update licence from GPL to Apache

--- examples/src/main/java/uk/ac/manchester/tornado/examples/bufet/InputScanner.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/bufet/InputScanner.java
@@ -1,20 +1,21 @@
 /*
- * Copyright (c) 2020, APT Group, Department of Computer Science,
- * School of Engineering, The University of Manchester. All rights reserved.
+ * Copyright (c) 2013-2019, APT Group, School of Computer Science,
+ * The University of Manchester.
  *
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- * *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
+
 package uk.ac.manchester.tornado.examples.bufet;
 
 
