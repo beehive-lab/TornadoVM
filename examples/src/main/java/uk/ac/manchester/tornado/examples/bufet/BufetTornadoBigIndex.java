f5fa1d36951c8608b4142ef7ddf8888f347eecdd
foutrisn <nikos.foutris@manchester.ac.uk>
2020-03-18 00:35:18 +0200

    update license heading

--- examples/src/main/java/uk/ac/manchester/tornado/examples/bufet/BufetTornadoBigIndex.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/bufet/BufetTornadoBigIndex.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019, APT Group, School of Computer Science,
+ * Copyright (c) 2013-2020, APT Group, Department of Computer Science,
  * The University of Manchester.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -51,7 +51,7 @@ public class BufetTornadoBigIndex {
         }
         return map;
     }
-    
+
 
     //Generate a random index
     public static int[] generateRandom(int upperBound, int size) {
