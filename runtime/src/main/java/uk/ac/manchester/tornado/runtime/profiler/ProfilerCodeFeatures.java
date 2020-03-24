a41adb9d1956af250632978c6dea6b518a4b3c76
Michalis Papadimitriou <mikepapadim@hotmail.com>
2020-03-24 13:37:21 +0000

    [feat] Add enum to hold feature types

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/profiler/ProfilerCodeFeatures.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/profiler/ProfilerCodeFeatures.java
@@ -2,7 +2,7 @@
  * This file is part of Tornado: A heterogeneous programming framework:
  * https://github.com/beehive-lab/tornadovm
  *
- * Copyright (c) 2013-2019, APT Group, School of Computer Science,
+ * Copyright (c) 2013-2020, APT Group, Department of Computer Science,
  * The University of Manchester. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -20,23 +20,42 @@
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Authors: Michalis Papadimitriou
- *
  *
  */
 package uk.ac.manchester.tornado.runtime.profiler;
 
 public enum ProfilerCodeFeatures {
+
     // @formatter:off
-    FLOATING_READ,
-    GLOBAL_THREAD_ID,
-    IF,
-    INTEGER_SWITCH,
-    LOOP_BEGIN,
-    OCL_FPU_BINARY_INTRINSIC,
-    OCL_INT_BINARY_INTRINSIC, 
-    SWITCH_CASES,
-    VECTOR_LOAD_ELEMENT,
-    WRITE,
+    GLOBAL_LOADS ("Global Memory Loads"),
+    GLOBAL_STORES ("Global Memory Stores"),
+    LOCAL_LOADS ("Local Memory Loads"),
+    LOCAL_STORES ("Local Memory Stores"),
+    CONSTANT_LOADS ("Constant Memory Loads"),
+    CONSTANT_STORES ("Constant Memory Stores"),
+    PRIVATE_LOADS ("Private Memory Loads"),
+    PRIVATE_STORES ("Private Memory Stores"),
+    LOOPS ("Total Loops"),
+    PARALLEL_LOOPS ("Parallel Loops"),
+    IFS ("If Statements"),
+    SWITCH ("Switch Statements"),
+    CASE ("Switch Cases"),
+    VECTORS ("Vector Operations"),
+    INTEGER ("Integer Operations"),
+    BINARY ("Binary Operations"),
+    FLOATS ("Floating Operations"),
+    I_CMP ("Integer Comparison"),
+    F_CMP ("Floating Comparison");
     // @formatter:on
+
+    private String feature;
+
+    ProfilerCodeFeatures(String featureType) {
+        this.feature = featureType;
+    }
+
+    @Override
+    public String toString() {
+        return feature;
+    }
 }
