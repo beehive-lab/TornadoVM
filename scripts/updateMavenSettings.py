90de6bd0de6c6eef9ed50076f7904beb3e35f994
Juan Fumero <juan.fumero@manchester.ac.uk>
2020-04-02 15:59:51 +0200

    [install] maven template added

---
+++ scripts/updateMavenSettings.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+#
+# This file is part of Tornado: A heterogeneous programming framework: 
+# https://github.com/beehive-lab/tornadovm
+#
+# Copyright (c) 2020, APT Group, Department of Computer Science,
+# The University of Manchester. All rights reserved.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details (a copy is included in the LICENSE file that
+# accompanied this code).
+#
+# You should have received a copy of the GNU General Public License version
+# 2 along with this work; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+import os
+import re
+
+def updateMavenSettingsFile():
+
+    f = open("scripts/templates/settings.xml", "r")
+    settingsXML = f.read()
+    
+    javaHome = os.environ["JAVA_HOME"]
+    settingsXML = settingsXML.replace("$$JDKPATH$$", javaHome)
+
+    print "JAVA_HOME  : " + javaHome
+
+    ## Get the JDK Version
+    javaVersion = re.search(r"1\.(?:([\d._])+)", javaHome).group(0)
+    if javaVersion:
+        print "JVM_VERSION: " + javaVersion
+    settingsXML = settingsXML.replace("$$JDKVERSION$$", javaVersion)    
+    
+    ## Store the file in ~/.m2/settings.xml
+    home = os.environ["HOME"]
+    fout = open(home + "/.m2/settings.xml", "w")
+    fout.write(settingsXML)
+    fout.close()
+
+if __name__ == "__main__":
+    updateMavenSettingsFile()
