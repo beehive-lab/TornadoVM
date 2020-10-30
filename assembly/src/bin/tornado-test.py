1d3f46a71bf3cbd4f5a34c91315b843891315a85
gigiblender <florin.blanaru96@gmail.com>
2020-10-30 12:18:46 +0000

    [fix] tornado-test.py whitelist

--- assembly/src/bin/tornado-test.py
+++ assembly/src/bin/tornado-test.py
@@ -105,7 +105,7 @@ __TORNADO_TESTS_WHITE_LIST__ = [
 	"uk.ac.manchester.tornado.unittests.virtual.TestVirtualDeviceKernel#testVirtualDeviceKernelCPU",
     "uk.ac.manchester.tornado.unittests.virtual.TestVirtualDeviceFeatureExtraction#testVirtualDeviceFeaturesCPU",
     "uk.ac.manchester.tornado.unittests.virtual.TestVirtualDeviceFeatureExtraction#testVirtualDeviceFeaturesGPU",
-	"uk.ac.manchester.tornado.unittests.atomics.TestAtomics#testAtomic12"
+	"uk.ac.manchester.tornado.unittests.atomics.TestAtomics#testAtomic12",
 	"uk.ac.manchester.tornado.unittests.atomics.TestAtomics#testAtomic15"
 ]
 
