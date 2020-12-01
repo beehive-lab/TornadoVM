c8d2db3ca6aece4f4c2f85addc097babe46acb01
grfrost <c9807bc1eddefcbc1d16984e7fa78a0e255aac92>
2020-12-01 16:08:42 +0000

    fixes suggested by Juan https://github.com/beehive-lab/TornadoVM/pull/41

--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/profiler/ChromeEventJSonWriter.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/profiler/ChromeEventJSonWriter.java
@@ -1,32 +1,55 @@
+
+/*
+ * This file is part of Tornado: A heterogeneous programming framework:
+ * https://github.com/beehive-lab/tornadovm
+ *
+ * Copyright (c) 2020, APT Group, Department of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * GNU Classpath is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * GNU Classpath is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Classpath; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module.  An independent module is a module which is not derived from
+ * or based on this library.  If you modify this library, you may extend
+ * this exception to your version of the library, but you are not
+ * obligated to do so.  If you do not wish to do so, delete this
+ * exception statement from your version.
+ *
+ * Author Gary Frost
+ */
 package uk.ac.manchester.tornado.api.profiler;
 
-import sun.management.VMManagement;
-
 import java.io.File;
-import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 
 public class ChromeEventJSonWriter extends JSonWriter<ChromeEventJSonWriter> {
-    static private int pid = 0;
     ContentWriter NO_ARGS = null;
 
     ChromeEventJSonWriter() {
         super();
-        // Java 9 we could use long pid = ProcessHandle.current().pid();
-        // For java 8 we must go through some hoops.
-        try {
-            RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
-            Field jvm = runtime.getClass().getDeclaredField("jvm");
-            jvm.setAccessible(true);
-            VMManagement mgmt = (VMManagement) jvm.get(runtime);
-            java.lang.reflect.Method pid_method = mgmt.getClass().getDeclaredMethod("getProcessId");
-            pid_method.setAccessible(true);
-            pid = (Integer) pid_method.invoke(mgmt);
-        } catch (NoSuchFieldException | IllegalAccessException | NoSuchMethodException | InvocationTargetException nsf) {
-            // well we tried. Lets just use the default 0
-        }
         objectStart();
         arrayStart("traceEvents");
         object(() -> {
@@ -41,7 +64,7 @@ public class ChromeEventJSonWriter extends JSonWriter<ChromeEventJSonWriter> {
     }
 
     JSonWriter pidAndTid() {
-        return kv("pid", pid).kv("tid", Thread.currentThread().getId());
+        return kv("pid", 0).kv("tid", Thread.currentThread().getId());
     }
 
     JSonWriter common(String phase, String name, String category) {
