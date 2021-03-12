ae051b363d6cbc85bb11b3798b3b3444864874e1
Thanos <tstratiko@gmail.com>
2021-03-12 15:45:29 +0200

    [feat] compute programs with TornadoVMContext features

--- examples/src/main/java/module-info.java
+++ examples/src/main/java/module-info.java
@@ -12,6 +12,7 @@ open module tornado.examples {
     exports uk.ac.manchester.tornado.examples.common;
     exports uk.ac.manchester.tornado.examples.compression;
     exports uk.ac.manchester.tornado.examples.compute;
+    exports uk.ac.manchester.tornado.examples.compute_tornadovmcontext;
     exports uk.ac.manchester.tornado.examples.dynamic;
     exports uk.ac.manchester.tornado.examples.fpga;
     exports uk.ac.manchester.tornado.examples.functional;
