b386c2168ef10625073b70810d41c13d06d2b42c
Thanos <tstratiko@gmail.com>
2021-03-23 12:56:18 +0200

    [test] Modified example for creation of prebuilt FPGA kernel

--- examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/VectorAddInt.java
+++ examples/src/main/java/uk/ac/manchester/tornado/examples/fpga/VectorAddInt.java
@@ -28,8 +28,8 @@ import uk.ac.manchester.tornado.api.annotations.Parallel;
  */
 public class VectorAddInt {
 
-    private static void vectorAdd(int[] a, int[] b, int[] c) {
-        for (@Parallel int i = 0; i < c.length; i++) {
+    private static void vectorAdd(int[] a, int[] b, int[] c, int size) {
+        for (@Parallel int i = 0; i < size; i++) {
             c[i] = a[i] + b[i];
         }
     }
@@ -50,13 +50,13 @@ public class VectorAddInt {
 
         //@formatter:off
         TaskSchedule graph = new TaskSchedule("s0")
-                .task("t0", VectorAddInt::vectorAdd, a, b, c)
+                .task("t0", VectorAddInt::vectorAdd, a, b, c, size)
         .streamOut(c);
         //@formatter:on
 
         for (int idx = 0; idx < 10; idx++) {
             graph.execute();
-            vectorAdd(a, b, result);
+            vectorAdd(a, b, result, size);
 
             boolean wrongResult = false;
             for (int i = 0; i < c.length; i++) {
