7f5507de75bf51737ad6706a337ab23d8f5972fa
Thanos <tstratiko@gmail.com>
2021-07-28 22:05:37 +0300

    [feat] Enabled a SchedulableTask to setGridScheduler of TaskMetaData

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/tasks/CompilableTask.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/tasks/CompilableTask.java
@@ -25,6 +25,7 @@
  */
 package uk.ac.manchester.tornado.runtime.tasks;
 
+import uk.ac.manchester.tornado.api.GridScheduler;
 import uk.ac.manchester.tornado.api.common.Access;
 import uk.ac.manchester.tornado.api.common.SchedulableTask;
 import uk.ac.manchester.tornado.api.common.TornadoDevice;
@@ -169,6 +170,11 @@ public class CompilableTask implements SchedulableTask {
     @Override
     public void setUseGridScheduler(boolean use) {
         meta.setUseGridScheduler(use);
+    }
+
+    @Override
+    public void setGridScheduler(GridScheduler gridScheduler) {
+        meta.setGridScheduler(gridScheduler);
     }
 
     @Override
