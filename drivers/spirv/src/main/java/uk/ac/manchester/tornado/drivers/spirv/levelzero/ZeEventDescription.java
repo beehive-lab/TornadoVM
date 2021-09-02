32e2db9b3686f12e9eb8f20fefe1211ff28dc063
Juan Fumero <juan.fumero@manchester.ac.uk>
2021-09-02 14:42:29 +0200

    [SPIRV][LevelZero] JNI lib sync with latest version

--- drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/levelzero/ZeEventDescription.java
+++ drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/levelzero/ZeEventDescription.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021, APT Group, Department of Computer Science,
+ * The University of Manchester.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package uk.ac.manchester.tornado.drivers.spirv.levelzero;
 
 /**
@@ -22,9 +46,9 @@ public class ZeEventDescription {
     private long index;
 
     /**
-     * [in] defines the scope of relevant cache hierarchies to flush on a
-     * signal action before the event is triggered.
-     * Must be 0 (default) or a valid combination of {@link ZeEventScopeFlags}
+     * [in] defines the scope of relevant cache hierarchies to flush on a signal
+     * action before the event is triggered. Must be 0 (default) or a valid
+     * combination of {@link ZeEventScopeFlags}
      *
      * The default behavior is synchronization within the command list only, no
      * additional cache hierarchies are flushed.
@@ -32,11 +56,11 @@ public class ZeEventDescription {
     private int signal;
 
     /**
-     * [in] defines the scope of relevant cache hierarchies to invalidate on
-     * a wait action after the event is complete.
-     * Must be 0 (default) or a valid combination of {@link ZeEventScopeFlags}
-     * default behavior is synchronization within the command list only, no
-     * additional cache hierarchies are invalidated.
+     * [in] defines the scope of relevant cache hierarchies to invalidate on a wait
+     * action after the event is complete. Must be 0 (default) or a valid
+     * combination of {@link ZeEventScopeFlags} default behavior is synchronization
+     * within the command list only, no additional cache hierarchies are
+     * invalidated.
      */
     private int wait;
 
--- drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/levelzero/ZeUtils.java
+++ drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/levelzero/ZeUtils.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021, APT Group, Department of Computer Science,
+ * The University of Manchester.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package uk.ac.manchester.tornado.drivers.spirv.levelzero;
 
 /**
@@ -31,9 +55,7 @@ public class ZeUtils {
         if ((ZE_DEVICE_MEMORY_PROPERTY_FLAG_TBD & flags) == ZE_DEVICE_MEMORY_PROPERTY_FLAG_TBD) {
             str += "MEMORY_PROPERTY_FLAG_TBD | ";
         }
-        return (str.length() > 3)
-                ? "Device::{ " + str.substring(0, str.length() - 3) + " }"
-                : "Device::{ ? }";
+        return (str.length() > 3) ? "Device::{ " + str.substring(0, str.length() - 3) + " }" : "Device::{ ? }";
     }
 
     public static String zeCommandQueueGroupFlagsToString(long access) {
@@ -42,7 +64,7 @@ public class ZeUtils {
             str += "0 | ";
         }
 
-        if ((ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE & access) == ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE){
+        if ((ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE & access) == ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE) {
             str += "ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE | ";
         }
 
@@ -50,17 +72,16 @@ public class ZeUtils {
             str += "ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COPY | ";
         }
 
-        if ((ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COOPERATIVE_KERNELS & access) == ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COOPERATIVE_KERNELS) {
+        if ((ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COOPERATIVE_KERNELS
+                & access) == ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COOPERATIVE_KERNELS) {
             str += "ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COOPERATIVE_KERNELS | ";
         }
 
-        if ((ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_METRICS & access) == ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_METRICS ) {
+        if ((ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_METRICS & access) == ZeCommandQueueGroupPropertyFlags.ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_METRICS) {
             str += "ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_METRICS | ";
         }
 
-        return ( str.length() > 3 )
-                ? "Device::{ " + str.substring(0, str.length() - 3) + " }"
-                : "Device::{ ? }";
+        return (str.length() > 3) ? "Device::{ " + str.substring(0, str.length() - 3) + " }" : "Device::{ ? }";
     }
 
     public static String zeMemporyAccessCapToString(long access) {
@@ -69,7 +90,7 @@ public class ZeUtils {
             str += "0 | ";
         }
 
-        if ((ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_RW & access) == ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_RW){
+        if ((ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_RW & access) == ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_RW) {
             str += "MEMORY_ACCESS_CAP_FLAG_RW | ";
         }
 
@@ -81,13 +102,11 @@ public class ZeUtils {
             str += "MEMORY_ACCESS_CAP_FLAG_CONCURRENT | ";
         }
 
-        if ((ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_CONCURRENT_ATOMIC & access) == ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_CONCURRENT_ATOMIC ) {
+        if ((ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_CONCURRENT_ATOMIC & access) == ZeMemoryAccessCapFlags.ZE_MEMORY_ACCESS_CAP_FLAG_CONCURRENT_ATOMIC) {
             str += "MEMORY_ACCESS_CAP_FLAG_CONCURRENT_ATOMIC | ";
         }
 
-        return ( str.length() > 3 )
-                ? "Device::{ " + str.substring(0, str.length() - 3) + " }"
-                : "Device::{ ? }";
+        return (str.length() > 3) ? "Device::{ " + str.substring(0, str.length() - 3) + " }" : "Device::{ ? }";
     }
 
     public static String zeFlagsCacheToString(long flags) {
@@ -98,9 +117,7 @@ public class ZeUtils {
         if ((ZeDeviceCachePropertyFlags.ZE_DEVICE_CACHE_PROPERTY_FLAG_USER_CONTROL & flags) == ZeDeviceCachePropertyFlags.ZE_DEVICE_CACHE_PROPERTY_FLAG_USER_CONTROL) {
             str += "CACHE_PROPERTY_FLAG_USER_CONTROL | ";
         }
-        return (str.length() > 3)
-                ? "Device::{ " + str.substring(0, str.length() - 3) + " }"
-                : "Device::{ ? }";
+        return (str.length() > 3) ? "Device::{ " + str.substring(0, str.length() - 3) + " }" : "Device::{ ? }";
     }
 
 }
