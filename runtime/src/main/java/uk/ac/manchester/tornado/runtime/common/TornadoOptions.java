2455f9f69f827d7ce800916bb624405394533110
Juan Fumero <juan.fumero@manchester.ac.uk>
2021-09-14 12:26:17 +0200

    [Dump] Low-Tier dump disabled by default

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/TornadoOptions.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/TornadoOptions.java
@@ -143,7 +143,11 @@ public class TornadoOptions {
      */
     public static String PROFILER_DIRECTORY = getProperty("tornado.profiler.dump.dir", "");
 
-    public static final boolean DUMP_LOW_TIER_WITH_IGV = getBooleanValue("tornado.debug.lowtier", "True");
+    /**
+     * Dump the Control-Flow-Graph with IGV for the compiled-graph after the last
+     * phase in the Low-Tier.
+     */
+    public static final boolean DUMP_LOW_TIER_WITH_IGV = getBooleanValue("tornado.debug.lowtier", "False");
 
     public static final boolean RECOVER_BAILOUT = getBooleanValue("tornado.recover.bailout", "True");
 
