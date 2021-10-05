3dca7ace4063963bd89ccfd9308614968c8378d3
gigiblender <florin.blanaru96@gmail.com>
2021-10-05 14:07:37 +0100

    [header] Update the license header

--- drivers/ptx/src/main/java/uk/ac/manchester/tornado/drivers/ptx/graal/nodes/vector/VectorStoreGlobalMemory.java
+++ drivers/ptx/src/main/java/uk/ac/manchester/tornado/drivers/ptx/graal/nodes/vector/VectorStoreGlobalMemory.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2021, APT Group, Department of Computer Science,
  * School of Engineering, The University of Manchester. All rights reserved.
+ * Copyright (c) 2009, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -16,8 +17,6 @@
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Authors: James Clarkson
  *
  */
 package uk.ac.manchester.tornado.drivers.ptx.graal.nodes.vector;
