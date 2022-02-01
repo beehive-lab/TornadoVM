c64747596cdb6815f484eeae2e165eb7def63347
Juan Fumero <jjfumero@gmail.com>
2022-02-01 14:41:40 +0100

    SPIRV Optimizer off by default

--- runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/TornadoOptions.java
+++ runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/TornadoOptions.java
@@ -203,7 +203,7 @@ public class TornadoOptions {
     /**
      * Optimizes Loads/Stores, V2
      */
-    public static final boolean OPTIMIZE_LOAD_STORE_SPIRV_V2 = getBooleanValue("tornado.spirv.loadstore", TRUE);
+    public static final boolean OPTIMIZE_LOAD_STORE_SPIRV_V2 = getBooleanValue("tornado.spirv.loadstore", FALSE);
 
     /**
      * Use Level Zero Thread Suggestions for the Thread Dispatcher. True by default.
