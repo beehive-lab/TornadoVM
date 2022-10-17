106df9419f045838e6f715089f8e24ddd361a4c3
Juan Fumero <jjfumero@gmail.com>
2022-10-17 11:16:14 +0200

    Refactor - memory allocator handler

--- drivers/drivers-common/src/main/java/uk/ac/manchester/tornado/drivers/common/TornadoBufferProvider.java
+++ drivers/drivers-common/src/main/java/uk/ac/manchester/tornado/drivers/common/TornadoBufferProvider.java
@@ -69,7 +69,7 @@ public abstract class TornadoBufferProvider {
     protected final TornadoDeviceContext deviceContext;
     protected final ArrayList<BufferInfo> freeBuffers;
     protected final ArrayList<BufferInfo> usedBuffers;
-    protected long availableMemory;
+    protected long currentMemoryAvailable;
 
     protected TornadoBufferProvider(TornadoDeviceContext deviceContext) {
         this.deviceContext = deviceContext;
@@ -78,7 +78,7 @@ public abstract class TornadoBufferProvider {
 
         // There is no way of querying the available memory on the device. Instead, use
         // a flag similar to -Xmx.
-        availableMemory = DEVICE_AVAILABLE_MEMORY;
+        currentMemoryAvailable = DEVICE_AVAILABLE_MEMORY;
     }
 
     protected abstract long allocateBuffer(long size);
@@ -87,7 +87,7 @@ public abstract class TornadoBufferProvider {
 
     private long allocate(long size) {
         long buffer = allocateBuffer(size);
-        availableMemory -= size;
+        currentMemoryAvailable -= size;
         BufferInfo bufferInfo = new BufferInfo(buffer, size);
         usedBuffers.add(bufferInfo);
         return bufferInfo.buffer;
@@ -100,7 +100,7 @@ public abstract class TornadoBufferProvider {
             BufferInfo bufferInfo = freeBuffers.remove(0);
             TornadoInternalError.guarantee(!usedBuffers.contains(bufferInfo), "This buffer should not be used");
             remainingSize -= bufferInfo.size;
-            availableMemory += bufferInfo.size;
+            currentMemoryAvailable += bufferInfo.size;
             releaseBuffer(bufferInfo.buffer);
         }
     }
@@ -112,41 +112,49 @@ public abstract class TornadoBufferProvider {
         return buffer;
     }
 
-    public long getBufferWithSize(long size) {
-        TornadoTargetDevice targetDevice = deviceContext.getDevice();
-        if (size <= availableMemory && size < targetDevice.getDeviceMaxAllocationSize()) {
-            // Allocate if there is enough device memory.
-            return allocate(size);
-        } else if (size < targetDevice.getDeviceMaxAllocationSize()) {
-            // First check if there is an available buffer of given size.
-            // Perform a sequential search through the freeBuffers to get the buffer with
-            // the lowest size
-            // than can fulfill the allocation. The number of allocated buffers is usually
-            // low, so searching
-            // sequentially should not take a lot of time.
-            int minBufferIndex = -1;
-            for (int i = 0; i < freeBuffers.size(); i++) {
-                BufferInfo bufferInfo = freeBuffers.get(i);
-                if (bufferInfo.size >= size && (minBufferIndex == -1 || bufferInfo.size < freeBuffers.get(minBufferIndex).size)) {
-                    minBufferIndex = i;
-                }
+    private int bufferIndexOfAFreeSpace(long sizeInBytes) {
+        // First check if there is an available buffer of given size. Perform a
+        // sequential search through the freeBuffers to get the buffer with
+        // the lowest size than can fulfill the allocation. The number of allocated
+        // buffers is usually low, so searching sequentially should not take a lot of
+        // time.
+        int minBufferIndex = -1;
+        for (int i = 0; i < freeBuffers.size(); i++) {
+            BufferInfo bufferInfo = freeBuffers.get(i);
+            if (bufferInfo.size >= sizeInBytes && (minBufferIndex == -1 || bufferInfo.size < freeBuffers.get(minBufferIndex).size)) {
+                minBufferIndex = i;
             }
+        }
+        return minBufferIndex;
+    }
+
+    private long freeMemoryAndAssignRegion(long sizeInBytes) {
+        // There is no buffer to fulfill the size. Start freeing unused buffers and try
+        // to allocate.
+        freeBuffers(sizeInBytes);
+        if (sizeInBytes <= currentMemoryAvailable) {
+            return allocate(sizeInBytes);
+        } else {
+            throw new TornadoOutOfMemoryException("Unable to allocate " + sizeInBytes + " bytes of memory.");
+        }
+    }
+
+    public long getBufferWithSize(long sizeInBytes) {
+        TornadoTargetDevice targetDevice = deviceContext.getDevice();
+        if (sizeInBytes <= currentMemoryAvailable && sizeInBytes < targetDevice.getDeviceMaxAllocationSize()) {
+            // Allocate if there is enough device memory.
+            return allocate(sizeInBytes);
+        } else if (sizeInBytes < targetDevice.getDeviceMaxAllocationSize()) {
+            int minBufferIndex = bufferIndexOfAFreeSpace(sizeInBytes);
             // If a buffer was found, mark it as used and return it.
             if (minBufferIndex != -1) {
                 return markBufferUsed(minBufferIndex).buffer;
             } else {
-                // There is no buffer to fulfill the size. Start freeing unused buffers and try
-                // to allocate.
-                freeBuffers(size);
-                if (size <= availableMemory) {
-                    return allocate(size);
-                } else {
-                    throw new TornadoOutOfMemoryException("Unable to allocate " + size + " bytes of memory.");
-                }
+                return freeMemoryAndAssignRegion(sizeInBytes);
             }
         } else {
             // Throw OOM exception.
-            throw new TornadoOutOfMemoryException("Unable to allocate " + size + " bytes of memory.");
+            throw new TornadoOutOfMemoryException("Unable to allocate " + sizeInBytes + " bytes of memory.");
         }
     }
 
