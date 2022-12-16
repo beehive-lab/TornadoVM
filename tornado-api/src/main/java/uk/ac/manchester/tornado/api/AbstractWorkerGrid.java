0360c940c3f5e3e737cd32b900fbe40e818e0dc7
Juan Fumero <jjfumero@gmail.com>
2022-12-16 16:04:38 +0100

    [API] Javadoc included for DRModes

--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/AbstractWorkerGrid.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/AbstractWorkerGrid.java
@@ -2,7 +2,7 @@
  * This file is part of Tornado: A heterogeneous programming framework:
  * https://github.com/beehive-lab/tornadovm
  *
- * Copyright (c) 2020, APT Group, Department of Computer Science,
+ * Copyright (c) 2020, 2022, APT Group, Department of Computer Science,
  * The University of Manchester. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
