7fa4d9d37598039061839202f540cf907774d16e
MaryXek <xekalaki.mary@gmail.com>
2023-04-24 12:57:22 +0300

    Remove unnecessary imports and change the name of the class variable to ensure uniformity in all tests

--- tornado-assembly/src/examples/polyglotTruffle/mxmWithTornadoVM.r
+++ tornado-assembly/src/examples/polyglotTruffle/mxmWithTornadoVM.r
@@ -16,14 +16,12 @@
 #
 #
 
-java.addToClasspath("uk.ac.manchester.tornado.examples.polyglot.MyCompute")
-
-comp <- java.type('uk.ac.manchester.tornado.examples.polyglot.MyCompute')
+myclass <- java.type('uk.ac.manchester.tornado.examples.polyglot.MyCompute')
 
 print("Hello TornadoVM from R!")
 for (i in 1:5) {
     start <- Sys.time()
-    comp$compute()
+    myclass$compute()
     end <- Sys.time()
     print(paste("Total time (s): ", end - start))
 }
