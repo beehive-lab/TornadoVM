a01432d6ad2ae0cef12b1ef68b19d8179e511f00
MaryXek <xekalaki.mary@gmail.com>
2023-09-22 17:38:40 +0300

    Update the compiler phase to remove unnecessary FixedGuard nodes

--- tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/graal/phases/TornadoNativeTypeElimination.java
+++ tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/graal/phases/TornadoNativeTypeElimination.java
@@ -1,11 +1,16 @@
 package uk.ac.manchester.tornado.runtime.graal.phases;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
-import org.graalvm.compiler.nodes.GraphState;
 import org.graalvm.compiler.graph.Node;
+import org.graalvm.compiler.nodes.FixedGuardNode;
+import org.graalvm.compiler.nodes.GraphState;
 import org.graalvm.compiler.nodes.PiNode;
 import org.graalvm.compiler.nodes.StructuredGraph;
+import org.graalvm.compiler.nodes.extended.JavaReadNode;
+import org.graalvm.compiler.nodes.extended.JavaWriteNode;
+import org.graalvm.compiler.nodes.java.InstanceOfNode;
 import org.graalvm.compiler.nodes.java.LoadFieldNode;
 import org.graalvm.compiler.nodes.memory.address.OffsetAddressNode;
 import org.graalvm.compiler.phases.BasePhase;
@@ -26,10 +31,17 @@ public class TornadoNativeTypeElimination extends BasePhase<TornadoHighTierConte
                 if (!TornadoLocalArrayHeaderEliminator.nativeTypes) {
                     TornadoLocalArrayHeaderEliminator.nativeTypes = true;
                 }
+
+                // Remove FixedGuard nodes
+                if (n.successors().filter(FixedGuardNode.class).isNotEmpty()) {
+                    FixedGuardNode fx = n.successors().filter(FixedGuardNode.class).first();
+                    removeFixedGuardNodes(fx, (LoadFieldNode) n);
+                }
+
                 for (Node in : n.inputs()) {
                     if (in instanceof PiNode) {
                         for (Node us : n.usages()) {
-                            if (us instanceof OffsetAddressNode) {
+                            if (us instanceof OffsetAddressNode) { // USAGE IS PI
                                 us.replaceFirstInput(n, in);
                             }
                         }
@@ -43,6 +55,38 @@ public class TornadoNativeTypeElimination extends BasePhase<TornadoHighTierConte
         if (graph.getNodes().filter(WriteAtomicNode.class).isNotEmpty()) {
             TornadoLocalArrayHeaderEliminator.isReduction = true;
         }
+
+    }
+
+    public static void removeFixedGuardNodes(FixedGuardNode fx, LoadFieldNode lf) {
+        ArrayList<Node> nodesToBeRemoved = new ArrayList<>();
+        // identify the input nodes of fixedguardnode that need to be removed
+        for (Node fxin : fx.inputs()) {
+            if (fxin instanceof InstanceOfNode) {
+                nodesToBeRemoved.add(fxin);
+            }
+        }
+
+        // identify the usages that need to be removed
+        for (Node fxuse : fx.usages()) {
+            if (fxuse instanceof PiNode) {
+                PiNode pi = (PiNode) fxuse;
+                if (pi.usages().filter(OffsetAddressNode.class).isNotEmpty()) {
+                    OffsetAddressNode off = pi.usages().filter(OffsetAddressNode.class).first();
+                    // if this address node is used by a javaread/javawrite node
+                    if (off.usages().filter(JavaReadNode.class).isNotEmpty() || off.usages().filter(JavaWriteNode.class).isNotEmpty()) {
+                        off.replaceFirstInput(pi, lf);
+                        nodesToBeRemoved.add(pi);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < nodesToBeRemoved.size(); i++) {
+            nodesToBeRemoved.get(i).safeDelete();
+        }
+
+        deleteFixed(fx);
 
     }
 
