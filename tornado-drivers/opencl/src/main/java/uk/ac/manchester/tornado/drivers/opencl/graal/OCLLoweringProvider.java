c97c889bb681b798074ae80a5a1b3209eb4ceee4
mikepapadim <mikepapadim@hotmail.com>
2023-11-03 18:10:46 +0200

    [PTX] duplicate phases needed for Paname memory allocations

--- tornado-drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/OCLLoweringProvider.java
+++ tornado-drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/OCLLoweringProvider.java
@@ -252,6 +252,7 @@ public class OCLLoweringProvider extends DefaultJavaLoweringProvider {
     }
 
     private void lowerReduceSnippets(Node node, LoweringTool tool) {
+
         StructuredGraph graph = null;
         ValueNode startIndexNode = null;
         if (node instanceof StoreAtomicIndexedNode) {
--- tornado-drivers/ptx/src/main/java/uk/ac/manchester/tornado/drivers/ptx/graal/phases/TornadoPanamaSegmentsHeaderPhase.java
+++ tornado-drivers/ptx/src/main/java/uk/ac/manchester/tornado/drivers/ptx/graal/phases/TornadoPanamaSegmentsHeaderPhase.java
@@ -1,4 +1,60 @@
+/*
+ * Copyright (c) 2023, APT Group, Department of Computer Science,
+ * School of Engineering, The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 package uk.ac.manchester.tornado.drivers.ptx.graal.phases;
 
-public class TornadoPanamaSegmentsHeaderPhase {
+import java.util.Optional;
+
+import org.graalvm.compiler.nodes.ConstantNode;
+import org.graalvm.compiler.nodes.GraphState;
+import org.graalvm.compiler.nodes.StructuredGraph;
+import org.graalvm.compiler.nodes.ValueNode;
+import org.graalvm.compiler.nodes.memory.ReadNode;
+import org.graalvm.compiler.nodes.memory.address.AddressNode;
+import org.graalvm.compiler.nodes.memory.address.OffsetAddressNode;
+import org.graalvm.compiler.phases.BasePhase;
+import org.graalvm.compiler.phases.Phase;
+
+public class TornadoPanamaSegmentsHeaderPhase extends Phase {
+
+    @Override
+    public Optional<BasePhase.NotApplicable> notApplicableTo(GraphState graphState) {
+        return ALWAYS_APPLICABLE;
+    }
+
+    @Override
+    protected void run(StructuredGraph graph) {
+        for (ReadNode readNode : graph.getNodes().filter(ReadNode.class)) {
+            String methodName = readNode.getLocationIdentity().toString();
+            if (methodName.endsWith("numberOfElements")) {
+                AddressNode address = readNode.getAddress();
+                if (address instanceof OffsetAddressNode offsetAddressNode) {
+                    ValueNode offset = offsetAddressNode.getOffset();
+                    if (offset instanceof ConstantNode) {
+                        // Reserve the position 0 of the array to set the array size.
+                        ConstantNode constantNode = graph.addOrUnique(ConstantNode.forLong((0)));
+                        offsetAddressNode.setOffset(constantNode);
+                        offset.safeDelete();
+                    }
+                }
+            }
+        }
+    }
 }
