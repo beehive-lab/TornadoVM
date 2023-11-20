72f4fe9ab0212347294a91faef2d6e2b4dcad1fb
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2023-11-20 13:29:57 +0200

    [feat] Add support of ruby code interoperability and mxm example

--- scripts/polyglotTests.sh
+++ scripts/polyglotTests.sh
@@ -2,6 +2,6 @@ echo " ============================================= "
 echo " GraalVM Polyglot Tests "
 echo " ============================================= "
 
-tornado --printKernel --truffle python $TORNADO_SDK/examples/polyglotTruffle/mxmWithTornadoVM.py 
+tornado --printKernel --truffle python $TORNADO_SDK/examples/polyglotTruffle/mxmWithTornadoVM.py
 tornado --printKernel --truffle js $TORNADO_SDK/examples/polyglotTruffle/mxmWithTornadoVM.js
-tornado --printKernel --truffle r $TORNADO_SDK/examples/polyglotTruffle/mxmWithTornadoVM.r
+tornado --printKernel --truffle ruby $TORNADO_SDK/examples/polyglotTruffle/mxmWithTornadoVM.rb
