1e99ded37d020c8a966cc67356e7d831ca38d175
Alireza Shateri <alirezashateri7@gmail.com>
2023-11-29 09:55:45 -0500

    Handle any number of spaces

--- tornado-assembly/src/bin/aws_post_processing.sh
+++ tornado-assembly/src/bin/aws_post_processing.sh
@@ -4,9 +4,9 @@ configurationFilePath=$1
 directoryBitstream=$2
 entryPoint=$3
 
-export S3_BUCKET=$(grep -w "AWS_S3_BUCKET" ${configurationFilePath} | awk -F'[[:space:]]=[[:space:]]' '{print $2}')
-export S3_DCP_KEY=$(grep -w "AWS_S3_DCP_KEY" ${configurationFilePath} | awk -F'[[:space:]]=[[:space:]]' '{print $2}')
-export S3_LOGS_KEY=$(grep -w "AWS_S3_LOGS_KEY" ${configurationFilePath} | awk -F'[[:space:]]=[[:space:]]' '{print $2}')
+export S3_BUCKET=$(grep -w "AWS_S3_BUCKET" ${configurationFilePath} | awk -F'[[:space:]]*=[[:space:]]*' '{print $2}')
+export S3_DCP_KEY=$(grep -w "AWS_S3_DCP_KEY" ${configurationFilePath} | awk -F'[[:space:]]*=[[:space:]]*' '{print $2}')
+export S3_LOGS_KEY=$(grep -w "AWS_S3_LOGS_KEY" ${configurationFilePath} | awk -F'[[:space:]]*=[[:space:]]*' '{print $2}')
 $VITIS_DIR/tools/create_vitis_afi.sh -xclbin=${directoryBitstream}/${entryPoint}.xclbin -s3_bucket="$S3_BUCKET" -s3_dcp_key="$S3_DCP_KEY" -s3_logs_key="$S3_LOGS_KEY"
 
 sudo mv ${entryPoint}.awsxclbin ${directoryBitstream}/${entryPoint}.awsxclbin
