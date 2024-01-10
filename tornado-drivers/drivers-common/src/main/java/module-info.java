2fb079d6c96c51feb8e70607642dc363977d1bc7
mikepapadim <mikepapadim@hotmail.com>
2024-01-10 11:52:15 +0200

    Change module to open; add exports in common-exports
    Changed module in module-info.java from module to open. This adjustment provides necessary permissions for handling reflections. Additional exports were also added in the common-exports file for the purpose of extension and API visibility.

--- tornado-drivers/drivers-common/src/main/java/module-info.java
+++ tornado-drivers/drivers-common/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module tornado.drivers.common {
+open module tornado.drivers.common {
     requires transitive jdk.internal.vm.ci;
     requires transitive jdk.internal.vm.compiler;
     requires transitive tornado.runtime;
