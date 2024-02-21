e8fa872a6911c0842f1f6958cb6b8d2a4d383ae4
Juan Fumero <jjfumero@gmail.com>
2024-02-21 15:10:16 +0100

    [api] Factory methods for Panama types initialization using the Parallel annotation

--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/arrays/ByteArray.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/arrays/ByteArray.java
@@ -23,6 +23,7 @@ import static java.lang.foreign.ValueLayout.JAVA_INT;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
+import uk.ac.manchester.tornado.api.annotations.Parallel;
 import uk.ac.manchester.tornado.api.internal.annotations.SegmentElementSize;
 
 /**
@@ -213,4 +214,19 @@ public final class ByteArray extends TornadoNativeArray {
     public long getNumBytesWithoutHeader() {
         return segmentByteSize - TornadoNativeArray.ARRAY_HEADER;
     }
+
+    /**
+     * Factory method to initialize a {@link ByteArray}. This method can be invoked from a Task-Graph.
+     *
+     * @param array
+     *     Input Array.
+     * @param value
+     *     The float value to initialize the {@code ByteArray} instance with.
+     */
+    public static void initialize(ByteArray array, byte value) {
+        for (@Parallel int i = 0; i < array.getSize(); i++) {
+            array.set(i, value);
+        }
+    }
+
 }
--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/arrays/IntArray.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/arrays/IntArray.java
@@ -22,7 +22,9 @@ import static java.lang.foreign.ValueLayout.JAVA_INT;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
+import uk.ac.manchester.tornado.api.annotations.Parallel;
 import uk.ac.manchester.tornado.api.internal.annotations.SegmentElementSize;
+import uk.ac.manchester.tornado.api.types.HalfFloat;
 
 /**
  * This class represents an array of ints stored in native memory.
@@ -210,6 +212,20 @@ public final class IntArray extends TornadoNativeArray {
     @Override
     public MemorySegment getSegment() {
         return segment;
+    }
+
+    /**
+     * Factory method to initialize a {@link IntArray}. This method can be invoked from a Task-Graph.
+     *
+     * @param array
+     *     Input Array.
+     * @param value
+     *     The float value to initialize the {@code IntArray} instance with.
+     */
+    public static void initialize(IntArray array, int value) {
+        for (@Parallel int i = 0; i < array.getSize(); i++) {
+            array.set(i, value);
+        }
     }
 
 }
