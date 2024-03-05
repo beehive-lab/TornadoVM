367a6dd9ae4742feaa5732647257786940df50ef
mikepapadim <mikepapadim@hotmail.com>
2024-03-05 14:26:38 +0200

    Update Tensor to support HalfFloat type
    Added support for the HalfFloat type in the Tensor class. This includes a new constructor to create Tensors with MemorySegment and HalfFloat type, and methods to set and get HalfFloat values in the Tensor. Also refactor a few methods for clarity.

--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/Tensor.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/Tensor.java
@@ -1,10 +1,12 @@
 package uk.ac.manchester.tornado.api.types.tensors;
 
 import static java.lang.foreign.ValueLayout.JAVA_INT;
+import static java.lang.foreign.ValueLayout.JAVA_SHORT;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
+import uk.ac.manchester.tornado.api.types.HalfFloat;
 import uk.ac.manchester.tornado.api.types.arrays.TornadoNativeArray;
 import uk.ac.manchester.tornado.api.types.tensors.dtype.DType;
 
@@ -18,6 +20,13 @@ public final class Tensor<T extends DType> extends TornadoNativeArray {
     private Shape shape;
     private T dtype;
 
+    public Tensor(int size, MemorySegment memorySegment, T dtype) {
+        assert size >= 0;
+        this.numberOfElements = size;
+        this.segment = memorySegment;
+        this.dtype = dtype;
+    }
+
     public Tensor(Shape shape, T dtype) {
         this.shape = shape;
         this.numberOfElements = shape.getSize();
@@ -25,20 +34,18 @@ public final class Tensor<T extends DType> extends TornadoNativeArray {
 
         arrayHeaderSize = (int) TornadoNativeArray.ARRAY_HEADER;
         baseIndex = arrayHeaderSize / dtype.getByteSize();
-        segmentByteSize = numberOfElements * dtype.getByteSize() + arrayHeaderSize;
+        segmentByteSize = (long) numberOfElements * dtype.getByteSize() + arrayHeaderSize;
 
-        segment = Arena.ofAuto().allocate(segmentByteSize, 1);
+        this.segment = Arena.ofAuto().allocate(segmentByteSize, 1);
         segment.setAtIndex(JAVA_INT, 0, numberOfElements);
     }
-
-    //Create an instance of T
 
     @Override
     public int getSize() {
         return 0;
     }
 
-    public String getDTYPE() {
+    public String getDTypeAsString() {
         return dtype.getDType();
     }
 
@@ -60,6 +67,21 @@ public final class Tensor<T extends DType> extends TornadoNativeArray {
     @Override
     protected void clear() {
 
+    }
+
+    public void set(int index, HalfFloat value) {
+        segment.setAtIndex(JAVA_SHORT, baseIndex + index, value.getHalfFloatValue());
+    }
+
+    public HalfFloat get(int index) {
+        short halfFloatValue = segment.getAtIndex(JAVA_SHORT, baseIndex + index);
+        return new HalfFloat(halfFloatValue);
+    }
+
+    public void init(HalfFloat value) {
+        for (int i = 0; i < getSize(); i++) {
+            segment.setAtIndex(JAVA_SHORT, baseIndex + i, value.getHalfFloatValue());
+        }
     }
 
     @Override
