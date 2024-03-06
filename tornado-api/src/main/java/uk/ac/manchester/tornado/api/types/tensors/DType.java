dc3dabd96b7bbba9ac4e7d5f9cce417ee77f124c
mikepapadim <mikepapadim@hotmail.com>
2024-03-06 16:41:51 +0200

    Remove array creation methods in DType
    The array creation methods in DType (Tornado API) have been removed. These changes make the code cleaner and easier to read, as these removed methods seemed to be extras that are not needed, hence streamlining the code.

--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/DType.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/DType.java
@@ -2,11 +2,6 @@ package uk.ac.manchester.tornado.api.types.tensors;
 
 import java.lang.foreign.ValueLayout;
 
-import uk.ac.manchester.tornado.api.types.arrays.DoubleArray;
-import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
-import uk.ac.manchester.tornado.api.types.arrays.IntArray;
-import uk.ac.manchester.tornado.api.types.arrays.TornadoNativeArray;
-
 public enum DType {
     // @formatter:off
     HALF_FLOAT(2, ValueLayout.JAVA_SHORT),
@@ -37,21 +32,6 @@ public enum DType {
 
     public ValueLayout getLayout() {
         return layout;
-    }
-
-    public TornadoNativeArray createArray(int[] shape) {
-        switch (this) {
-            case HALF_FLOAT:
-            case FLOAT:
-                return new FloatArray(calculateSize(shape));
-            case DOUBLE:
-                return new DoubleArray(calculateSize(shape));
-            case INT8:
-                return new IntArray(calculateSize(shape));
-            // Add cases for other DType implementations
-            default:
-                throw new IllegalArgumentException("Unsupported dtype: " + this);
-        }
     }
 
     private int calculateSize(int[] shape) {
