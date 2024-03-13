5b6aabc251e7f3bcbdc74b37ca00d593677a2007
mikepapadim <mikepapadim@hotmail.com>
2024-03-13 14:41:02 +0200

    Add detailed comments and improve documentation in Tensor modules

--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/DType.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/DType.java
@@ -1,21 +1,78 @@
+/*
+ * Copyright (c) 2024, APT Group, Department of Computer Science,
+ * The University of Manchester.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package uk.ac.manchester.tornado.api.types.tensors;
 
 import java.lang.foreign.ValueLayout;
 
+/**
+ * The {@code DType} enum represents the various data types supported by a data management or processing system.
+ * Each enum constant specifies a data type, its size in bytes, and its corresponding {@link ValueLayout} for memory layout.
+ * This allows for easy management and interpretation of different types of data in a uniform way.
+ */
 public enum DType {
     // @formatter:off
+    /**
+     * Represents a half-precision floating-point data type using 2 bytes.
+     */
     HALF_FLOAT(2, ValueLayout.JAVA_SHORT),
+    /**
+     * Represents a single-precision 32-bit IEEE floating-point data type using 4 bytes.
+     */
     FLOAT(4, ValueLayout.JAVA_FLOAT),
+    /**
+     * Represents a double-precision 64-bit IEEE floating-point data type using 8 bytes.
+     */
     DOUBLE(8, ValueLayout.JAVA_DOUBLE),
+    /**
+     * Represents an 8-bit signed integer data type using 1 byte.
+     */
     INT8(1, ValueLayout.JAVA_BYTE),
+    /**
+     * Represents a 16-bit signed integer data type using 2 bytes.
+     */
     INT16(2, ValueLayout.JAVA_SHORT),
+    /**
+     * Represents a 32-bit signed integer data type using 4 bytes.
+     */
     INT32(4, ValueLayout.JAVA_INT),
+    /**
+     * Represents a 64-bit signed integer data type using 8 bytes.
+     */
     INT64(8, ValueLayout.JAVA_LONG),
+    /**
+     * Represents an 8-bit unsigned integer data type using 1 byte.
+     */
     UINT8(1, ValueLayout.JAVA_BYTE),
+    /**
+     * Represents a boolean data type using 1 byte for true/false values.
+     */
     BOOL(1, ValueLayout.JAVA_BYTE),
+    /**
+     * Represents a quantized 8-bit signed integer used in specialized applications like machine learning, using 1 byte.
+     */
     QINT8(1, ValueLayout.JAVA_BYTE),
-    QUINT8(1, ValueLayout.JAVA_BYTE),
-    QINT32(4, ValueLayout.JAVA_INT);
+    /**
+     * Represents a quantized 8-bit unsigned integer used in specialized applications like machine learning, using 1 byte.
+     */
+    QUINT8(1, ValueLayout.JAVA_BYTE);
+    /**
+     * Represents a quantized 32-bit signed integer used in specialized applications like machine learning, using 4 bytes.
+     */
     // @formatter:on
 
     private final int size;
--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/Tensor.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/types/tensors/Tensor.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2024, APT Group, Department of Computer Science,
+ * The University of Manchester.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package uk.ac.manchester.tornado.api.types.tensors;
 
 import static java.lang.foreign.ValueLayout.JAVA_FLOAT;
@@ -11,16 +28,50 @@ import uk.ac.manchester.tornado.api.internal.annotations.SegmentElementSize;
 import uk.ac.manchester.tornado.api.types.HalfFloat;
 import uk.ac.manchester.tornado.api.types.arrays.TornadoNativeArray;
 
-@SegmentElementSize(size = 2) //This needs to be fixed
+@SegmentElementSize(size = 4) //This needs to be fixed
 public final class Tensor extends TornadoNativeArray {
 
+    /**
+     * The data type of the elements contained within the tensor.
+     */
     private final DType dType;
+
+    /**
+     * The total number of elements in the tensor.
+     */
     private int numberOfElements;
+
+    /**
+     * The memory segment representing the tensor data in native memory.
+     */
     private MemorySegment segment;
+
+    /**
+     * The index offset in the memory segment where the tensor data starts.
+     */
     private int baseIndex;
+
+    /**
+     * The size in bytes of the memory segment including the header.
+     */
     private long segmentByteSize;
+
+    /**
+     * The shape of the tensor, representing its dimensions.
+     */
     private Shape shape;
 
+    /**
+     * Constructs a tensor with a specified size, memory segment, and data type.
+     *
+     * @param size
+     *     the number of elements in the tensor
+     * @param memorySegment
+     *     the memory segment where the tensor is stored
+     * @param dtype
+     *     the data type of the tensor elements
+     */
+    // This is problematic, it needs the segment to be shifted to cater the ARRAY_HEADER
     public Tensor(int size, MemorySegment memorySegment, DType dtype) {
         assert size >= 0;
         this.numberOfElements = size;
@@ -28,6 +79,14 @@ public final class Tensor extends TornadoNativeArray {
         this.dType = dtype;
     }
 
+    /**
+     * Constructs a tensor with a specified shape and data type. The memory for the tensor is automatically allocated.
+     *
+     * @param shape
+     *     the shape of the tensor
+     * @param dtype
+     *     the data type of the tensor elements
+     */
     public Tensor(Shape shape, DType dtype) {
         this.shape = shape;
         this.numberOfElements = shape.getSize();
@@ -38,10 +97,18 @@ public final class Tensor extends TornadoNativeArray {
         segment.setAtIndex(JAVA_INT, 0, numberOfElements);
     }
 
+    /**
+     * Creates a tensor from a float array.
+     *
+     * @param values
+     *     the float array to convert into a tensor
+     * @return a new tensor containing the provided values
+     */
     public static Tensor fromArray(float[] values) {
         return createSegment(values);
     }
 
+    // Private helper method to support fromArray
     private static Tensor createSegment(float[] values) {
         Shape shape = new Shape(values.length);
         Tensor array = new Tensor(shape, DType.FLOAT);
@@ -62,6 +129,10 @@ public final class Tensor extends TornadoNativeArray {
 
     public String getDTypeAsString() {
         return dType.toString();
+    }
+
+    public DType getDType() {
+        return dType;
     }
 
     @Override
@@ -103,6 +174,18 @@ public final class Tensor extends TornadoNativeArray {
 
     public float getFloatValue(int index) {
         return segment.getAtIndex(JAVA_FLOAT, baseIndex + index);
+    }
+
+    //Example usage: tensorC.set(i, tensorA.get(i, Float.class) + tensorB.get(i, Float.class));
+    public <T> T get(int index, Class<T> type) {
+        if (type.equals(HalfFloat.class)) {
+            short halfFloatValue = segment.getAtIndex(JAVA_SHORT, baseIndex + index);
+            return type.cast(new HalfFloat(halfFloatValue)); // You need to ensure HalfFloat can be cast to T
+        } else if (type.equals(Float.class)) {
+            float floatValue = segment.getAtIndex(JAVA_FLOAT, baseIndex + index);
+            return type.cast(floatValue); // Autoboxing from float to Float
+        }
+        throw new IllegalArgumentException(STR."Unsupported type: \{type}");
     }
 
     public void init(HalfFloat value) {
