0833d06b372e8edacbc31994654eb3b6c3d52f88
mikepapadim <mikepapadim@hotmail.com>
2024-07-19 12:32:53 +0100

    Update module dependencies in SPIR-V and PTX drivers

--- tornado-drivers/ptx/src/main/java/module-info.java
+++ tornado-drivers/ptx/src/main/java/module-info.java
@@ -1,7 +1,30 @@
 import uk.ac.manchester.tornado.runtime.TornadoBackendProvider;
 
-module tornado.drivers.ptx{requires java.base;requires transitive jdk.internal.vm.ci;requires transitive jdk.graal.compiler;requires transitive org.graalvm.collections;requires transitive org.graalvm.word;requires transitive tornado.api;requires transitive tornado.runtime;requires tornado.drivers.common;
+module tornado.drivers.ptx {
+    requires java.base;
+    requires transitive jdk.internal.vm.ci;
+    requires transitive jdk.graal.compiler;
+    requires transitive org.graalvm.collections;
+    requires transitive org.graalvm.word;
+    requires transitive tornado.api;
+    requires transitive tornado.runtime;
+    requires tornado.drivers.common;
 
-exports uk.ac.manchester.tornado.drivers.ptx;exports uk.ac.manchester.tornado.drivers.ptx.enums;exports uk.ac.manchester.tornado.drivers.ptx.graal;exports uk.ac.manchester.tornado.drivers.ptx.graal.asm;exports uk.ac.manchester.tornado.drivers.ptx.graal.backend;exports uk.ac.manchester.tornado.drivers.ptx.graal.compiler;exports uk.ac.manchester.tornado.drivers.ptx.graal.lir;exports uk.ac.manchester.tornado.drivers.ptx.graal.meta;exports uk.ac.manchester.tornado.drivers.ptx.graal.nodes;exports uk.ac.manchester.tornado.drivers.ptx.graal.nodes.calc;exports uk.ac.manchester.tornado.drivers.ptx.graal.nodes.vector;exports uk.ac.manchester.tornado.drivers.ptx.graal.phases;exports uk.ac.manchester.tornado.drivers.ptx.mm;exports uk.ac.manchester.tornado.drivers.ptx.runtime;exports uk.ac.manchester.tornado.drivers.ptx.power;
+    exports uk.ac.manchester.tornado.drivers.ptx;
+    exports uk.ac.manchester.tornado.drivers.ptx.enums;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.asm;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.backend;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.compiler;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.lir;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.meta;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.nodes;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.nodes.calc;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.nodes.vector;
+    exports uk.ac.manchester.tornado.drivers.ptx.graal.phases;
+    exports uk.ac.manchester.tornado.drivers.ptx.mm;
+    exports uk.ac.manchester.tornado.drivers.ptx.runtime;
+    exports uk.ac.manchester.tornado.drivers.ptx.power;
 
-provides TornadoBackendProvider with uk.ac.manchester.tornado.drivers.ptx.PTXTornadoDriverProvider;}
+    provides TornadoBackendProvider with uk.ac.manchester.tornado.drivers.ptx.PTXTornadoDriverProvider;
+}
