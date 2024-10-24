0432f37acc8eaef2355c6977e70236f80c013a7c
Thanos Stratikopoulos <tstratiko@gmail.com>
2024-10-24 13:13:40 +0100

    [refactor] Refactored script to replace place_holder variables with runtime information

--- tornado-assembly/src/bin/idea_xml_utils.py
+++ tornado-assembly/src/bin/idea_xml_utils.py
@@ -31,191 +31,114 @@ def generate_backend_profiles_as_xml_entries(BACKEND_PROFILES):
 
     return "\n".join(xml_entries)
 
-def define_and_get_internal_maven_content(TORNADO_ROOT, JAVA_HOME, BACKEND_PROFILES):
-    maven_directory = os.path.join(str(TORNADO_ROOT), "etc", "dependencies", "apache-maven-3.9.3")
-    if "graal" in JAVA_HOME:
-        JAVA_PROFILE = "graal-jdk-21"
+def read_template(template_path):
+    with open(template_path, 'r') as file:
+        template_content = file.read()
+    return template_content
+
+def write_generated_template(xml_content, xml_file_path):
+    with open(xml_file_path, "w") as file:
+        file.write(xml_content)
+
+def define_and_get_internal_maven_content(xml_templates_directory, project_directory, java_home, backend_profiles):
+    maven_directory = os.path.join(project_directory, "etc", "dependencies", "apache-maven-3.9.3")
+    xml_internal_maven_build_content_directory = os.path.join(xml_templates_directory, "maven_template.xml")
+    xml_internal_maven_build_content = read_template(xml_internal_maven_build_content_directory)
+
+    if "graal" in java_home:
+        java_profile = "graal-jdk-21"
     else:
-        JAVA_PROFILE = "jdk21"
+        java_profile = "jdk21"
     
-    xml_backend_profiles = generate_backend_profiles_as_xml_entries(BACKEND_PROFILES)
-    xml_internal_maven_build_content = f"""<?xml version="1.0" encoding="UTF-8"?>
-    <component name="ProjectRunConfigurationManager">
-      <configuration default="false" name="_internal_TornadoVM_Maven-cleanAndinstall" type="MavenRunConfiguration" factoryName="Maven">
-        <MavenSettings>
-          <option name="myGeneralSettings">
-            <MavenGeneralSettings>
-              <option name="alwaysUpdateSnapshots" value="false" />
-              <option name="checksumPolicy" value="NOT_SET" />
-              <option name="emulateTerminal" value="true" />
-              <option name="failureBehavior" value="NOT_SET" />
-              <option name="localRepository" value="" />
-              <option name="mavenHome" value= "{maven_directory}" />
-              <option name="nonRecursive" value="false" />
-              <option name="outputLevel" value="INFO" />
-              <option name="printErrorStackTraces" value="false" />
-              <option name="showDialogWithAdvancedSettings" value="false" />
-              <option name="threads" />
-              <option name="useMavenConfig" value="true" />
-              <option name="usePluginRegistry" value="false" />
-              <option name="userSettingsFile" value="" />
-              <option name="workOffline" value="false" />
-            </MavenGeneralSettings>
-          </option>
-          <option name="myRunnerSettings" />
-          <option name="myRunnerParameters">
-            <MavenRunnerParameters>
-              <option name="cmdOptions" />
-              <option name="profiles">
-                <set />
-              </option>
-              <option name="goals">
-                <list>
-                  <option value="clean" />
-                  <option value="install" />
-                </list>
-              </option>
-              <option name="pomFileName" />
-              <option name="profilesMap">
-                <map>
-                  <entry key="{JAVA_PROFILE}" value="true" />
-                  {xml_backend_profiles}
-                </map>
-              </option>
-              <option name="projectsCmdOptionValues">
-                <list />
-              </option>
-              <option name="resolveToWorkspace" value="false" />
-              <option name="workingDirPath" value="{TORNADO_ROOT}" />
-            </MavenRunnerParameters>
-          </option>
-        </MavenSettings>
-        <method v="2" />
-      </configuration>
-    </component>"""
+    xml_backend_profiles = generate_backend_profiles_as_xml_entries(backend_profiles)
+
+    xml_internal_maven_build_content = xml_internal_maven_build_content.replace("[@@MAVEN_DIRECTORY@@]", maven_directory)
+    xml_internal_maven_build_content = xml_internal_maven_build_content.replace("[@@JAVA_PROFILE@@]", java_profile)
+    xml_internal_maven_build_content = xml_internal_maven_build_content.replace("[@@BACKEND_PROFILES@@]", xml_backend_profiles)
+    xml_internal_maven_build_content = xml_internal_maven_build_content.replace("[@@PROJECT_DIR@@]", project_directory)
+
     return xml_internal_maven_build_content
 
-def define_and_get_tornadovm_build_content(TORNADO_ROOT, TORNADO_SDK, JAVA_HOME, PYTHON_HOME, BACKEND_PROFILES):
-    post_installation_script_directory = os.path.join(str(TORNADO_ROOT), "bin", "post_installation.py")
-    python_version = subprocess.check_output([str(PYTHON_HOME), "--version"]).decode().strip()
+def define_and_get_tornadovm_build_content(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backend_profiles):
+    post_installation_script_directory = os.path.join(project_directory, "bin", "post_installation.py")
+    xml_TornadoVM_build_content_directory = os.path.join(xml_templates_directory, "tornadovm_build_template.xml")
+    xml_TornadoVM_build_content = read_template(xml_TornadoVM_build_content_directory)
+
+    python_version = subprocess.check_output([str(python_home), "--version"]).decode().strip()
     python_version_name = python_version.split()[1]
-    python_sdk_home = str(PYTHON_HOME)
+    python_sdk_home = str(python_home)
     python_sdk_name = f"Python {python_version_name}"
 
-    xml_TornadoVM_build_content = f"""<?xml version="1.0" encoding="UTF-8"?>
-    <component name="ProjectRunConfigurationManager">
-        <configuration default="false" name="TornadoVM-Build" type="PythonConfigurationType"
-                       factoryName="Python">
-            <module name="tornado-annotation"/>
-            <option name="INTERPRETER_OPTIONS" value=""/>
-            <option name="PARENT_ENVS" value="true"/>
-            <envs>
-                <env name="PYTHONUNBUFFERED" value="1"/>
-                <env name="BACKEND" value="opencl"/>
-                <env name="TORNADO_SDK" value="{TORNADO_SDK}"/>
-                <env name="JAVA_HOME" value="{JAVA_HOME}" />
-                <env name="selected_backends" value="{BACKEND_PROFILES}"/>
-            </envs>
-            <option name="SDK_HOME" value="{python_sdk_home}"/>
-            <option name="SDK_NAME" value="{python_sdk_name}"/>
-            <option name="WORKING_DIRECTORY" value="{TORNADO_ROOT}"/>
-            <option name="IS_MODULE_SDK" value="false"/>
-            <option name="ADD_CONTENT_ROOTS" value="true"/>
-            <option name="ADD_SOURCE_ROOTS" value="true"/>
-            <EXTENSION ID="PythonCoverageRunConfigurationExtension" runner="coverage.py"/>
-            <option name="SCRIPT_NAME" value="{post_installation_script_directory}"/>
-            <option name="PARAMETERS" value=""/>
-            <option name="SHOW_COMMAND_LINE" value="false"/>
-            <option name="EMULATE_TERMINAL" value="false"/>
-            <option name="MODULE_MODE" value="false"/>
-            <option name="REDIRECT_INPUT" value="false"/>
-            <option name="INPUT_FILE" value=""/>
-            <method v="2">
-                <option name="RunConfigurationTask" enabled="true"
-                        run_configuration_name="_internal_TornadoVM_Maven-cleanAndinstall"
-                        run_configuration_type="MavenRunConfiguration"/>
-            </method>
-        </configuration>
-    </component>"""
+    xml_TornadoVM_build_content = xml_TornadoVM_build_content.replace("[@@TORNADO_SDK@@]", tornado_sdk)
+    xml_TornadoVM_build_content = xml_TornadoVM_build_content.replace("[@@JAVA_HOME@@]", java_home)
+    xml_TornadoVM_build_content = xml_TornadoVM_build_content.replace("[@@BACKEND_PROFILES@@]", backend_profiles)
+    xml_TornadoVM_build_content = xml_TornadoVM_build_content.replace("[@@PYTHON_SDK_HOME@@]", python_sdk_home)
+    xml_TornadoVM_build_content = xml_TornadoVM_build_content.replace("[@@PYTHON_SDK_NAME@@]", python_sdk_name)
+    xml_TornadoVM_build_content = xml_TornadoVM_build_content.replace("[@@PROJECT_DIR@@]", project_directory)
+    xml_TornadoVM_build_content = xml_TornadoVM_build_content.replace("[@@POST_INSTALLATION_SCRIPT_DIRECTORY@@]", post_installation_script_directory)
+
     return xml_TornadoVM_build_content
 
-def define_and_get_tornadovm_tests_content(TORNADO_ROOT, TORNADO_SDK, JAVA_HOME, PYTHON_HOME, BACKEND_PROFILES):
-    test_script_directory = os.path.join(str(TORNADO_SDK), "bin", "tornado-test")
-    python_version = subprocess.check_output([str(PYTHON_HOME), "--version"]).decode().strip()
+def define_and_get_tornadovm_tests_content(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backend_profiles):
+    test_script_directory = os.path.join(tornado_sdk, "bin", "tornado-test")
+    xml_TornadoVM_tests_content_directory = os.path.join(xml_templates_directory, "tornadovm_tests_template.xml")
+    xml_TornadoVM_tests_content = read_template(xml_TornadoVM_tests_content_directory)
+
+    python_version = subprocess.check_output([str(python_home), "--version"]).decode().strip()
     python_version_name = python_version.split()[1]
-    python_sdk_home = str(PYTHON_HOME)
+    python_sdk_home = str(python_home)
     python_sdk_name = f"Python {python_version_name}"
 
-    xml_TornadoVM_tests_content = f"""<?xml version="1.0" encoding="UTF-8"?>
-    <component name="ProjectRunConfigurationManager">
-      <configuration default="false" name="TornadoVM-Tests" type="PythonConfigurationType" factoryName="Python">
-        <module name="tornado-annotation" />
-        <option name="ENV_FILES" value="" />
-        <option name="INTERPRETER_OPTIONS" value="" />
-        <option name="PARENT_ENVS" value="true" />
-        <envs>
-          <env name="BACKEND" value="opencl" />
-          <env name="PYTHONUNBUFFERED" value="1" />
-          <env name="selected_backends" value="{BACKEND_PROFILES}"/>
-          <env name="TORNADO_SDK" value="{TORNADO_SDK}" />
-          <env name="JAVA_HOME" value="{JAVA_HOME}" />
-        </envs>
-        <option name="SDK_HOME" value="{python_sdk_home}"/>
-        <option name="SDK_NAME" value="{python_sdk_name}"/>
-        <option name="WORKING_DIRECTORY" value="{TORNADO_ROOT}" />:wq
-        <option name="IS_MODULE_SDK" value="false" />
-        <option name="ADD_CONTENT_ROOTS" value="true" />
-        <option name="ADD_SOURCE_ROOTS" value="true" />
-        <EXTENSION ID="PythonCoverageRunConfigurationExtension" runner="coverage.py" />
-        <option name="SCRIPT_NAME" value="{TORNADO_SDK}/bin/tornado-test" />
-        <option name="PARAMETERS" value="--ea --verbose --quickPass" />
-        <option name="SHOW_COMMAND_LINE" value="false" />
-        <option name="EMULATE_TERMINAL" value="false" />
-        <option name="MODULE_MODE" value="false" />
-        <option name="REDIRECT_INPUT" value="false" />
-        <option name="INPUT_FILE" value="" />
-        <method v="2" />
-      </configuration>
-    </component>"""
+    xml_TornadoVM_tests_content = xml_TornadoVM_tests_content.replace("[@@BACKEND_PROFILES@@]", backend_profiles)
+    xml_TornadoVM_tests_content = xml_TornadoVM_tests_content.replace("[@@TORNADO_SDK@@]", tornado_sdk)
+    xml_TornadoVM_tests_content = xml_TornadoVM_tests_content.replace("[@@JAVA_HOME@@]", java_home)
+    xml_TornadoVM_tests_content = xml_TornadoVM_tests_content.replace("[@@PYTHON_SDK_HOME@@]", python_sdk_home)
+    xml_TornadoVM_tests_content = xml_TornadoVM_tests_content.replace("[@@PYTHON_SDK_NAME@@]", python_sdk_name)
+    xml_TornadoVM_tests_content = xml_TornadoVM_tests_content.replace("[@@PROJECT_DIR@@]", project_directory)
+    xml_TornadoVM_tests_content = xml_TornadoVM_tests_content.replace("[@@TORNADO_TEST_DIR@@]", test_script_directory)
+
     return xml_TornadoVM_tests_content
 
-def generate_internal_maven_build_xml(TORNADO_ROOT, JAVA_HOME, BACKEND_PROFILES):
-    xml_internal_content = define_and_get_internal_maven_content(TORNADO_ROOT, JAVA_HOME, BACKEND_PROFILES)
-    xml_directory = os.path.join(str(TORNADO_ROOT), ".build", "_internal_TornadoVM_Maven-cleanAndinstall.run.xml")
-    with open(xml_directory, "w") as file:
-        file.write(xml_internal_content)
+def generate_internal_maven_build_xml(xml_templates_directory, project_directory, java_home, backend_profiles):
+    xml_internal_content = define_and_get_internal_maven_content(xml_templates_directory, project_directory, java_home, backend_profiles)
+    xml_build_directory = os.path.join(project_directory, ".build", "_internal_TornadoVM_Maven-cleanAndinstall.run.xml")
+    write_generated_template(xml_internal_content, xml_build_directory)    
 
-def generate_tornadovm_build_xml(TORNADO_ROOT, TORNADO_SDK, JAVA_HOME, PYTHON_HOME, BACKEND_PROFILES):
-    xml_build_content = define_and_get_tornadovm_build_content(TORNADO_ROOT, TORNADO_SDK, JAVA_HOME, PYTHON_HOME, BACKEND_PROFILES)
-    xml_directory = os.path.join(str(TORNADO_ROOT), ".build", "TornadoVM-Build.run.xml")
-    with open(xml_directory, "w") as file:
-        file.write(xml_build_content)
+def generate_tornadovm_build_xml(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backend_profiles):
+    xml_build_content = define_and_get_tornadovm_build_content(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backend_profiles)
+    xml_build_directory = os.path.join(project_directory, ".build", "TornadoVM-Build.run.xml")
+    write_generated_template(xml_build_content, xml_build_directory)
 
-def generate_tornadovm_tests_xml(TORNADO_ROOT, TORNADO_SDK, JAVA_HOME, PYTHON_HOME, BACKEND_PROFILES):
-    xml_tests_content = define_and_get_tornadovm_tests_content(TORNADO_ROOT, TORNADO_SDK, JAVA_HOME, PYTHON_HOME, BACKEND_PROFILES)
-    xml_directory = os.path.join(str(TORNADO_ROOT), ".build", "TornadoVM-Tests.run.xml")
-    with open(xml_directory, "w") as file:
-        file.write(xml_tests_content)
+def generate_tornadovm_tests_xml(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backend_profiles):
+    xml_tests_content = define_and_get_tornadovm_tests_content(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backend_profiles)
+    xml_tests_directory = os.path.join(project_directory, ".build", "TornadoVM-Tests.run.xml")
+    write_generated_template(xml_tests_content, xml_tests_directory)
 
-def cleanup_build_directory(buildDirectoryString):
-    tornadoRootDirectory = Path(buildDirectoryString)
-    if tornadoRootDirectory.exists() and tornadoRootDirectory.is_dir():
-        for file in tornadoRootDirectory.iterdir():
+def generate_xml_files(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backends):
+    backends_separated_comma = ",".join(backends)
+    generate_internal_maven_build_xml(xml_templates_directory, project_directory, java_home, backends)
+    generate_tornadovm_build_xml(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backends_separated_comma)
+    generate_tornadovm_tests_xml(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backends_separated_comma)
+
+def cleanup_build_directory(build_directory_string):
+    tornado_root_directory = Path(build_directory_string)
+    if tornado_root_directory.exists() and tornado_root_directory.is_dir():
+        for file in tornado_root_directory.iterdir():
             if file.name == ".gitignore":
                 continue
             if file.is_file():
                 file.unlink()
 
-def tornadovm_ide_init(tornadoSDKPath, javaHome, backends):
+def tornadovm_ide_init(tornado_sdk, java_home, backends):
     """
     Function to generate IDEA xml files for building and running TornadoVM.
     """
 
-    if tornadoSDKPath == None:
+    if tornado_sdk == None:
         print("Cannot initiate ide. TORNADO_SDK is not defined")
         sys.exit(0)
 
-    if javaHome == None:
+    if java_home == None:
         print("Cannot initiate ide. JAVA_HOME is not defined")
         sys.exit(0)
 
@@ -223,24 +146,22 @@ def tornadovm_ide_init(tornadoSDKPath, javaHome, backends):
         print("Cannot initiate ide. Backends are not defined")
         sys.exit(0)
 
-    backends_separated_comma = ",".join(backends)
-
     if os.name == 'nt':
-        # Multiple paths of pytho executables may be listed, the first is selected as default
-        pythonHome = subprocess.check_output(["where", "python"]).decode().strip().split("\r\n")
-        pythonHome = Path(pythonHome[0])
+        # Multiple paths of python executables may be listed, the first is selected as default
+        python_home = subprocess.check_output(["where", "python"]).decode().strip().split("\r\n")
+        python_home = Path(python_home[0])
     else:
-        pythonHome = subprocess.check_output(["which", "python3"]).decode().strip()
+        python_home = subprocess.check_output(["which", "python3"]).decode().strip()
 
-    if pythonHome == None:
+    if python_home == None:
         print("python is not found in the PATH.")
         sys.exit(0)
 
-    tornadoRoot = Path(tornadoSDKPath)
-    tornadoRoot = tornadoRoot.parents[1]
+    tornado_sdk_path = Path(tornado_sdk)
+    tornado_root = tornado_sdk_path.parents[1]
+    project_directory = str(tornado_root)
 
-    cleanup_build_directory(os.path.join(str(tornadoRoot), ".build"))
+    cleanup_build_directory(os.path.join(project_directory, ".build"))
+    xml_templates_directory = os.path.join(project_directory, "scripts", "templates", "intellij-settings", "ideinit")
 
-    generate_internal_maven_build_xml(tornadoRoot, javaHome, backends)
-    generate_tornadovm_build_xml(tornadoRoot, tornadoSDKPath, javaHome, pythonHome, backends_separated_comma)
-    generate_tornadovm_tests_xml(tornadoRoot, tornadoSDKPath, javaHome, pythonHome, backends_separated_comma)
+    generate_xml_files(xml_templates_directory, project_directory, tornado_sdk, java_home, python_home, backends)
