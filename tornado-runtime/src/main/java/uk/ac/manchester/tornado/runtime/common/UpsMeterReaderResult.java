f656b4e8e654a39b3d5d8c4d37ecdb233e420e5e
Thanos Stratikopoulos <tstratiko@gmail.com>
2024-11-05 11:58:13 +0000

    [style] Add copyright header java doc and dynamic IP

--- tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/UpsMeterReaderResult.java
+++ tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/UpsMeterReaderResult.java
@@ -1,3 +1,26 @@
+/*
+ * This file is part of Tornado: A heterogeneous programming framework:
+ * https://github.com/beehive-lab/tornadovm
+ *
+ * Copyright (c) 2024, APT Group, Department of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 package uk.ac.manchester.tornado.runtime.common;
 
 import org.snmp4j.CommunityTarget;
@@ -15,10 +38,15 @@ import org.snmp4j.transport.DefaultUdpTransportMapping;
 
 public class UpsMeterReaderResult {
 
+    /*
+     * The constants are specific identifiers for the EATON 5PX UPS model that is used
+     * to query the output voltage and power metrics via the SNMP protocol.
+     * The constants can be appended with new values or extended with additional metrics.
+     */
     private static String OUTPUT_VOLTAGE_OID = "1.3.6.1.2.1.33.1.4.4.1.2.1";
     private static String OUTPUT_POWER_OID = "1.3.6.1.2.1.33.1.4.4.1.4.1";
     private static String COMMUNITY = "public";
-    private static String ADDRESS = "192.168.8.193";
+    private static String ADDRESS = TornadoOptions.UPS_IP_ADDRESS;
     private static int SNMP_VERSION = SnmpConstants.version1;
 
     private static String getSnmpValue(String oid) {
