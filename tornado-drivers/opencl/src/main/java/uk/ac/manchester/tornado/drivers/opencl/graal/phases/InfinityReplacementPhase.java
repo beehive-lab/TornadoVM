8de52cbc11e29bdbb261c49827f68906d27496e5
mikepapadim <mikepapadim@hotmail.com>
2024-11-21 13:43:35 +0000

    Simplify infinity replacement

--- tornado-drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/phases/InfinityReplacementPhase.java
+++ tornado-drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/phases/InfinityReplacementPhase.java
@@ -21,7 +21,7 @@
 package uk.ac.manchester.tornado.drivers.opencl.graal.phases;
 
 import jdk.vm.ci.meta.JavaConstant;
-]import org.graalvm.compiler.nodes.GraphState;
+import org.graalvm.compiler.nodes.GraphState;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.phases.Phase;
 
@@ -40,42 +40,28 @@ public class InfinityReplacementPhase  extends Phase {
     @Override
     protected void run(StructuredGraph graph) {
         graph.getNodes().filter(ConstantNode.class).forEach(constantNode -> {
-
-
-            String resultValue = constantNode.getValue().toValueString(); // Get the result of the division
+            String resultValue = constantNode.getValue().toValueString();
             if (resultValue.equals("Infinity") || resultValue.equals("-Infinity")) {
-
-                if (resultValue.equals("Infinity")) {
-                    ConstantNode constantOne = ConstantNode.forConstant(JavaConstant.forFloat(1.0f), null, graph);
-                    ConstantNode constantZero = ConstantNode.forConstant(JavaConstant.forFloat(0.0f), null, graph);
-
-                    // Step 2: Create the division node (1.0f / 0.0f)
-                    FloatDivNode divisionNode = new FloatDivNode(constantOne, constantZero);
-
-                    // Add the division node to the graph
-                    graph.addOrUnique(divisionNode);
-
-                    constantNode.replaceAtUsages(divisionNode);
-                    if (constantNode.hasNoUsages()) {
-                        constantNode.safeDelete();
-                    }
-
-                } else if (resultValue.equals("-Infinity")) {
-                    ConstantNode constantMinusOne = ConstantNode.forConstant(JavaConstant.forFloat(-1.0f), null, graph);
-                    ConstantNode constantZero = ConstantNode.forConstant(JavaConstant.forFloat(0.0f), null, graph);
-
-                    // Step 2: Create the division node (1.0f / 0.0f)
-                    FloatDivNode divisionNode = new FloatDivNode(constantMinusOne, constantZero);
-
-                    // Add the division node to the graph
-                    graph.addOrUnique(divisionNode);
-
-                    constantNode.replaceAtUsages(divisionNode);
-                    if (constantNode.hasNoUsages()) {
-                        constantNode.safeDelete();
-                    }
-                }
+                float constantValue = resultValue.equals("Infinity") ? 1.0f : -1.0f;
+                replaceWithDivisionNode(constantValue, graph, constantNode);
             }
         });
     }
+
+    private void replaceWithDivisionNode(float constantValue, StructuredGraph graph, ConstantNode constantNode) {
+        ConstantNode constant = ConstantNode.forConstant(JavaConstant.forFloat(constantValue), null, graph);
+        ConstantNode constantZero = ConstantNode.forConstant(JavaConstant.forFloat(0.0f), null, graph);
+
+        // Create the division node (constantValue / 0.0f)
+        FloatDivNode divisionNode = new FloatDivNode(constant, constantZero);
+
+        // Add the division node to the graph
+        graph.addOrUnique(divisionNode);
+
+        constantNode.replaceAtUsages(divisionNode);
+        if (constantNode.hasNoUsages()) {
+            constantNode.safeDelete();
+        }
+    }
+
 }
