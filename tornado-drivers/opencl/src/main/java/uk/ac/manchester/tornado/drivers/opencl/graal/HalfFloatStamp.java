e7af5d68300b5c1097ca28bf2cf58399074541b8
MaryXek <xekalaki.mary@gmail.com>
2025-01-17 10:25:16 +0200

    Fix license header

--- tornado-drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/HalfFloatStamp.java
+++ tornado-drivers/opencl/src/main/java/uk/ac/manchester/tornado/drivers/opencl/graal/HalfFloatStamp.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2018-2022 APT Group, Department of Computer Science,
+ * Copyright (c) 2025, APT Group, Department of Computer Science,
  * The University of Manchester. All rights reserved.
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
