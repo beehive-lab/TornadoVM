f083c60a0690400a697aadcde5c46ce37a19a5e9
mikepapadim <mikepapadim@hotmail.com>
2025-02-13 23:55:47 +0200

    [WIP] Add missing taskgraph noce

---
+++ tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/graph/nodes/OnDeviceObjectNode.java
@@ -0,0 +1,42 @@
+package uk.ac.manchester.tornado.runtime.graph.nodes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OnDeviceObjectNode extends ContextOpNode {
+    public OnDeviceObjectNode(ContextNode context) {
+        super(context);
+    }
+
+    private ObjectNode value;
+
+    public void setValue(ObjectNode object) {
+        value = object;
+    }
+
+    public ObjectNode getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d]: on-device object %d", id,  value.getIndex());
+    }
+
+    @Override
+    public boolean hasInputs() {
+        return value != null;
+    }
+
+    @Override
+    public List<AbstractNode> getInputs() {
+        if (!hasInputs()) {
+            return Collections.emptyList();
+        }
+
+        final List<AbstractNode> result = new ArrayList<AbstractNode>();
+        result.add(value);
+        return result;
+    }
+}
