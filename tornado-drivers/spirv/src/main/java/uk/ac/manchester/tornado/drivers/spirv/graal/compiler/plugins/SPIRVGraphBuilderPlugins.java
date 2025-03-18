f09620f8690d6143f311c901ffae7fa0a9e2da99
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2025-03-18 16:17:18 +0200

    [fix] Add sqrt plugin for float type in SPIRV

--- tornado-drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/graal/compiler/plugins/SPIRVGraphBuilderPlugins.java
+++ tornado-drivers/spirv/src/main/java/uk/ac/manchester/tornado/drivers/spirv/graal/compiler/plugins/SPIRVGraphBuilderPlugins.java
@@ -330,6 +330,14 @@ public class SPIRVGraphBuilderPlugins {
             }
         });
 
+        r.register(new InvocationPlugin("sqrt", Float.TYPE) {
+            @Override
+            public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode value) {
+                b.push(JavaKind.Float, b.append(SPIRVFPUnaryIntrinsicNode.create(value, SQRT, JavaKind.Float)));
+                return true;
+            }
+        });
+
         r.register(new InvocationPlugin("sqrt", Double.TYPE) {
             @Override
             public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode value) {
