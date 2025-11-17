417a28e27587b13c9a0bac22347204e2a4e3d454
ruiqi <yrqsdu1995@outlook.com>
2025-11-17 19:08:19 +0000

    update header file accordingly

--- tornado-drivers/ptx-jni/src/main/cpp/source/PTXModule.h
+++ tornado-drivers/ptx-jni/src/main/cpp/source/PTXModule.h
@@ -46,10 +46,10 @@ JNIEXPORT jbyteArray JNICALL Java_uk_ac_manchester_tornado_drivers_ptx_PTXModule
 /*
  * Class:     uk_ac_manchester_tornado_drivers_ptx_PTXModule
  * Method:    cuModuleLoadDataEx
- * Signature: (Ljava/lang/String;Ljava/lang/String;)V
+ * Signature: ([B[I)[B
  */
 JNIEXPORT jbyteArray JNICALL Java_uk_ac_manchester_tornado_drivers_ptx_PTXModule_cuModuleLoadDataEx
-        (JNIEnv *, jclass, jbyteArray, jstring);
+        (JNIEnv *, jclass, jbyteArray, jintArray);
 
 /*
  * Class:     uk_ac_manchester_tornado_drivers_ptx_PTXModule
