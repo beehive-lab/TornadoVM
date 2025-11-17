e8fbef294de92690954f1cef764570b50b453e91
ruiqi <yrqsdu1995@outlook.com>
2025-11-17 19:08:03 +0000

    pass the CUDA JIT flags individually instead of string them together

--- tornado-drivers/ptx-jni/src/main/cpp/source/PTXModule.cpp
+++ tornado-drivers/ptx-jni/src/main/cpp/source/PTXModule.cpp
@@ -27,22 +27,8 @@
 #include <cuda.h>
 
 #include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <cstdint>
-#include <limits>
 #include "PTXModule.h"
 #include "ptx_log.h"
-
-static const std::unordered_map<std::string, CUjit_option> CUDAJITFlagsMap {
-    {"CU_JIT_OPTIMIZATION_LEVEL", CU_JIT_OPTIMIZATION_LEVEL},
-    {"CU_JIT_TARGET",             CU_JIT_TARGET},
-    {"CU_JIT_MAX_REGISTERS",  CU_JIT_MAX_REGISTERS},
-    {"CU_JIT_THREADS_PER_BLOCK",         CU_JIT_THREADS_PER_BLOCK},
-    {"CU_JIT_CACHE_MODE",   CU_JIT_CACHE_MODE}
-};//currently supported CUDA JIT options in TornadoVM, not all options are included due to backwards compatibility.
 
 jbyteArray from_module(JNIEnv *env, CUmodule *module) {
     jbyteArray array = env->NewByteArray(sizeof(CUmodule));
@@ -92,10 +78,10 @@ JNIEXPORT jbyteArray JNICALL Java_uk_ac_manchester_tornado_drivers_ptx_PTXModule
 /*
  * Class:     uk_ac_manchester_tornado_drivers_ptx_PTXModule
  * Method:    cuModuleLoadDataEx
- * Signature:
+ * Signature: ([B[I)[B
  */
 JNIEXPORT jbyteArray JNICALL Java_uk_ac_manchester_tornado_drivers_ptx_PTXModule_cuModuleLoadDataEx
-  (JNIEnv *env, jclass clazz, jbyteArray source, jstring compilerFlags) {
+  (JNIEnv *env, jclass clazz, jbyteArray source, jintArray CompilerFlags) {
     CUresult result;
 
     size_t ptx_length = env->GetArrayLength(source);
@@ -107,67 +93,26 @@ JNIEXPORT jbyteArray JNICALL Java_uk_ac_manchester_tornado_drivers_ptx_PTXModule
     env->GetByteArrayRegion(source, 0, ptx_length, reinterpret_cast<jbyte *>(ptx));
     ptx[ptx_length] = 0; // Make sure string terminates with a 0
 
-    const char* chars = env->GetStringUTFChars(compilerFlags, nullptr);
-    std::string flags(chars);
-    env->ReleaseStringUTFChars(compilerFlags, chars);
+    unsigned int length = env->GetArrayLength(CompilerFlags);
 
-    std::istringstream iss(flags);
-    std::vector<CUjit_option> options;
-    std::vector<unsigned int> values;
+    jint *elements = env->GetIntArrayElements(CompilerFlags, NULL);
 
-    std::string token;
-    std::vector<std::pair<std::string, std::string>> entries;
-    while (iss >> token) {
-        if (token.rfind("CU_JIT", 0) == 0) {
-            std::string val;
-            if (!(iss >> val)) {
-                throw std::runtime_error("Missing value for flag: " + token);
-            }
-            entries.emplace_back(token, val);
-        } else {
-            std::cerr << "Skipping unsupported CUDA JIT flag: " << token << "\n";
-        }
-    }
-    options.reserve(entries.size());
-    values.reserve(entries.size());
-
-    for (const auto &p : entries) {
-        const std::string &flagName = p.first;
-        const std::string &valStr = p.second;
-
-        auto it = CUDAJITFlagsMap.find(flagName);
-        if (it == CUDAJITFlagsMap.end()) {
-            std::cerr << "Unsupported CUDA JIT flag: " << flagName << "\n";
-            continue;
-        }
-
-        options.push_back(it->second);
-        try{
-            unsigned int v = std::stoi(valStr);
-            if (v > std::numeric_limits<unsigned int>::max()) {
-                std::cerr << "Warning: value for " << flagName << " (" << valStr << ") out of range for unsigned int.\n";
-                v = std::numeric_limits<unsigned int>::max();
-            }
-            values.push_back(v);
-        }
-        catch (const std::invalid_argument&) {
-            std::cerr << "Error: value for " << flagName << " is not a valid integer (" << valStr << ")\n";
-            values.push_back(0);
-        }
-        catch (const std::out_of_range&) {
-            std::cerr << "Error: value for " << flagName << " is out of integer range (" << valStr << ")\n";
-            unsigned int v = std::numeric_limits<unsigned int>::max();
-            values.push_back(v);
-        }
+    CUjit_option options[length];
+    void **jitOptVals = new void *[length];
+    for(int i = 0; i < length; i++){
+        jitOptVals[i] = (void *)(uintptr_t)elements[i];
     }
 
-    void **jitOptVals = new void *[options.size()];
-    for(int i = 0; i < options.size(); i++){
-        jitOptVals[i] = (void *)(uintptr_t)values[i];
-    }
+    options[0] = CU_JIT_OPTIMIZATION_LEVEL;
+    options[1] = CU_JIT_TARGET;
+    options[2] = CU_JIT_MAX_REGISTERS;
+    options[3] = CU_JIT_CACHE_MODE;
+    options[4] = CU_JIT_GENERATE_DEBUG_INFO;
+    options[5] = CU_JIT_LOG_VERBOSE;
+    options[6] = CU_JIT_GENERATE_LINE_INFO;
 
     CUmodule module;
-    result = cuModuleLoadDataEx(&module, ptx, options.size(),  options.data(), (void **)jitOptVals);
+    result = cuModuleLoadDataEx(&module, ptx, length,  options, (void **)jitOptVals);
 
     delete[] jitOptVals;
 
