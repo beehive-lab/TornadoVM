91ca2b5083b415a4ef5a1511aba575057a3a3f9a
ruiqi <yrqsdu1995@outlook.com>
2025-11-18 11:31:56 +0000

    fix potential type issues in PTXModule.cpp

--- tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/TornadoOptions.java
+++ tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/common/TornadoOptions.java
@@ -51,32 +51,32 @@ public class TornadoOptions {
     /**
      * Target microarchitecture for the CUDA JIT compiler. Default is 120 (Blackwell).
      */
-    //public static final int PTX_COMPILER_JIT_TARGET = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_TARGET", "120"));
+    public static final int PTX_COMPILER_JIT_TARGET = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_TARGET", "120"));
 
     /**
      * Maximum number of registers a CUDA thread can use. Default is 255.
      */
-    //public static final int PTX_COMPILER_MAX_REG = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_MAX_REGISTERS", "255"));
+    public static final int PTX_COMPILER_MAX_REG = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_MAX_REGISTERS", "255"));
 
     /**
      * Data load cache mode of the CUDA JIT compiler. Default is 0 (no -dlcm flag specified).
      */
-    //public static final int PTX_COMPILER_CACHE_MODE = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_CACHE_MODE", "0"));
+    public static final int PTX_COMPILER_CACHE_MODE = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_CACHE_MODE", "0"));
 
     /**
      * Generate debug info for the CUDA JIT compiler. Default is 0 (false).
      */
-    //public static final int PTX_COMPILER_GENERATE_DEBUG_INFO = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_GENERATE_DEBUG_INFO", "0"));
+    public static final int PTX_COMPILER_GENERATE_DEBUG_INFO = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_GENERATE_DEBUG_INFO", "0"));
 
     /**
      * Generate verbose log for the CUDA JIT compiler. Default is 0 (false).
      */
-    //public static final int PTX_COMPILER_LOG_VERBOSE = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_LOG_VERBOSE", "0"));
+    public static final int PTX_COMPILER_LOG_VERBOSE = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_LOG_VERBOSE", "0"));
 
     /**
      * Generate line info for the CUDA JIT compiler. Default is 0 (false).
      */
-    //public static final int PTX_COMPILER_GENERATE_LINE_INFO = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_GENERATE_LINE_INFO", "0"));
+    public static final int PTX_COMPILER_GENERATE_LINE_INFO = Integer.parseInt(getProperty("tornado.ptx.compiler.CU_JIT_GENERATE_LINE_INFO", "0"));
 
     /**
      * Default SPIR-V/LevelZero Flags.
