52a5214b28ab702811915b4a3f833b05b004e198
Thanos Stratikopoulos <34061419+stratika@users.noreply.github.com>
2025-12-16 13:39:46 +0200

    Update tornado-drivers/ptx/src/main/java/uk/ac/manchester/tornado/drivers/ptx/CUjitOption.java

--- tornado-drivers/ptx/src/main/java/uk/ac/manchester/tornado/drivers/ptx/CUjitOption.java
+++ tornado-drivers/ptx/src/main/java/uk/ac/manchester/tornado/drivers/ptx/CUjitOption.java
@@ -2,7 +2,7 @@
  * This file is part of Tornado: A heterogeneous programming framework:
  * https://github.com/beehive-lab/tornadovm
  *
- * Copyright (c) 2020, APT Group, Department of Computer Science,
+ * Copyright (c) 2025, APT Group, Department of Computer Science,
  * School of Engineering, The University of Manchester. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
