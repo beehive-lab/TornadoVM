0859439a5537a6dddb873a63fdb75bf9a7fdcb5e
Lorè Jacopo <jacopo.lore@tasgroup.eu>
2026-01-01 17:52:55 +0100

    add the new test in the tornado-test.py script and add more test that uses TornadoNativeTypes instead of primitive types

--- tornado-unittests/src/main/java/uk/ac/manchester/tornado/unittests/fields/TestInheritedFields.java
+++ tornado-unittests/src/main/java/uk/ac/manchester/tornado/unittests/fields/TestInheritedFields.java
@@ -1,6 +1,6 @@
 package uk.ac.manchester.tornado.unittests.fields;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import uk.ac.manchester.tornado.api.TaskGraph;
@@ -8,6 +8,7 @@ import uk.ac.manchester.tornado.api.TornadoExecutionPlan;
 import uk.ac.manchester.tornado.api.annotations.Parallel;
 import uk.ac.manchester.tornado.api.enums.DataTransferMode;
 import uk.ac.manchester.tornado.api.exceptions.TornadoExecutionPlanException;
+import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
 import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
 
 /**
@@ -20,28 +21,32 @@ import uk.ac.manchester.tornado.unittests.common.TornadoTestBase;
  */
 public class TestInheritedFields extends TornadoTestBase {
 
-    public static class A{
+    /**
+     * primitive field
+     */
+
+    public static class PrimitiveA {
         public final float[] valueA;
-        public A(int size){
+        public PrimitiveA(int size){
             valueA = new float[size];
         }
     }
 
-    public static class B extends A{
+    public static class PrimitiveB extends PrimitiveA {
         public final float[] valueB;
-        public B(int size){
+        public PrimitiveB(int size){
             super(size);
             valueB = new float[size];
         }
     }
 
-    public static void incrementA(A a, int size){
+    public static void incrementPrimitiveA(PrimitiveA a, int size){
         for(@Parallel int i = 0; i<size; i++){
             a.valueA[i] += 1f;
         }
     }
 
-    public static void incrementB(B b, int size){
+    public static void incrementPrimitiveB(PrimitiveB b, int size){
         for(@Parallel int i = 0; i<size; i++){
             b.valueA[i] += 1f;
             b.valueB[i] += 1f;
@@ -49,39 +54,111 @@ public class TestInheritedFields extends TornadoTestBase {
     }
 
     @Test
-    public void testIncrementB(){
+    public void testIncrementPrimitiveB(){
         final int N = 1000;
-        B b = new B(N);
+        PrimitiveB b = new PrimitiveB(N);
 
         TaskGraph tg = new TaskGraph("tg");
         tg.transferToDevice(DataTransferMode.EVERY_EXECUTION, b);
-        tg.task("t0", TestInheritedFields::incrementB, b, N);
+        tg.task("t0", TestInheritedFields::incrementPrimitiveB, b, N);
         tg.transferToHost(DataTransferMode.EVERY_EXECUTION, b);
 
         try(TornadoExecutionPlan executionPlan = new TornadoExecutionPlan(tg.snapshot())){
             executionPlan.execute();
-            assertTrue(b.valueA[0] == 1f);
-            assertTrue(b.valueB[0] == 1f);
+            assertEquals(1f, b.valueA[0], 0.0);
+            assertEquals(1f, b.valueB[0], 0.0);
         } catch (TornadoExecutionPlanException e){
             e.printStackTrace();
         }
     }
 
     @Test
-    public void testIncrementAB(){
+    public void testIncrementPrimitiveAB(){
         final int N = 1000;
-        B b = new B(N);
+        PrimitiveB b = new PrimitiveB(N);
 
         TaskGraph tg = new TaskGraph("tg");
         tg.transferToDevice(DataTransferMode.EVERY_EXECUTION, b);
-        tg.task("t0", TestInheritedFields::incrementA, b, N);
-        tg.task("t1", TestInheritedFields::incrementB, b, N);
+        tg.task("t0", TestInheritedFields::incrementPrimitiveA, b, N);
+        tg.task("t1", TestInheritedFields::incrementPrimitiveB, b, N);
         tg.transferToHost(DataTransferMode.EVERY_EXECUTION, b);
 
         try(TornadoExecutionPlan executionPlan = new TornadoExecutionPlan(tg.snapshot())){
             executionPlan.execute();
-            assertTrue(b.valueA[0] == 2f);
-            assertTrue(b.valueB[0] == 1f);
+            assertEquals(2f, b.valueA[0], 0.0);
+            assertEquals(1f, b.valueB[0], 0.0);
+        } catch (TornadoExecutionPlanException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * tornado native types
+     */
+
+    public static class TornadoA {
+        public final FloatArray valueA;
+        public TornadoA(int size){
+            valueA = new FloatArray(size);
+        }
+    }
+
+    public static class TornadoB extends TornadoA {
+        public final FloatArray valueB;
+        public TornadoB(int size){
+            super(size);
+            valueB = new FloatArray(size);
+        }
+    }
+
+    public static void incrementTornadoA(TornadoA a, int size){
+        for(@Parallel int i = 0; i<size; i++){
+            a.valueA.set(i, a.valueA.get(i) + 1f);
+        }
+    }
+
+    public static void incrementTornadoB(TornadoB b, int size){
+        for(@Parallel int i = 0; i<size; i++){
+            b.valueA.set(i, b.valueA.get(i) + 1f);
+            b.valueB.set(i, b.valueB.get(i) + 1f);
+        }
+    }
+
+    @Test
+    public void testIncrementTornadoB(){
+        final int N = 1000;
+        TornadoB b = new TornadoB(N);
+
+        TaskGraph tg = new TaskGraph("tg");
+        tg.transferToDevice(DataTransferMode.EVERY_EXECUTION, b);
+        tg.task("t0", TestInheritedFields::incrementTornadoB, b, N);
+        tg.transferToHost(DataTransferMode.EVERY_EXECUTION, b);
+
+        try(TornadoExecutionPlan executionPlan = new TornadoExecutionPlan(tg.snapshot())){
+            executionPlan.execute();
+            assertEquals(1f, b.valueA.get(0), 0.0);
+            assertEquals(1f, b.valueB.get(0), 0.0);
+        } catch (TornadoExecutionPlanException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testIncrementTornadoAB(){
+        final int N = 1000;
+        TornadoB b = new TornadoB(N);
+
+        TaskGraph tg = new TaskGraph("tg");
+        tg.transferToDevice(DataTransferMode.EVERY_EXECUTION, b);
+        tg.task("t0", TestInheritedFields::incrementTornadoA, b, N);
+        tg.task("t1", TestInheritedFields::incrementTornadoB, b, N);
+        tg.transferToHost(DataTransferMode.EVERY_EXECUTION, b);
+
+        try(TornadoExecutionPlan executionPlan = new TornadoExecutionPlan(tg.snapshot())){
+            executionPlan.execute();
+            assertEquals(2f, b.valueA.get(0), 0.0);
+            assertEquals(1f, b.valueB.get(0), 0.0);
         } catch (TornadoExecutionPlanException e){
             e.printStackTrace();
         }
