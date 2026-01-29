8aad08c1e537924b4b81d633ecaaf866e376bb23
Lorè Jacopo <jacopo.lore@tasgroup.eu>
2026-01-29 20:25:35 +0100

    Removed unusued import

--- tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/utils/TornadoUtils.java
+++ tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/utils/TornadoUtils.java
@@ -25,7 +25,6 @@
  */
 package uk.ac.manchester.tornado.runtime.utils;
 
-import uk.ac.manchester.tornado.api.common.Access;
 import uk.ac.manchester.tornado.api.enums.TornadoDeviceType;
 import uk.ac.manchester.tornado.api.exceptions.TornadoRuntimeException;
 
