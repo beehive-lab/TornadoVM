91901a8341f9549419512c821333d1d2e9af904a
mikepapadim <mikepapadim@hotmail.com>
2026-07-01 22:11:13 +0100

    Add library task API and runtime SPI for external native libraries

---
+++ tornado-runtime/src/main/java/uk/ac/manchester/tornado/runtime/library/spi/LibraryInvocation.java
@@ -0,0 +1,87 @@
+/*
+ * This file is part of Tornado: A heterogeneous programming framework:
+ * https://github.com/beehive-lab/tornadovm
+ *
+ * Copyright (c) 2026, APT Group, Department of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+package uk.ac.manchester.tornado.runtime.library.spi;
+
+import uk.ac.manchester.tornado.runtime.common.TornadoXPUDevice;
+
+/**
+ * A single library-task call with its arguments already resolved by the
+ * TornadoVM interpreter: reference arguments (off-heap arrays/tensors) carry
+ * the raw device pointer of their TornadoVM-managed device buffer (past the
+ * array header), primitive arguments carry their boxed value.
+ */
+public final class LibraryInvocation {
+
+    private final Object[] javaArgs;
+    private final long[] devicePointers;
+    private final boolean[] isReference;
+    private final TornadoXPUDevice device;
+    private final long executionPlanId;
+    private final LibraryContext context;
+
+    public LibraryInvocation(Object[] javaArgs, long[] devicePointers, boolean[] isReference, TornadoXPUDevice device, long executionPlanId, LibraryContext context) {
+        this.javaArgs = javaArgs;
+        this.devicePointers = devicePointers;
+        this.isReference = isReference;
+        this.device = device;
+        this.executionPlanId = executionPlanId;
+        this.context = context;
+    }
+
+    public int getNumArgs() {
+        return javaArgs.length;
+    }
+
+    /**
+     * The original Java argument at the given position (boxed primitive, or the
+     * host-side array/tensor object for reference arguments).
+     */
+    public Object getArg(int index) {
+        return javaArgs[index];
+    }
+
+    /**
+     * The raw device pointer for a reference argument at the given position,
+     * pointing at the first data element (past the TornadoVM array header).
+     */
+    public long getDevicePointer(int index) {
+        return devicePointers[index];
+    }
+
+    public boolean isReference(int index) {
+        return isReference[index];
+    }
+
+    public TornadoXPUDevice getDevice() {
+        return device;
+    }
+
+    public long getExecutionPlanId() {
+        return executionPlanId;
+    }
+
+    public LibraryContext getContext() {
+        return context;
+    }
+}
