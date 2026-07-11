396e4fd4918596ea627561541759db09c8be32bc
mikepapadim <mikepapadim@hotmail.com>
2026-07-11 01:18:23 +0100

    Add BenchmarkCutlassGemm: JIT naive/tiled vs CUTLASS FP32/FP16 GEMM + fused vs unfused MLP (FP16 39 TFLOP/s = 7.7x tiled; fusion 1.28x)

--- tornado-cutlass/src/main/java/module-info.java
+++ tornado-cutlass/src/main/java/module-info.java
@@ -4,6 +4,7 @@ open module tornado.cutlass {
 
     exports uk.ac.manchester.tornado.cutlass;
     exports uk.ac.manchester.tornado.cutlass.provider;
+    exports uk.ac.manchester.tornado.cutlass.tests;
 
     provides uk.ac.manchester.tornado.runtime.library.spi.TornadoLibraryProvider with //
             uk.ac.manchester.tornado.cutlass.provider.CutlassLibraryProvider;
