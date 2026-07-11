519dd9267870963040219be1719bfcfc8337bf9f
mikepapadim <mikepapadim@hotmail.com>
2026-07-11 03:13:19 +0100

    Add cuTENSOR benchmark, README, and hybrid API branch tracker row (matmul 21 TFLOP/s = 5.4x JIT; two-mode contraction 2.3x)

--- tornado-cutensor/src/main/java/module-info.java
+++ tornado-cutensor/src/main/java/module-info.java
@@ -4,6 +4,7 @@ open module tornado.cutensor {
 
     exports uk.ac.manchester.tornado.cutensor;
     exports uk.ac.manchester.tornado.cutensor.provider;
+    exports uk.ac.manchester.tornado.cutensor.tests;
 
     provides uk.ac.manchester.tornado.runtime.library.spi.TornadoLibraryProvider with //
             uk.ac.manchester.tornado.cutensor.provider.CutensorLibraryProvider;
